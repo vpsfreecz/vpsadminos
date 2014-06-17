08b5a4ec99d4f44c9fdd596d039874996e90dcc5
Petr Vanek <pvanek@linux-a266.prague.qoretechnologies.com>
2014-06-17 13:44:42 +0200

    image-scripts: initial template for opensuse

---
+++ image-scripts/include/opensuse.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+. $BASEDIR/include/common.sh
+
+REPOSITORY=http://download.opensuse.org/distribution/$RELVER/repo/oss
+UPDATES=http://download.opensuse.org/update/$RELVER/
+EXTRAPKGS='vim'
+
+ZYPPER="zypper -v --root=$INSTALL --non-interactive --no-gpg-checks "
+
+function bootstrap {
+
+        $ZYPPER addrepo --refresh $REPOSITORY openSUSE-oss
+        $ZYPPER addrepo --refresh $UPDATES openSUSE-updates
+        $ZYPPER install openSUSE-release 
+        $ZYPPER install -t pattern base sw_management
+        $ZYPPER install $EXTRAPKGS
+	
+}
+
+function configure-opensuse {
+	configure-append <<EOF
+systemctl disable NetworkManager.service
+systemctl disable NetworkManager-wait-online.service
+systemctl disable NetworkManager-dispatcher.service
+systemctl enable  network.service
+usermod -L root
+cp /usr/share/zoneinfo/Europe/Prague /etc/localtime
+systemctl enable sshd
+EOF
+}
