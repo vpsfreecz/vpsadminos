2533cb8150fe97fde4922057b0cc4d8c82934196
Badbear <admin@jirimedved.cz>
2014-08-20 22:47:28 +0200

    image-scripts: Fix Fedora-20 template

--- image-scripts/templates/fedora-20.sh
+++ image-scripts/templates/fedora-20.sh
@@ -1,6 +1,6 @@
 DISTNAME=fedora
 RELVER=20
-RELEASE=http://ftp.fi.muni.cz/pub/linux/fedora/linux/releases/20/Fedora/x86_64/os/Packages/f/fedora-release-20-1.noarch.rpm
+RELEASE=http://ftp.fi.muni.cz/pub/linux/fedora/linux/releases/20/Fedora/x86_64/os/Packages/f/fedora-release-20-3.noarch.rpm
 BASEURL=http://ftp.fi.muni.cz/pub/linux/fedora/linux/releases/20/Fedora/x86_64/os
 UPDATES=http://ftp.fi.muni.cz/pub/linux/fedora/linux/updates/20/x86_64
 GROUPNAME='minimal install'
@@ -18,6 +18,7 @@ systemctl disable NetworkManager.service
 systemctl disable NetworkManager-wait-online.service
 systemctl disable NetworkManager-dispatcher.service
 systemctl enable  network.service
+systemctl disable firewalld.service
 EOF
 
 run-configure
