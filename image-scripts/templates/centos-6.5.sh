43350b47b84beabc1bf8477b9df79c534167e3a6
Stanislav Petr <glux@glux.org>
2014-09-14 17:01:10 +0200

    image-scripts: Update building all distros from Red Hat family. Add CentOS 6.5. Add CentOS 7.0.

---
+++ image-scripts/templates/centos-6.5.sh
@@ -0,0 +1,22 @@
+DISTNAME=centos
+RELVER=6.5
+RELVERPACK=11.1
+RELEASE=http://mirror.hosting90.cz/centos/${RELVER}/os/x86_64/Packages/centos-release-${RELVER//./-}.el6.centos.{$RELVERPACK}.x86_64.rpm
+BASEURL=http://mirror.hosting90.cz/centos/${RELVER}/os/x86_64
+UPDATES=http://mirror.hosting90.cz/centos/${RELVER}/updates/x86_64/
+GROUPNAME='core'
+EXTRAPKGS='vim openssh-clients'
+
+. $INCLUDE/redhat-family.sh
+
+bootstrap
+configure-common
+
+configure-redhat-common
+
+configure-append <<EOF
+/sbin/chkconfig network on
+/sbin/chkconfig iptables off
+EOF
+
+run-configure
