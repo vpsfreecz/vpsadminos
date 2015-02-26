f18f9090a4e0659d9ddef644b7279f094b799d0b
Badbear <admin@jirimedved.cz>
2015-02-26 12:43:27 +0100

    image-scripts: scientific linux 6.6

--- image-scripts/templates/scientific-6.6.sh
+++ image-scripts/templates/scientific-6.6.sh
@@ -1,9 +1,10 @@
 DISTNAME=scientific
-RELVER=6.5
-RELEASE=http://mirror.vpsfree.cz/scientific/6.5/x86_64/os/Packages/sl-release-6.5-1.x86_64.rpm
-BASEURL=http://mirror.vpsfree.cz/scientific/6.5/x86_64/os
-UPDATES=http://mirror.vpsfree.cz/scientific/6.5/x86_64/updates/security
-GROUPNAME='base'
+RELVER=6.6
+RELVERPACK=6.6-1
+RELEASE=http://mirror.vpsfree.cz/scientific/${RELVER}/x86_64/os/Packages/sl-release-${RELVERPACK}.x86_64.rpm
+BASEURL=http://mirror.vpsfree.cz/scientific/${RELVER}/x86_64/os
+UPDATES=http://mirror.vpsfree.cz/scientific/${RELVER}/x86_64/updates/security
+GROUPNAME='core'
 EXTRAPKGS='vim'
 
 . $INCLUDE/redhat-family.sh
