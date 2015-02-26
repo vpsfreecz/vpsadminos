e96b535775fdbd310ba593f46755822253f03572
Badbear <admin@jirimedved.cz>
2015-02-26 09:26:40 +0100

    image-scripts: update centos 7.0

--- image-scripts/templates/centos-7.0.sh
+++ image-scripts/templates/centos-7.0.sh
@@ -1,7 +1,7 @@
 DISTNAME=centos
 RELVER=7.0
 BUILD=1406
-RELVERPACK=2.3
+RELVERPACK=2.6
 RELEASE=http://mirror.hosting90.cz/centos/${RELVER}.${BUILD}/os/x86_64/Packages/centos-release-${RELVER//./-}.${BUILD}.el7.centos.${RELVERPACK}.x86_64.rpm
 BASEURL=http://mirror.hosting90.cz/centos/${RELVER}.${BUILD}/os/x86_64
 UPDATES=http://mirror.hosting90.cz/centos/${RELVER}.${BUILD}/updates/x86_64/
@@ -21,7 +21,6 @@ configure-append <<EOF
 /usr/bin/systemctl disable NetworkManager-dispatcher.service
 /usr/sbin/chkconfig network on
 /usr/bin/systemctl disable firewalld.service
-sed -i "s/\[1\-6\]/\[0\-6\]/" /etc/init/start-ttys.conf
 EOF
 
 run-configure
