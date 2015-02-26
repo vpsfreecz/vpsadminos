119c34e343f99f499f0e54c72ce2473b79ae37fb
Badbear <admin@jirimedved.cz>
2015-02-26 07:52:32 +0100

    image-scripts: centos 6.6

--- image-scripts/templates/centos-6.6.sh
+++ image-scripts/templates/centos-6.6.sh
@@ -1,16 +1,21 @@
 DISTNAME=centos
-RELVER=6.5
-RELVERPACK=11.1
+RELVER=6.6
+RELVERPACK=12.2
 RELEASE=http://mirror.hosting90.cz/centos/${RELVER}/os/x86_64/Packages/centos-release-${RELVER//./-}.el6.centos.{$RELVERPACK}.x86_64.rpm
 BASEURL=http://mirror.hosting90.cz/centos/${RELVER}/os/x86_64
 UPDATES=http://mirror.hosting90.cz/centos/${RELVER}/updates/x86_64/
 GROUPNAME='core'
-EXTRAPKGS='vim openssh-clients'
+EXTRAPKGS='openssh-clients'
 
 . $INCLUDE/redhat-family.sh
 
 bootstrap
 configure-common
+
+cat > $INSTALL/etc/fstab <<SOURCES
+none    /dev/pts        devpts  rw,gid=5,mode=620       0       0
+none    /dev/shm        tmpfs   defaults                0       0
+SOURCES
 
 configure-redhat-common
 
