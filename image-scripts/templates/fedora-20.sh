fabd0b992507069ed1cddc2ff03cfc3beb2f5cf1
Badbear <admin@jirimedved.cz>
2015-03-04 09:47:02 +0100

    image-scripts: fedora 20: fix release version

--- image-scripts/templates/fedora-20.sh
+++ image-scripts/templates/fedora-20.sh
@@ -1,6 +1,6 @@
 DISTNAME=fedora
 RELVER=20
-RELEASE=http://ftp.fi.muni.cz/pub/linux/fedora/linux/releases/20/Fedora/x86_64/os/Packages/f/fedora-release-20-3.noarch.rpm
+RELEASE=http://ftp.fi.muni.cz/pub/linux/fedora/linux/releases/20/Fedora/x86_64/os/Packages/f/fedora-release-20-1.noarch.rpm
 BASEURL=http://ftp.fi.muni.cz/pub/linux/fedora/linux/releases/20/Fedora/x86_64/os
 UPDATES=http://ftp.fi.muni.cz/pub/linux/fedora/linux/updates/20/x86_64
 GROUPNAME='minimal install'
