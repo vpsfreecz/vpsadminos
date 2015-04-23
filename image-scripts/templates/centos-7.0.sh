27c6f7ac3d8a0e5c1e29039209b5849c2a508bff
Badbear <admin@jirimedved.cz>
2015-04-23 21:30:53 +0200

    image-scripts: centos 7: update

--- image-scripts/templates/centos-7.0.sh
+++ image-scripts/templates/centos-7.0.sh
@@ -2,9 +2,9 @@ DISTNAME=centos
 RELVER=7.0
 BUILD=1406
 RELVERPACK=2.3
-RELEASE=http://mirror.hosting90.cz/centos/${RELVER}.${BUILD}/os/x86_64/Packages/centos-release-${RELVER//./-}.${BUILD}.el7.centos.${RELVERPACK}.x86_64.rpm
-BASEURL=http://mirror.hosting90.cz/centos/${RELVER}.${BUILD}/os/x86_64
-UPDATES=http://mirror.hosting90.cz/centos/${RELVER}.${BUILD}/updates/x86_64/
+RELEASE=http://vault.centos.org/${RELVER}.${BUILD}/os/x86_64/Packages/centos-release-${RELVER//./-}.${BUILD}.el7.centos.${RELVERPACK}.x86_64.rpm
+BASEURL=http://vault.centos.org/${RELVER}.${BUILD}/os/x86_64
+UPDATES=http://vault.centos.org/${RELVER}.${BUILD}/updates/x86_64/
 GROUPNAME='core'
 EXTRAPKGS='vim'
 
