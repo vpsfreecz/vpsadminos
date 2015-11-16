a74b27af9ec66edafebbe079ae93506d5b343fc3
jiri.medved <admin@jirimedved.cz>
2015-11-16 14:35:41 +0100

    image-scripts: templates: update scientific 6.x

--- image-scripts/templates/scientific-6.sh
+++ image-scripts/templates/scientific-6.sh
@@ -1,6 +1,6 @@
 DISTNAME=scientific
-RELVER=6.6
-RELVERPACK=6.6-1
+RELVER=6.7
+RELVERPACK=6.7-2
 RELEASE=http://mirror.vpsfree.cz/scientific/${RELVER}/x86_64/os/Packages/sl-release-${RELVERPACK}.x86_64.rpm
 BASEURL=http://mirror.vpsfree.cz/scientific/${RELVER}/x86_64/os
 UPDATES=http://mirror.vpsfree.cz/scientific/${RELVER}/x86_64/updates/security
