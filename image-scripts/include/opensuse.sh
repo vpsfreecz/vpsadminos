3bc2f512c53123b1edca7c9b456cca5fba4dab8f
Tomáš Chvátal <tchvatal@suse.com>
2017-03-29 13:55:13 +0200

    image-scripts: Remove cnf command from default install as it pulls full python stack

--- image-scripts/include/opensuse.sh
+++ image-scripts/include/opensuse.sh
@@ -13,7 +13,7 @@ else
 	UPDATES=http://download.opensuse.org/update/$RELVER/
 fi
 
-EXTRAPKGS='vim iproute2 iputils command-not-found net-tools procps less psmisc'
+EXTRAPKGS='vim iproute2 iputils net-tools procps less psmisc'
 
 ZYPPER="zypper -v --root=$INSTALL --non-interactive --gpg-auto-import-keys "
 
