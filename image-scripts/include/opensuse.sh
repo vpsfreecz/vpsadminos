25c8b529160a3ef91e6b39cf5badb513058cda43
Tomáš Chvátal <tchvatal@suse.com>
2017-05-16 15:24:34 +0200

    image-scripts: Hack is no longer needed

--- image-scripts/include/opensuse.sh
+++ image-scripts/include/opensuse.sh
@@ -18,11 +18,6 @@ EXTRAPKGS='vim iproute2 iputils net-tools procps less psmisc timezone aaa_base-e
 ZYPPER="zypper -v --root=$INSTALL --non-interactive --gpg-auto-import-keys "
 
 function bootstrap {
-	if [ $RELVER == "42.2" ]; then
-		# hack to fix wicked until maintenance update is released
-		# will be obsoleted by the next version update
-		$ZYPPER ar -f http://download.opensuse.org/repositories/home:/scarabeus_iv:/branches:/openSUSE:/Leap:/42.2:/Update/standard/home:scarabeus_iv:branches:openSUSE:Leap:42.2:Update.repo
-	fi
 	$ZYPPER addrepo --refresh $REPOSITORY openSUSE-oss
 	$ZYPPER addrepo --refresh $UPDATES openSUSE-updates
 	$ZYPPER install --no-recommends aaa_base shadow
