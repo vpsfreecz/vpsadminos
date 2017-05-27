21df134f16939e46e06d76506e14af7495cbc16d
Richard Marko <srk@48.io>
2017-05-27 22:25:50 +0200

    image-scripts: add centos-7.3

---
+++ image-scripts/templates/centos-7.3.sh
@@ -0,0 +1,25 @@
+DISTNAME=centos
+RELVER=7.3
+BUILD=1611
+RELEASE=http://mirror.centos.org/centos/${RELVER}.${BUILD}/os/x86_64/Packages/centos-release-${RELVER//./-}.${BUILD}.el7.centos.x86_64.rpm
+BASEURL=http://mirror.centos.org/centos/${RELVER}.${BUILD}/os/x86_64
+UPDATES=http://mirror.centos.org/centos/${RELVER}.${BUILD}/updates/x86_64/
+GROUPNAME='core'
+EXTRAPKGS='vim man'
+
+. $INCLUDE/redhat-family.sh
+
+bootstrap
+configure-common
+
+configure-redhat-common
+
+configure-append <<EOF
+/usr/bin/systemctl disable NetworkManager.service
+/usr/bin/systemctl disable NetworkManager-wait-online.service
+/usr/bin/systemctl disable NetworkManager-dispatcher.service
+/usr/sbin/chkconfig network on
+/usr/bin/systemctl disable firewalld.service
+EOF
+
+run-configure
