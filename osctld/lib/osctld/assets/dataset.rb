f98b9b1b9010f5a0abad1507bcb60178a2a37d0e
Jakub Skokan <jakub.skokan@havefun.cz>
2018-02-04 17:52:58 +0100

    Assets: check dataset user, group or mode

--- osctld/lib/osctld/assets/dataset.rb
+++ osctld/lib/osctld/assets/dataset.rb
@@ -7,31 +7,61 @@ module OsCtld
     include Utils::Zfs
 
     # @param opts [Hash] options
-    # @opt opts [Integer, nil] uidoffset
-    # @opt opts [Integer, nil] gidoffset
+    # @option opts [Integer, nil] uidoffset
+    # @option opts [Integer, nil] gidoffset
+    # @option opts [Integer, nil] user
+    # @option opts [Integer, nil] group
+    # @option opts [Integer, nil] mode
     def initialize(path, opts)
       super
     end
 
     def valid?
-      ret = zfs(:get, '-H -ovalue uidoffset,gidoffset', path, valid_rcs: [1])
+      ret = zfs(
+        :get,
+        '-H -ovalue mountpoint,uidoffset,gidoffset',
+        path,
+        valid_rcs: [1]
+      )
 
       if ret[:exitstatus] > 0
         add_error('does not exist')
         return super
       end
 
-      uidoffset, gidoffset = ret[:output].split("\n").map(&:to_i)
+      @mountpoint, uidoffset, gidoffset = ret[:output].split("\n")
 
-      if opts[:uidoffset] && opts[:uidoffset] != uidoffset
+      if opts[:user] && stat.uid != opts[:user]
+        add_error('invalid owner')
+      end
+
+      if opts[:group] && stat.gid != opts[:group]
+        add_error('invalid group')
+      end
+
+      if opts[:mode] && mode != opts[:mode]
+        add_error('invalid mode')
+      end
+
+      if opts[:uidoffset] && opts[:uidoffset] != uidoffset.to_i
         add_error('invalid uidoffset')
       end
 
-      if opts[:gidoffset] && opts[:gidoffset] != gidoffset
+      if opts[:gidoffset] && opts[:gidoffset] != gidoffset.to_i
         add_error('invalid gidoffset')
       end
 
       super
+    end
+
+    protected
+    def stat
+      @stat ||= File.stat(@mountpoint)
+    end
+
+    def mode
+      # Extract permission bits, see man inode(7)
+      stat.mode & 07777
     end
   end
 end
