d80cb3819ff343d2ac408f3834ae0bd8e035f296
Jakub Skokan <jakub.skokan@havefun.cz>
2018-02-05 16:40:19 +0100

    Assets: add local sockets

---
+++ osctld/lib/osctld/assets/unix_socket.rb
@@ -0,0 +1,14 @@
+module OsCtld
+  class Assets::UnixSocket < Assets::BaseFile
+    register :socket
+
+    def valid?
+      add_error('not a socket') if exist? && !opts[:optional] && !stat.socket?
+      super
+
+    rescue Errno::ENOENT
+      add_error('does not exist')
+      false
+    end
+  end
+end
