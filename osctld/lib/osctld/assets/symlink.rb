01c057fbfde32270782c4141e65245f423a47687
Jakub Skokan <jakub.skokan@havefun.cz>
2018-02-05 16:40:39 +0100

    Assets: add symlinks

---
+++ osctld/lib/osctld/assets/symlink.rb
@@ -0,0 +1,19 @@
+module OsCtld
+  class Assets::Symlink < Assets::BaseFile
+    register :symlink
+
+    def valid?
+      add_error('not a symlink') if exist? && !opts[:optional] && !stat.symlink?
+      super
+
+    rescue Errno::ENOENT
+      add_error('does not exist')
+      false
+    end
+
+    protected
+    def stat
+      @stat ||= File.lstat(path)
+    end
+  end
+end
