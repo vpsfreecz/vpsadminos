5c21c13e8ac4d0f1a5819933f6bf8db6ebbe0dfe
Jakub Skokan <jakub.skokan@havefun.cz>
2018-04-03 18:12:06 +0200

    os: update zfs to current unstable

--- os/overlays/zfs.nix
+++ os/overlays/zfs.nix
@@ -2,12 +2,12 @@ self: super:
 {
   zfs = super.zfsUnstable.overrideAttrs (oldAttrs: rec {
     name = "zfs-${version}";
-    version = "2017-11-16.aither";
+    version = "2018-04-03.aither";
     src = super.fetchFromGitHub {
       owner = "aither64";
       repo = "zfs";
-      rev = "16148803eb0adb351b9a2c083729db9aca26f7bc";
-      sha256 = "0q2d93k4kg4y9ljk12qlkzlrv6sqild1ki2p9y794fawy9c1py6g";
+      rev = "57d3aed17eab10e028c6b4c965eb30721d654e5e";
+      sha256 = "1cmjnb9pmag4vwvfvx4l1603q3pd2f15yhhl64sg74wa4v972d9b";
     };
   });
 }
