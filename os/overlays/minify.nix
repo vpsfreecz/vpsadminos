7a19b79b28cdb930cf840bb1d3fc500c88b44451
Richard Marko <srk@48.io>
2018-05-01 12:00:28 +0200

    os: use dhcpcd without udev

--- os/overlays/minify.nix
+++ os/overlays/minify.nix
@@ -3,4 +3,5 @@ self: super:
   lxc = super.lxc.override { systemd = null; };
   gnupg = super.gnupg.override { guiSupport = false; libusb = null; pinentry = null; openldap = null; };
   utillinux = super.utillinux.override { systemd = null; };
+  dhcpcd = super.dhcpcd.override { udev = null; };
 }
