e89b50d12c016c3593d48d02e8343359159652e3
Jakub Skokan <jakub.skokan@havefun.cz>
2018-05-31 11:07:25 +0200

    os: move motd from configs/common.nix to configs/default.nix

--- os/configs/common.nix
+++ os/configs/common.nix
@@ -40,30 +40,6 @@
     home = "/run/osctl/repository";
   };
 
-  users.motd = ''
-
-    Welcome to vpsAdminOS
-
-    Configure osctld:
-      osctl pool install tank
-
-    Create a user:
-      osctl user new --ugid 5000 --map 0:666000:65536 myuser01
-
-    Create a container:
-      osctl ct new --user myuser01 --distribution alpine --version 3.7 myct01
-
-    Configure container networking:
-      osctl ct netif new routed --via 10.100.10.100/30 myct01 eth0
-      osctl ct netif ip add myct01 eth0 1.2.3.4/32
-
-    Start the container:
-      osctl ct start myct01
-
-    More information:
-      man osctl
-    '';
-
   programs.ssh.package = pkgs.openssh;
   programs.htop.enable = true;
 }
--- os/configs/default.nix
+++ os/configs/default.nix
@@ -23,4 +23,28 @@
     screen
     strace
   ];
+
+  users.motd = ''
+
+    Welcome to vpsAdminOS
+
+    Configure osctld:
+      osctl pool install tank
+
+    Create a user:
+      osctl user new --ugid 5000 --map 0:666000:65536 myuser01
+
+    Create a container:
+      osctl ct new --user myuser01 --distribution alpine --version 3.7 myct01
+
+    Configure container networking:
+      osctl ct netif new routed --via 10.100.10.100/30 myct01 eth0
+      osctl ct netif ip add myct01 eth0 1.2.3.4/32
+
+    Start the container:
+      osctl ct start myct01
+
+    More information:
+      man osctl
+    '';
 }
