690617217f79e375b5d81979f65b3cb46f964533
Jakub Skokan <jakub.skokan@havefun.cz>
2018-05-31 11:09:52 +0200

    os/default: refactor baseModules

--- os/default.nix
+++ os/default.nix
@@ -27,29 +27,29 @@ let
       ./modules/zfs.nix
       ./modules/version.nix
       ./modules/vpsadmin.nix
-      (nixpkgs + "/nixos/modules/misc/extra-arguments.nix")
-      (nixpkgs + "/nixos/modules/system/etc/etc.nix")
-      (nixpkgs + "/nixos/modules/system/activation/activation-script.nix")
-      (nixpkgs + "/nixos/modules/system/boot/modprobe.nix")
-      (nixpkgs + "/nixos/modules/misc/nixpkgs.nix")
-      (nixpkgs + "/nixos/modules/config/swap.nix")
-      (nixpkgs + "/nixos/modules/config/shells-environment.nix")
-      (nixpkgs + "/nixos/modules/config/system-environment.nix")
-      (nixpkgs + "/nixos/modules/tasks/filesystems.nix")
-#     (nixpkgs + "/nixos/modules/tasks/filesystems/zfs.nix")
+      <nixpkgs/nixos/modules/misc/extra-arguments.nix>
+      <nixpkgs/nixos/modules/system/etc/etc.nix>
+      <nixpkgs/nixos/modules/system/activation/activation-script.nix>
+      <nixpkgs/nixos/modules/system/boot/modprobe.nix>
+      <nixpkgs/nixos/modules/misc/nixpkgs.nix>
+      <nixpkgs/nixos/modules/config/swap.nix>
+      <nixpkgs/nixos/modules/config/shells-environment.nix>
+      <nixpkgs/nixos/modules/config/system-environment.nix>
+      <nixpkgs/nixos/modules/tasks/filesystems.nix>
+#     <nixpkgs/nixos/modules/tasks/filesystems/zfs.nix>
 #                                                  ^ we use custom, minimal zfs.nix implementation
-      (nixpkgs + "/nixos/modules/programs/bash/bash.nix")
-      (nixpkgs + "/nixos/modules/programs/shadow.nix")
-      (nixpkgs + "/nixos/modules/programs/environment.nix")
-      (nixpkgs + "/nixos/modules/security/ca.nix")
-      (nixpkgs + "/nixos/modules/security/apparmor.nix")
-      (nixpkgs + "/nixos/modules/security/pam.nix")
-      (nixpkgs + "/nixos/modules/config/ldap.nix")
-      (nixpkgs + "/nixos/modules/config/nsswitch.nix")
-      (nixpkgs + "/nixos/modules/misc/ids.nix")
-      (nixpkgs + "/nixos/modules/virtualisation/lxc.nix")
-      (nixpkgs + "/nixos/modules/virtualisation/lxcfs.nix")
-      (nixpkgs + "/nixos/modules/services/networking/dhcpd.nix")
+      <nixpkgs/nixos/modules/programs/bash/bash.nix>
+      <nixpkgs/nixos/modules/programs/shadow.nix>
+      <nixpkgs/nixos/modules/programs/environment.nix>
+      <nixpkgs/nixos/modules/security/ca.nix>
+      <nixpkgs/nixos/modules/security/apparmor.nix>
+      <nixpkgs/nixos/modules/security/pam.nix>
+      <nixpkgs/nixos/modules/config/ldap.nix>
+      <nixpkgs/nixos/modules/config/nsswitch.nix>
+      <nixpkgs/nixos/modules/misc/ids.nix>
+      <nixpkgs/nixos/modules/virtualisation/lxc.nix>
+      <nixpkgs/nixos/modules/virtualisation/lxcfs.nix>
+      <nixpkgs/nixos/modules/services/networking/dhcpd.nix>
       <nixpkgs/nixos/modules/services/networking/ssh/sshd.nix>
       <nixpkgs/nixos/modules/system/boot/kernel.nix>
       <nixpkgs/nixos/modules/misc/assertions.nix>
