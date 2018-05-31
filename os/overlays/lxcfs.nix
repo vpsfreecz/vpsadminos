0cdf1ee5dfac7e9a0f9f7cc3118c6a32688a164d
Jakub Skokan <jakub.skokan@havefun.cz>
2018-05-31 10:52:52 +0200

    os: patch lxcfs to search for its library in /run/current-system/sw/lib/lxcfs
    This will allow us to upgrade lxcfs at runtime, without the need of
    rebooting containers.
    
    1. rebuild OS with updated lxcfs
    2. nix copy the system closure
    3. activate the new system
    4. send SIGUSR1 to lxcfs

--- os/overlays/lxcfs.nix
+++ os/overlays/lxcfs.nix
@@ -7,5 +7,10 @@ self: super:
       rev = "2d383c50a997c43167e3b470860a7c655ea8a7a8";
       sha256 = "1nwj2xcvag814sdn92xkg1c0a0wrqi369dzwql8g2f3rmb4x7h0i";
     };
+
+    postFixup = ''
+      # liblxcfs.so is reloaded with dlopen()
+      patchelf --set-rpath "/run/current-system/sw/lib/lxcfs:$(patchelf --print-rpath "$out/bin/lxcfs"):$out/lib" "$out/bin/lxcfs"
+    '';
   });
 }
