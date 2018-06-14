85b94ab0a200856f2cfb3cae1571035835481759
Jakub Skokan <jakub.skokan@havefun.cz>
2018-06-14 14:23:30 +0200

    os: update lxcfs

--- os/overlays/lxcfs.nix
+++ os/overlays/lxcfs.nix
@@ -4,8 +4,8 @@ self: super:
     src = super.fetchFromGitHub {
       owner = "vpsfreecz";
       repo = "lxcfs";
-      rev = "d6a82867e7d4a24a3d432eb1d3e4cc17d01534cc";
-      sha256 = "11x7165cas5s4zddjgnlyjrfx38wb2vqdan4ws5b0yzfn13ddqlj";
+      rev = "9c8f7d6285c3c24ff2cb265d1b06fedfe3b784ac";
+      sha256 = "0wp29nyj40ppzrw48p0ywq0fj438sy6dpxf0cj7z2b9qjwswgy7v";
     };
 
     postFixup = ''
--- os/runit.nix
+++ os/runit.nix
@@ -147,6 +147,13 @@ in
     "service/lxcfs/run".source = pkgs.writeScript "lxcfs" ''
       #!/bin/sh
       mkdir -p /var/lib/lxcfs
+      mkdir -p /var/log
+      
+      exec 1<&-                     # Close STDOUT file descriptor
+      exec 2<&-                     # Close STDERR FD
+      exec 1<>/var/log/lxcfs.output # Open STDOUT as file for read and write
+      exec 2>&1                     # Redirect STDERR to STDOUT
+
       exec ${pkgs.lxcfs}/bin/lxcfs -l /var/lib/lxcfs
     '';
 
