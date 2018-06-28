46cb123edcaa0a59c1b7551600e09c651d958145
Jakub Skokan <jakub.skokan@havefun.cz>
2018-06-28 18:59:57 +0200

    image-scripts: CentOS 6: update to 6.9

--- image-scripts/templates/centos-6.9.sh
+++ image-scripts/templates/centos-6.9.sh
@@ -1,6 +1,6 @@
 DISTNAME=centos
-RELVER=6.6
-RELVERPACK=12.2
+RELVER=6.9
+RELVERPACK=12.3
 RELEASE=http://mirror.hosting90.cz/centos/${RELVER}/os/x86_64/Packages/centos-release-${RELVER//./-}.el6.centos.{$RELVERPACK}.x86_64.rpm
 BASEURL=http://mirror.hosting90.cz/centos/${RELVER}/os/x86_64
 UPDATES=http://mirror.hosting90.cz/centos/${RELVER}/updates/x86_64/
@@ -11,15 +11,10 @@ EXTRAPKGS="openssh-clients man"
 
 bootstrap
 configure-common
-
-cat > $INSTALL/etc/fstab <<SOURCES
-none    /dev/pts        devpts  rw,gid=5,mode=620       0       0
-none    /dev/shm        tmpfs   defaults                0       0
-SOURCES
-
 configure-redhat-common
 
 configure-append <<EOF
+echo > /etc/fstab
 /sbin/chkconfig network on
 /sbin/chkconfig iptables off
 sed -i "s/\[1\-6\]/\[0\-6\]/" /etc/init/start-ttys.conf
