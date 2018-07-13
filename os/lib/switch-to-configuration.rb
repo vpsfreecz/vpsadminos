ed9e98481340b8437dacc2e40665a954932c0291
Jakub Skokan <jakub.skokan@havefun.cz>
2018-07-13 15:42:50 +0200

    os switch: use osup to upgrade/rollback pools

--- os/lib/switch-to-configuration.rb
+++ os/lib/switch-to-configuration.rb
@@ -4,7 +4,8 @@ class Configuration
   OUT = '@out@'
   ETC = '@etc@'
   SERVICE_DIR = '/etc/service'
-  BIN = '/run/current-system/sw/bin'
+  CURRENT_BIN = '/run/current-system/sw/bin'
+  NEW_BIN = File.join(OUT, 'sw', 'bin')
 
   def self.switch
     new(dry_run: false).switch
@@ -21,6 +22,15 @@ class Configuration
   def dry_run
     puts 'probing runit services...'
     services = Services.new(opts)
+
+    puts 'probing pools...'
+    pools = Pools.new(opts)
+    pools.export
+    pools.rollback
+
+    if pools.error.any?
+      puts "unable to handle pools: #{pools.error.map(&:name).join(',')}"
+    end
 
     puts 'would stop deprecated services...'
     services.stop.each(&:stop)
@@ -41,6 +51,15 @@ class Configuration
   def switch
     puts 'probing runit services...'
     services = Services.new(opts)
+
+    puts 'probing pools...'
+    pools = Pools.new(opts)
+    pools.export
+    pools.rollback
+
+    if pools.error.any?
+      puts "unable to handle pools: #{pools.error.map(&:name).join(',')}"
+    end
 
     puts 'stopping deprecated services...'
     services.stop.each(&:stop)
@@ -79,7 +98,7 @@ class Services
         puts "> sv #{m} #{name}"
 
         unless opts[:dry_run]
-          system(File.join(Configuration::BIN, 'sv'), m.to_s, name)
+          system(File.join(Configuration::CURRENT_BIN, 'sv'), m.to_s, name)
         end
       end
     end
@@ -89,7 +108,7 @@ class Services
       puts "> sv #{m} #{name}"
 
       unless opts[:dry_run]
-        system(File.join(Configuration::BIN, 'sv'), m, name)
+        system(File.join(Configuration::CURRENT_BIN, 'sv'), m, name)
       end
     end
 
@@ -155,6 +174,107 @@ class Services
     end
 
     ret
+  end
+end
+
+class Pools
+  Pool = Struct.new(:name, :state, :rollback_version)
+
+  attr_reader :uptodate, :to_upgrade, :to_rollback, :error
+
+  def initialize(dry_run: true)
+    @opts = {dry_run: dry_run}
+
+    @uptodate = []
+    @to_upgrade = []
+    @to_rollback = []
+    @error = []
+
+    @old_pools = check(Configuration::CURRENT_BIN)
+    @new_pools = check(Configuration::NEW_BIN)
+
+    resolve
+  end
+
+  # Rollback pools using the current OS version, as the activated OS version
+  # is older
+  def rollback
+    to_rollback.each do |pool|
+      puts "> rolling back pool #{pool.name}"
+      next if opts[:dry_run]
+
+      ret = system(
+        File.join(Configuration::CURRENT_BIN, 'osup'),
+        'rollback', pool.name, pool.rollback_version
+      )
+
+      unless ret
+        fail "rollback of pool #{pool.name} failed, cannot proceed"
+      end
+    end
+  end
+
+  # Export pools from osctld before upgrade
+  #
+  # This will stop all containers from outdated pools. We're counting on the
+  # fact that if there are new migrations, then osctld has to have changed
+  # as well, so it is restarted by {Services}. After restart, osctld will run
+  # `osup upgrade` on all imported pools.
+  def export
+    (to_rollback + to_upgrade).each do |pool|
+      puts "> exporting pool #{pool.name} to upgrade"
+      next if opts[:dry_run]
+
+      # TODO: do not fail if the pool is not imported
+      ret = system(
+        File.join(Configuration::CURRENT_BIN, 'osctl'),
+        'pool', 'export', '-f', pool.name
+      )
+
+      unless ret
+        fail "export of pool #{pool.name} failed, cannot proceed"
+      end
+    end
+  end
+
+  protected
+  attr_reader :opts, :old_pools, :new_pools
+
+  def resolve
+    new_pools.each do |name, pool|
+      case pool.state
+      when :ok
+        uptodate << pool
+
+      when :outdated
+        to_upgrade << pool
+
+      when :incompatible
+        if old_pools[name] && old_pools[name].state == :ok
+          to_rollback << pool
+
+        else
+          error << pool
+        end
+      end
+    end
+  end
+
+  def check(swbin)
+    ret = {}
+
+    IO.popen("#{File.join(swbin, 'osup')} check") do |io|
+      io.each_line do |line|
+        name, state, version = line.strip.split
+        ret[name] = Pool.new(name, state.to_sym, version)
+      end
+    end
+
+    ret
+
+  rescue Errno::ENOENT
+    # osup isn't available in the to-be-replaced OS version
+    {}
   end
 end
 
