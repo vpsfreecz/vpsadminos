0aa96505724a63668e9071327cfce73cb5c14695
Richard Marko <srk@48.io>
2018-08-05 16:14:49 +0200

    os: add minified logrotate

--- os/overlays/minify.nix
+++ os/overlays/minify.nix
@@ -4,4 +4,5 @@ self: super:
   gnupg = super.gnupg.override { guiSupport = false; libusb = null; pinentry = null; openldap = null; };
   utillinux = super.utillinux.override { systemd = null; };
   dhcpcd = super.dhcpcd.override { udev = null; };
+  logrotate = super.logrotate.override { mailutils = null; };
 }
