e50a33c67e213de0c5e151f67fc72a6339ec76d7
Jakub Skokan <jakub.skokan@havefun.cz>
2018-08-27 22:47:02 +0200

    os: lxcfs: log using svlogd

--- os/modules/lxcfs.nix
+++ os/modules/lxcfs.nix
@@ -11,16 +11,14 @@ with lib;
   ###### implementation
 
   config = {
-   runit.services.lxcfs.run = ''
-     mkdir -p /var/lib/lxcfs
-     mkdir -p /var/log
+    runit.services.lxcfs = {
+      run = ''
+        mkdir -p /var/lib/lxcfs
+        exec ${pkgs.lxcfs}/bin/lxcfs -l /var/lib/lxcfs
+      '';
 
-     exec 1<&-                     # Close STDOUT file descriptor
-     exec 2<&-                     # Close STDERR FD
-     exec 1<>/var/log/lxcfs.output # Open STDOUT as file for read and write
-     exec 2>&1                     # Redirect STDERR to STDOUT
-
-     exec ${pkgs.lxcfs}/bin/lxcfs -l /var/lib/lxcfs
-   '';
+      log.enable = true;
+      log.sendTo = "127.0.0.1";
+    };
   };
 }
