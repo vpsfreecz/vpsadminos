c1ee915439c7686897d7516fa76966b9125ef1fe
Jakub Skokan <jakub.skokan@havefun.cz>
2018-08-27 22:46:00 +0200

    os: runit: add support for svlogd

--- os/modules/runit.nix
+++ os/modules/runit.nix
@@ -20,6 +20,98 @@ let
         default = "";
         description = "Called to check service status.";
       };
+
+      log = {
+        enable = mkEnableOption "Start svlogd for the service.";
+
+        logStandardError = mkOption {
+          type = types.bool;
+          default = true;
+          description = "Log messages the service writes to stderr.";
+        };
+
+        run = mkOption {
+          type = types.str;
+          default = "";
+          description = "Called to start log service.";
+        };
+
+        maxFileSize = mkOption {
+          type = types.ints.unsigned;
+          default = 1000000;
+          description = ''
+            Sets the maximum file size of current when svlogd should rotate
+            the current log file to size bytes. Default is 1000000. If fileSize
+            is zero, svlogd doesn’t rotate log files.
+          '';
+        };
+
+        logFiles = mkOption {
+          type = types.ints.unsigned;
+          default = 10;
+          description = ''
+            Sets the number of old log files svlogd should maintain.
+            If svlogd sees more old log files in log after log file rotation,
+            it deletes the oldest one. Default is 10. If set to zero, svlogd
+            doesn’t remove old log files.
+          '';
+        };
+
+        minLogFiles = mkOption {
+          type = types.ints.unsigned;
+          default = 0;
+          description = ''
+            Sets the minimum number of old log files svlogd should maintain.
+            It must be less than logFiles. If it is set, and svlogd cannot
+            write to current because the filesystem is full, and it sees more
+            than minLogFiles old log files, it deletes the oldest one.
+          '';
+        };
+
+        timeout = mkOption {
+          type = types.ints.unsigned;
+          default = 0;
+          description = ''
+            Sets the maximum age of the current log file when svlogd should
+            rotate the current log file to timeout seconds. If current is
+            timeout seconds old, and is not empty, svlogd forces log file
+            rotation.
+          '';
+        };
+
+        sendTo = mkOption {
+          type = types.str;
+          default = "";
+          example = "a.b.c.d[:port]";
+          description = ''
+            Tells svlogd to transmit the first len characters of selected log
+            messages to the IP address a.b.c.d, port number port. If port
+            isn’t set, the default port for syslog is used (514). len can be
+            set through the -l option, see below. If svlogd has trouble
+            sending udp packets, it writes error messages to the log directory.
+
+            Attention: logging through udp is unreliable, and should be used
+            in private networks only.
+          '';
+        };
+
+        sendOnly = mkOption {
+          type = types.bool;
+          default = false;
+          description = ''
+            Send messages only via UDP, don't store them in the log directory.
+          '';
+        };
+
+        linePrefix = mkOption {
+          type = types.str;
+          default = "";
+          description = ''
+            Tells svlogd to prefix each line to be written to the log
+            directory, to standard error, or through UDP.
+          '';
+        };
+      };
     };
   };
 
@@ -33,6 +125,30 @@ let
 
   mkService = name: type: script: mkScript "sv-${name}-${type}" script;
 
+  mkServiceRun = name: service:
+    mkService name "run" ''
+      ${optionalString (service.log.enable && service.log.logStandardError) "exec 2>&1"}
+      ${service.run}
+    '';
+
+  mkLogRun = name: service:
+    if service.log.run == "" then ''
+      mkdir -p /var/log/${name}
+      chown log /var/log/${name}
+      ln -sf ${mkLogConfig name service} /var/log/${name}/config
+      exec chpst -ulog svlogd -tt /var/log/${name}
+    '' else service.log.run;
+
+  mkLogConfig = name: service:
+    pkgs.writeText "sv-${name}-log-config" ''
+      s${toString service.log.maxFileSize}
+      n${toString service.log.logFiles}
+      ${optionalString (service.log.minLogFiles > 0) "N${toString service.log.minLogFiles}"}
+      ${optionalString (service.log.timeout > 0) "t${toString service.log.timeout}"}
+      ${optionalString (service.log.sendTo != "") "${if service.log.sendOnly then "U" else "u"}${service.log.sendTo}"}
+      ${optionalString (service.log.linePrefix != "") "p${service.log.linePrefix}"}
+    '';
+
   runlevels = lib.unique (lib.flatten (mapAttrsToList (name: service: service.runlevels) config.runit.services));
 
   runlevelServices = rv: lib.remove null (mapAttrsToList (name: service:
@@ -45,11 +161,15 @@ let
   mkServices = mkMerge (mapAttrsToList (name: service:
     mkMerge [
       {
-        "runit/services/${name}/run".source = mkService name "run" service.run;
+        "runit/services/${name}/run".source = mkServiceRun name service;
       }
 
       (mkIf (service.check != "") {
         "runit/services/${name}/check".source = mkService name "check" service.check;
+      })
+
+      (mkIf (service.log.enable) {
+        "runit/services/${name}/log/run".source = mkService name "log-run" (mkLogRun name service);
       })
     ]
   ) config.runit.services);
@@ -117,6 +237,10 @@ in
   ### Implementation
   config = mkMerge [
     {
+      users.extraUsers.log = {
+        isSystemUser = true;
+      };
+
       environment.etc = {
         "runit/1".source = mkStage "1" config.runit.stage1;
         "runit/2".source = mkStage "2" config.runit.stage2;
