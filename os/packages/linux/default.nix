8851b6fd67c10b15f0a107317b95e6879969a721
Richard Marko <srk@48.io>
2018-09-03 12:18:02 +0200

    os: bump kernel to 4.18

--- os/packages/linux/default.nix
+++ os/packages/linux/default.nix
@@ -2,15 +2,14 @@
 let
   kernelPatches = pkgs.kernelPatches;
 in
-  pkgs.callPackage <nixpkgs/pkgs/os-specific/linux/kernel/linux-4.17.nix> {
+  pkgs.callPackage <nixpkgs/pkgs/os-specific/linux/kernel/linux-4.18.nix> {
     kernelPatches =
       [ kernelPatches.bridge_stp_helper
         # See pkgs/os-specific/linux/kernel/cpu-cgroup-v2-patches/README.md
         # when adding a new linux version
         # kernelPatches.cpu-cgroup-v2."4.11"
         kernelPatches.modinst_arg_list_too_long
-        kernelPatches.bcm2835_mmal_v4l2_camera_driver # Only needed for 4.16!
         { name = "sched_getaffinity_cfs_quota";
           patch = ./sched_getaffinity_cfs_quota.patch; }
-      ];    
+      ];
   }
