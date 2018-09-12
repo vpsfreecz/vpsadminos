67ce2987e2b6e0f5bd7d143731f577b07ceaec77
Jakub Skokan <jakub.skokan@havefun.cz>
2018-09-12 14:54:09 +0200

    os: containers: update examples

--- os/configs/containers/features.nix
+++ os/configs/containers/features.nix
@@ -3,94 +3,113 @@
   # example of declarative containers
   # covering most features of osctl
 
-  containers = let
-    sampleUser = { name = "sample"; ugid = 5000; offset = 666000; size = 65536; };
-    largeUser = { name = "large";  ugid = 5001; offset = 900000; size = 1048576; };
-
-    memLimit = { name = "memory.limit_in_bytes"; value = "10G"; subsystem = "memory"; };
-    cpuShares = { name = "cpu.shares"; value = "768"; subsystem = "cpu"; };
-    limitedGroup = { name = "/limited"; cgparams = [ cpuShares memLimit ]; };
-
-    fuse = { name = "/dev/fuse"; type = "char"; major = 10; minor = 229; mode = "rw"; };
-    deviceGroup = { name = "/devices"; devices = [ fuse ]; };
-
-    ifbr = {
-      name = "eth0";
-      type = "bridge";
-      link = "lxcbr0";
-      ipv4.addresses = [ { address = "192.168.1.2"; prefixLength = 24; } ];
+  osctl.pools.tank = {
+    users.sampleuser = let mapping = [ "0:666000:65536" ]; in {
+      ugid = 5000;
+      uidMap = mapping;
+      gidMap = mapping;
     };
 
-    ifrt = {
-      name = "eth1";
-      type = "routed";
-      ipv4 =  {
-        via = { address = "172.17.77.76"; prefixLength=30; };
-        addresses = [ { address = "172.17.66.66"; prefixLength = 32; } ];
-      };
-      ipv6 = {
-        via = { address = "2a03:3b40:7:666::"; prefixLength=64; };
-        addresses = [ { address = "2a03:3b40:7:667::1"; prefixLength=64; } ];
-      };
+    users.largeuser = let mapping = [ "0:900000:1048576" ]; in {
+      ugid = 5001;
+      uidMap = mapping;
+      gidMap = mapping;
     };
 
-    filelim = { name = "nofile"; soft = 1024; hard = 4096; };
+    groups."/" = {
+      devices = (import <vpsadminos/os/modules/osctl/standard-devices.nix>) ++ [
+        {
+          name = "/dev/fuse";
+          type = "char";
+          major = "10";
+          minor = "229";
+          mode = "rwm";
+        }
+      ];
+    };
 
-    resolv = "1.1.1.1";
+    groups."/limited" = {
+      cgparams = [
+        { name = "memory.limit_in_bytes"; value = "10G"; }
+        { name = "cpu.shares"; value = "768"; }
+      ];
+    };
 
-    mount = { fs = "/var/share"; mountpoint = "/mnt"; };
-  in
-  {
-    defaults =
-      { config =
-          { config, pkgs, ... }:
-          {
-            # not much here
+    containers =
+      let
+        ifbr = {
+          name = "eth0";
+          type = "bridge";
+          link = "lxcbr0";
+          ipv4.addresses = [ { address = "192.168.1.2"; prefixLength = 24; } ];
+        };
+
+        ifrt = {
+          name = "eth1";
+          type = "routed";
+          ipv4 =  {
+            via = { address = "172.17.77.76"; prefixLength=30; };
+            addresses = [ { address = "172.17.66.66"; prefixLength = 32; } ];
           };
-        pool = "tank";
-        user = sampleUser;
-        autostart.enable = true;
-      };
-
-    webserver =
-      { config =
-          { config, pkgs, ... }:
-          {
-            services.nginx.enable = true;
-            services.openssh.enable = true;
-            networking.firewall.allowedTCPPorts = [ 80 ];
+          ipv6 = {
+            via = { address = "2a03:3b40:7:666::"; prefixLength=64; };
+            addresses = [ { address = "2a03:3b40:7:667::1"; prefixLength=64; } ];
           };
-        pool = "tank";
-        user = sampleUser;
-        group = deviceGroup;
-        interfaces = [ ifbr ];
-        resolvers = [ resolv ];
-        autostart.enable = true;
-      };
+        };
 
-    proxy =
-      { config =
-          { config, pkgs, ... }:
-          {
-            services.nginx.enable = true;
-          };
-        pool = "tank";
-        user = largeUser;
-        group = limitedGroup;
-        nesting = true;
-        interfaces = [ ifrt ];
-        prlimits = [ filelim ];
-      };
+        filelim = { name = "nofile"; soft = 1024; hard = 4096; };
 
-    hasmounts =
-      { config =
-          { config, pkgs, ... }:
-          {
-            # not much
+        resolv = "1.1.1.1";
+
+        mount = { fs = "/var/share"; mountpoint = "/mnt"; };
+      in {
+        defaults =
+          { config =
+              { config, pkgs, ... }:
+              {
+                # not much here
+              };
+            user = "sampleuser";
+            autostart.enable = true;
           };
-        pool = "tank";
-        user = largeUser;
-        mounts = [ mount ];
+
+        webserver =
+          { config =
+              { config, pkgs, ... }:
+              {
+                services.nginx.enable = true;
+                services.openssh.enable = true;
+                networking.firewall.allowedTCPPorts = [ 80 ];
+              };
+            user = "sampleuser";
+            group = "/default";
+            interfaces = [ ifbr ];
+            resolvers = [ resolv ];
+            autostart.enable = true;
+          };
+
+        proxy =
+          { config =
+              { config, pkgs, ... }:
+              {
+                services.nginx.enable = true;
+              };
+            user = "largeuser";
+            group = "/limited";
+            nesting = true;
+            interfaces = [ ifrt ];
+            prlimits = [ filelim ];
+          };
+
+        hasmounts =
+          { config =
+              { config, pkgs, ... }:
+              {
+                # not much
+              };
+            user = "largeuser";
+            mounts = [ mount ];
+          };
       };
   };
 }
--- os/configs/containers/webserver.nix
+++ os/configs/containers/webserver.nix
@@ -2,47 +2,42 @@
 {
   # example of a webserver using declarative containers
 
-  containers = let
-    sampleUser = {
-      name = "sample";
+  osctl.pools.tank = {
+    users.sampleuser = let mapping = [ "0:666000:65536" ]; in {
       ugid = 5000;
-      offset = 666000;
-      size = 65536;
+      uidMap = mapping;
+      gidMap = mapping;
     };
 
-    ifbr = {
-      name = "eth0";
-      type = "bridge";
-      link = "lxcbr0";
-    };
-
-    demoRoot = pkgs.runCommand "makeDemo" {} ''
-      mkdir $out
-      echo 'Hello from NixOS running on vpsAdminOS!' > $out/index.html
-    '';
-  in
-  {
-    webserver =
-      { config =
-          { config, pkgs, ... }:
-          {
-            services.nginx = {
-              enable = true;
-              virtualHosts = {
-                "demo.example.org" = {
-                  root = demoRoot;
-                  default = true;
-                };
+    containers.webserver = {
+      config =
+        { config, pkgs, ... }:
+        {
+          services.nginx = {
+            enable = true;
+            virtualHosts = {
+              "demo.example.org" = {
+                root = demoRoot;
+                default = true;
               };
             };
-
-            services.openssh.enable = true;
-            networking.firewall.allowedTCPPorts = [ 80 ];
           };
-        pool = "tank";
-        user = sampleUser;
-        interfaces = [ ifbr ];
-        autostart.enable = true;
-      };
+
+          services.openssh.enable = true;
+          networking.firewall.allowedTCPPorts = [ 80 ];
+        };
+
+      user = "sampleuser";
+
+      interfaces = [
+        {
+          name = "eth0";
+          type = "bridge";
+          link = "lxcbr0";
+        }
+      ];
+
+      autostart.enable = true;
+    };
   };
 }
