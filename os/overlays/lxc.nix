0a523620cdc6c84d8443b601ca88a638ffcf37cd
Jakub Skokan <jakub.skokan@havefun.cz>
2018-10-28 18:01:36 +0100

    os: lxc: use statically linked libcap to build init.lxc.static
    init.lxc.static is necessary for lxc-execute to work properly.

--- os/overlays/lxc.nix
+++ os/overlays/lxc.nix
@@ -1,11 +1,25 @@
 self: super:
 {
-  lxc = super.lxc.overrideAttrs (oldAttrs: rec {
-    src = super.fetchFromGitHub {
-      owner = "vpsfreecz";
-      repo = "lxc";
-      rev = "44b80445dd8c6b8218afb62c56f752b418a858e8";
-      sha256 = "19jkqmnr4ss5jmjdk7dr05bavd0d7jjimzlhfizr93wz7glzwp20";
-    };
-  });
+  lxc =
+    let
+      libcap = super.libcap.overrideAttrs (oldAttrs: rec {
+        postInstall = builtins.replaceStrings [ ''rm "$lib"/lib/*.a'' ] [ "" ]
+                                              oldAttrs.postInstall;
+      });
+
+      lxc = super.callPackage <nixpkgs/pkgs/os-specific/linux/lxc/default.nix> {
+        inherit libcap;
+      };
+
+    in
+      lxc.overrideAttrs (oldAttrs: rec {
+        src = super.fetchFromGitHub {
+          owner = "vpsfreecz";
+          repo = "lxc";
+          rev = "44b80445dd8c6b8218afb62c56f752b418a858e8";
+          sha256 = "19jkqmnr4ss5jmjdk7dr05bavd0d7jjimzlhfizr93wz7glzwp20";
+        };
+
+        buildInputs = oldAttrs.buildInputs ++ [ super.glibc super.glibc.static ];
+      });
 }
