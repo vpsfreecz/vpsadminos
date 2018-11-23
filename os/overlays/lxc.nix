be7692529fe96022b5a128e5d3817e6e1ba71a2e
Jakub Skokan <jakub.skokan@havefun.cz>
2018-11-23 09:23:54 +0100

    os: update lxc to 3.0.3

--- os/overlays/lxc.nix
+++ os/overlays/lxc.nix
@@ -16,8 +16,8 @@ self: super:
         src = super.fetchFromGitHub {
           owner = "vpsfreecz";
           repo = "lxc";
-          rev = "44b80445dd8c6b8218afb62c56f752b418a858e8";
-          sha256 = "19jkqmnr4ss5jmjdk7dr05bavd0d7jjimzlhfizr93wz7glzwp20";
+          rev = "2063f5387178d29085f946d1eaf99346352c9fa3";
+          sha256 = "1nl6an18rg6m21sl8ajn7xq9b0g82v7w8wg95wh2yjpzq9kj50vl";
         };
 
         buildInputs = oldAttrs.buildInputs ++ [ super.glibc super.glibc.static ];
