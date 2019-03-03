5a248f21aff612ec3bca66d49454c5738ef53f53
Jakub Skokan <jakub.skokan@havefun.cz>
2019-03-03 12:16:24 +0100

    os: update mariadb connector/c to 3.0.9

--- os/overlays/gem-config.nix
+++ os/overlays/gem-config.nix
@@ -1,8 +1,20 @@
 self: super:
+let
+  mariadb-connector-c = super.mysql.connector-c.overrideAttrs (oldAttrs: rec {
+    name = "mariadb-connector-c-${version}";
+    version = "3.0.9";
+
+    src = super.fetchurl {
+      url = "https://downloads.mariadb.org/interstitial/connector-c-${version}/mariadb-connector-c-${version}-src.tar.gz/from/http%3A//nyc2.mirrors.digitalocean.com/mariadb/";
+      sha256 = "0hc6hnkkdkwdkgh017mgwvi66aln1fpjns0xgv8b2l3gpb5c0xvj";
+      name   = "mariadb-connector-c-${version}-src.tar.gz";
+    };
+  });
+in
 {
   defaultGemConfig =
     super.callPackage (
-      { lib, apparmor-parser, ncurses }:
+      { lib, apparmor-parser, ncurses, openssl, zlib }:
 
       lib.mergeAttrs super.defaultGemConfig {
         curses = attrs: {
@@ -15,6 +27,10 @@ self: super:
 
         osctld = attrs: {
           buildInputs = [ apparmor-parser ];
+        };
+
+        mysql2 = attrs: {
+          buildInputs = [ mariadb-connector-c zlib openssl ];
         };
       }) {};
 }
