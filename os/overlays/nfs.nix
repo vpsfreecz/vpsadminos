ed700ab04a66bf62498f6ae6b26a5e9c020776ff
Jakub Skokan <jakub.skokan@havefun.cz>
2019-04-02 15:07:07 +0200

    os: annotate patches

--- os/overlays/nfs.nix
+++ os/overlays/nfs.nix
@@ -2,6 +2,7 @@ self: super:
 {
   nfs-utils = super.nfs-utils.overrideAttrs (oldAttrs: rec {
     patches = oldAttrs.patches ++ [
+      # Userspace counterpart to the patch introducting export option root_uid
       ../packages/nfs-utils/patches/000-export_option_root_uid.patch
     ];
   });
