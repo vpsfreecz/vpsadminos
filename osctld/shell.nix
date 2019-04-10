b23aa42d3085ce709aa5ac2b4af1588562386637
Jakub Skokan <jakub.skokan@havefun.cz>
2019-04-10 16:42:24 +0200

    osctld/shell.nix: add dependencies to path as in the runit service

--- osctld/shell.nix
+++ osctld/shell.nix
@@ -1,26 +1,42 @@
 let
   pkgs = import <nixpkgs> { overlays = (import ../os/overlays/common.nix); };
+  lib = pkgs.lib;
   stdenv = pkgs.stdenv;
-  apparmor_paths = pkgs.lib.concatMapStringsSep ":" (s: "${s}/etc/apparmor.d")
-          [ pkgs.apparmor-profiles pkgs.lxc ];
+  path = with pkgs; [
+    apparmor-parser
+    coreutils
+    iproute
+    glibc.bin
+    gzip
+    lxc
+    nettools
+    gnutar
+    openssh
+    pty-wrapper
+    shadow
+    utillinux
+    zfs
+  ];
+  pathJoined = lib.concatMapStringsSep ":" (s: "${s}/bin") path;
+  apparmorPaths = [ pkgs.apparmor-profiles ];
+  apparmorPathsJoined = lib.concatMapStringsSep ":" (s: "${s}/etc/apparmor.d") apparmorPaths;
 
 in stdenv.mkDerivation rec {
   name = "osctld";
 
   buildInputs = [
     pkgs.ruby
-    pkgs.lxc
     pkgs.git
+    pkgs.lxc
     pkgs.zlib
     pkgs.openssl
-    pkgs.apparmor-parser
   ];
 
   shellHook = ''
     mkdir -p /tmp/dev-ruby-gems
     export GEM_HOME="/tmp/dev-ruby-gems"
     export GEM_PATH="$GEM_HOME:$PWD/lib"
-    export PATH="$GEM_HOME/bin:$PATH"
+    export PATH="$GEM_HOME/bin:$PATH:${pathJoined}"
 
     BUNDLE="$GEM_HOME/bin/bundle"
 
@@ -33,7 +49,7 @@ in stdenv.mkDerivation rec {
 
     export RUBYOPT=-rbundler/setup
 
-    export OSCTLD_APPARMOR_PATHS="${apparmor_paths}"
+    export OSCTLD_APPARMOR_PATHS="${apparmorPathsJoined}"
 
     # Suids
     chmod 04755 ${pkgs.lxc}/libexec/lxc/lxc-user-nic
