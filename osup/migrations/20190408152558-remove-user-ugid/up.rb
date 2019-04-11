3933aa618abe2cae0782e311be724b8e64abd444
Jakub Skokan <jakub.skokan@havefun.cz>
2019-04-11 15:28:16 +0200

    osup: remove-user-ugid: remember original user ugids

--- osup/migrations/20190408152558-remove-user-ugid/up.rb
+++ osup/migrations/20190408152558-remove-user-ugid/up.rb
@@ -3,10 +3,26 @@ require 'libosctl'
 include OsCtl::Lib::Utils::Log
 include OsCtl::Lib::Utils::System
 
+zfs(:create, '-p', File.join($POOL, 'migration'))
+
+mig_dir = File.join(
+  zfs(:get, '-Hp -o value mountpoint', File.join($POOL, 'migration'))[:output].strip,
+  $MIGRATION_ID.to_s
+)
+Dir.mkdir(mig_dir)
+
 conf_dir = zfs(:get, '-Hp -o value mountpoint', File.join($POOL, 'conf'))[:output].strip
+ugid_map = {}
 
 Dir.glob(File.join(conf_dir, 'user', '*.yml')).each do |f|
+  name = File.basename(f)[0..(('.yml'.length+1) * -1)]
   cfg = YAML.load_file(f)
+  ugid_map[name] = cfg['ugid']
   cfg.delete('ugid')
   File.write(f, YAML.dump(cfg))
 end
+
+File.write(
+  File.join(mig_dir, 'user_ugids.yml'),
+  YAML.dump(ugid_map)
+)
