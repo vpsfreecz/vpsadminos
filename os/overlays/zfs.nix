a564c638b5606fbde224bd500d1766a5dc6d0dea
Jakub Skokan <jakub.skokan@havefun.cz>
2019-05-15 16:00:49 +0200

    os: update zfs to 0.8.0-rc5

--- os/overlays/zfs.nix
+++ os/overlays/zfs.nix
@@ -2,12 +2,12 @@ self: super:
 {
   zfs = super.zfsUnstable.overrideAttrs (oldAttrs: rec {
     name = "zfs-${version}";
-    version = "0.8.0-rc4.vpsfree";
+    version = "0.8.0-rc5.vpsfree";
     src = super.fetchFromGitHub {
       owner = "vpsfreecz";
       repo = "zfs";
-      rev = "8f776f88e8a21e59fa668ae9768b93b0e9f6e1b1";
-      sha256 = "132p3xdg9f0dflygfyfndqqavl6pi78sakm0r5zi72wnlx1nnwbv";
+      rev = "5bb0af8289de1a1e660bb1bd6263cd910eceac63";
+      sha256 = "0wgwxhkf9di67jd5hyj2yylb9016iv90hlqb5xsimn0amfbdm15q";
     };
   });
 }
