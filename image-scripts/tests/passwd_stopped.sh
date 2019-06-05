0c4739c0fcd43ecc9badec1c265c148516ebe023
Jakub Skokan <jakub.skokan@havefun.cz>
2019-06-05 12:19:19 +0200

    image-scripts: test/passwd_stopped: handle missing /etc on nixos

--- image-scripts/tests/passwd_stopped.sh
+++ image-scripts/tests/passwd_stopped.sh
@@ -1,1 +1,7 @@
+# NixOS does not have /etc until the first start
+if [ "$DISTNAME" == "nixos" ] ; then
+	osctl ct start $CTID || fail "unable to start"
+	sleep 10
+	osctl ct stop $CTID || fail "unable to stop"
+fi
 osctl ct passwd $CTID root secret || fail "unable to set password"
