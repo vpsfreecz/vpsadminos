1bfb625a23b3cdc2dcdf1b5b7faed46ebc4183de
Jakub Skokan <jakub.skokan@havefun.cz>
2019-06-05 14:19:04 +0200

    image-scripts: tests/stop: give debian 8 more time to start

--- image-scripts/tests/stop.sh
+++ image-scripts/tests/stop.sh
@@ -1,3 +1,10 @@
 osctl ct start $CTID || fail "unable to start container"
-sleep 10 # give the system some time to complete boot
+
+# Give the system some time to complete boot
+if [ "$DISTNAME" == "debian" ] && [ "$RELVER" == "8" ] ; then
+	sleep 30
+else
+	sleep 10
+fi
+
 osctl ct stop --dont-kill $CTID || fail "unable to stop container"
