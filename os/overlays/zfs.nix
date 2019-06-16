d7f8165507df3734ede091dab24373bf65890ff2
Jakub Skokan <jakub.skokan@havefun.cz>
2019-06-16 11:50:26 +0200

    os: update zfs to 0.8.1 with recursive zfs share

--- os/overlays/zfs.nix
+++ os/overlays/zfs.nix
@@ -2,12 +2,12 @@ self: super:
 {
   zfs = super.zfsUnstable.overrideAttrs (oldAttrs: rec {
     name = "zfs-${version}";
-    version = "0.8.0.vpsadminos";
+    version = "0.8.1.vpsadminos";
     src = super.fetchFromGitHub {
       owner = "vpsfreecz";
       repo = "zfs";
-      rev = "d22d5bea8a1b4b25d4515b10297f02de3678ca95";
-      sha256 = "1afgdc5h3xwxhrmb19ai0rk0mb8y7ivbgpiqkcmflmbhvcc2bvys";
+      rev = "c9e6782ae63b7f9b50c1d8c96eb09a1469ad7cbf";
+      sha256 = "15nswnhjwlk73a9cbrv7zk59xrnzsb3ziihpvvm9iyj1z4vc4yks";
     };
   });
 }
