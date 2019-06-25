1a6c608a9086fa77cffc1bbed5b6d3e47a55063b
Jakub Skokan <jakub.skokan@havefun.cz>
2019-06-25 13:59:36 +0200

    osctl ct send: acquire manipulation lock when sending at once

--- osctld/lib/osctld/commands/container/send_now.rb
+++ osctld/lib/osctld/commands/container/send_now.rb
@@ -8,39 +8,41 @@ module OsCtld
       ct = DB::Containers.find(opts[:id], opts[:pool])
       error!('container not found') unless ct
 
-      progress(type: :step, title: 'Sending config')
-      call_cmd!(
-        Commands::Container::SendConfig,
-        id: ct.id,
-        pool: ct.pool.name,
-        dst: opts[:dst],
-        port: opts[:port],
-        as_id: opts[:as_id],
-      )
+      manipulate(ct) do
+        progress(type: :step, title: 'Sending config')
+        call_cmd!(
+          Commands::Container::SendConfig,
+          id: ct.id,
+          pool: ct.pool.name,
+          dst: opts[:dst],
+          port: opts[:port],
+          as_id: opts[:as_id],
+        )
 
-      progress(type: :step, title: 'Sending rootfs')
-      call_cmd!(
-        Commands::Container::SendRootfs,
-        id: ct.id,
-        pool: ct.pool.name
-      )
+        progress(type: :step, title: 'Sending rootfs')
+        call_cmd!(
+          Commands::Container::SendRootfs,
+          id: ct.id,
+          pool: ct.pool.name
+        )
 
-      progress(type: :step, title: 'Sending state')
-      call_cmd!(
-        Commands::Container::SendState,
-        id: ct.id,
-        pool: ct.pool.name,
-        clone: opts[:clone],
-        restart: opts[:restart],
-        start: opts[:start],
-      )
+        progress(type: :step, title: 'Sending state')
+        call_cmd!(
+          Commands::Container::SendState,
+          id: ct.id,
+          pool: ct.pool.name,
+          clone: opts[:clone],
+          restart: opts[:restart],
+          start: opts[:start],
+        )
 
-      progress(type: :step, title: 'Cleaning up')
-      call_cmd!(
-        Commands::Container::SendCleanup,
-        id: ct.id,
-        pool: ct.pool.name,
-      )
+        progress(type: :step, title: 'Cleaning up')
+        call_cmd!(
+          Commands::Container::SendCleanup,
+          id: ct.id,
+          pool: ct.pool.name,
+        )
+      end
     end
   end
 end
