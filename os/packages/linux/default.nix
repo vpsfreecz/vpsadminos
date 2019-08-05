b92b772bfc831d89bf762dd9d999d03a9ce3c99e
Jakub Skokan <jakub.skokan@havefun.cz>
2019-08-05 17:19:35 +0200

    os: enable aufs in kernel

--- os/packages/linux/default.nix
+++ os/packages/linux/default.nix
@@ -20,6 +20,23 @@ in
             MEMCG_32BIT_IDS y
             CGROUP_CGLIMIT y
             SYSLOG_NS y
+            AUFS_FS y
+            AUFS_BRANCH_MAX_127 y
+            AUFS_SBILIST y
+            AUFS_HNOTIFY y
+            AUFS_HFSNOTIFY y
+            AUFS_EXPORT y
+            AUFS_INO_T_64 y
+            AUFS_XATTR y
+            AUFS_FHSM y
+            AUFS_RDU y
+            AUFS_DIRREN y
+            AUFS_SHWH y
+            AUFS_BR_RAMFS y
+            AUFS_BR_FUSE y
+            AUFS_POLL y
+            AUFS_BR_HFSPLUS y
+            AUFS_BDEV_LOOP y
           '';
         }
 
