66fa82aac5b117b0e5e42a2557011ac911ba0658
Jakub Skokan <jakub.skokan@havefun.cz>
2019-09-03 15:09:22 +0200

    os: refactor module lists to standalone files

--- os/default.nix
+++ os/default.nix
@@ -19,49 +19,7 @@ let
       nixpkgs.overlays = import ./overlays { lib = pkgs_.lib; inherit vpsadmin; };
     };
   };
-  baseModules = [
-      <nixpkgs/nixos/modules/misc/extra-arguments.nix>
-      <nixpkgs/nixos/modules/system/etc/etc.nix>
-      <nixpkgs/nixos/modules/system/activation/activation-script.nix>
-      <nixpkgs/nixos/modules/system/boot/modprobe.nix>
-      <nixpkgs/nixos/modules/system/boot/loader/efi.nix>
-      <nixpkgs/nixos/modules/system/boot/loader/generations-dir/generations-dir.nix>
-      <nixpkgs/nixos/modules/system/boot/loader/loader.nix>
-      <nixpkgs/nixos/modules/system/boot/luksroot.nix>
-      <nixpkgs/nixos/modules/system/boot/initrd-network.nix>
-      <nixpkgs/nixos/modules/system/boot/initrd-ssh.nix>
-      <nixpkgs/nixos/modules/misc/nixpkgs.nix>
-      <nixpkgs/nixos/modules/config/swap.nix>
-      <nixpkgs/nixos/modules/config/shells-environment.nix>
-      <nixpkgs/nixos/modules/config/system-environment.nix>
-      <nixpkgs/nixos/modules/config/timezone.nix>
-      <nixpkgs/nixos/modules/tasks/filesystems.nix>
-#     <nixpkgs/nixos/modules/tasks/filesystems/zfs.nix>
-#                                                  ^ we use custom, minimal zfs.nix implementation
-      <nixpkgs/nixos/modules/programs/bash/bash.nix>
-      <nixpkgs/nixos/modules/programs/shadow.nix>
-      <nixpkgs/nixos/modules/programs/environment.nix>
-      <nixpkgs/nixos/modules/security/ca.nix>
-      <nixpkgs/nixos/modules/security/apparmor.nix>
-      <nixpkgs/nixos/modules/security/pam.nix>
-      <nixpkgs/nixos/modules/security/sudo.nix>
-      <nixpkgs/nixos/modules/security/wrappers/default.nix>
-      <nixpkgs/nixos/modules/config/ldap.nix>
-      <nixpkgs/nixos/modules/config/nsswitch.nix>
-      <nixpkgs/nixos/modules/misc/ids.nix>
-      <nixpkgs/nixos/modules/virtualisation/lxc.nix>
-      <nixpkgs/nixos/modules/virtualisation/lxcfs.nix>
-      <nixpkgs/nixos/modules/services/misc/nix-daemon.nix>
-      <nixpkgs/nixos/modules/services/networking/dhcpd.nix>
-      <nixpkgs/nixos/modules/services/networking/ssh/sshd.nix>
-      <nixpkgs/nixos/modules/system/boot/kernel.nix>
-      <nixpkgs/nixos/modules/misc/assertions.nix>
-      <nixpkgs/nixos/modules/misc/lib.nix>
-      <nixpkgs/nixos/modules/config/sysctl.nix>
-      <nixpkgs/nixos/modules/config/users-groups.nix>
-      <nixpkgs/nixos/modules/config/i18n.nix>
-      pkgsModule
-  ] ++ (import ./modules/module-list.nix);
+  baseModules = import ./modules/module-list.nix;
   evalConfig = modulesArgs: pkgs_.lib.evalModules {
     prefix = [];
     check = true;
