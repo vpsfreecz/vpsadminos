1b5e6bba34b48427a901b69c14a7effed957d4d9
Jakub Skokan <jakub.skokan@havefun.cz>
2019-09-04 09:24:24 +0200

    os: sysfs: allow access to pci devices
    Required by libvirtd.

--- os/modules/system/boot/proc-sysfs.nix
+++ os/modules/system/boot/proc-sysfs.nix
@@ -1,18 +1,24 @@
 {
   "/proc/bus" = false;
   "/sys/block" = false;
-  "/sys/bus" = false;
+  "/sys/bus" = {
+    subdirs = {
+      "pci" = true;
+    };
+  };
   "/sys/class" = {
     subdirs = {
       "mem" = true;
       "misc" = true;
       "net" = true;
+      "pci_bus" = true;
       "tty" = true;
     };
   };
   "/sys/dev/block" = true;
   "/sys/devices" = {
     subdirs = {
+      "pci*" = true;
       "/system" = {
         subdirs = {
           "/cpu" = true;
