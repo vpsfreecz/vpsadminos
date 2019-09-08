8eb8a7003bcab1bfd1853b04097a5afa17895edb
Jakub Skokan <jakub.skokan@havefun.cz>
2019-09-08 10:45:21 +0200

    os: upgrade kernel to 5.3

--- os/packages/linux/default.nix
+++ os/packages/linux/default.nix
@@ -2,13 +2,12 @@
 let
   kernelPatches = pkgs.kernelPatches;
 in
-  pkgs.callPackage ./linux-5.2.nix {
+  pkgs.callPackage ./linux-5.3.nix {
     kernelPatches =
       [ kernelPatches.bridge_stp_helper
         # See pkgs/os-specific/linux/kernel/cpu-cgroup-v2-patches/README.md
         # when adding a new linux version
         # kernelPatches.cpu-cgroup-v2."4.11"
-        kernelPatches.modinst_arg_list_too_long
 
         {
           name = "vpsadminos-kernel-config";
@@ -38,22 +37,6 @@ in
             AUFS_BR_HFSPLUS y
             AUFS_BDEV_LOOP y
           '';
-        }
-
-        # br_netfilter in non-initial network namespaces. Known to be used by
-        # docker deployments.
-        # See:
-        #   https://lkml.org/lkml/2018/11/7/681
-        #   https://lore.kernel.org/patchwork/patch/1007863/
-        #   https://lore.kernel.org/patchwork/patch/1007864/
-        #   https://github.com/lxc/lxd/issues/5193
-        rec {
-          name = "br_netfilter_namespace";
-          patch = fetchpatch {
-            name = name + ".patch";
-            url = https://github.com/vpsfreecz/linux/compare/e93c9c99a629c61837d5a7fc2120cd2b6c70dbdd...b78bce45f60a80c3eacfe4b10aeab48e11d29eeb.patch;
-            sha256 = "1dvlhqbj3c7ml5gqgnpy0xmcbc9k0plnh7v23kjijjz9zpadw1hz";
-          };
         }
       ];
   }
