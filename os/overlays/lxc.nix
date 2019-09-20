034abb8c8c5cf97e2eba139954a50425022fd79e
Jakub Skokan <jakub.skokan@havefun.cz>
2019-09-20 12:53:37 +0200

    os: fix glibc build by disabling stringop truncation errors

--- os/overlays/lxc.nix
+++ os/overlays/lxc.nix
@@ -2,6 +2,11 @@ self: super:
 {
   lxc =
     let
+      # Workaround for build failures, try to remove in the future
+      glibc = super.glibc.overrideAttrs (oldAttrs: rec {
+        NIX_CFLAGS_COMPILE = "-Wno-error=stringop-truncation";
+      });
+
       libcap = super.libcap.overrideAttrs (oldAttrs: rec {
         postInstall = builtins.replaceStrings [ ''rm "$lib"/lib/*.a'' ] [ "" ]
                                               oldAttrs.postInstall;
@@ -22,6 +27,6 @@ self: super:
           sha256 = "0b5z6x2kcvq1sb0da4dw8gyh45j1jnq7i0fn1ns64i9y1jcgvjx5";
         };
 
-        buildInputs = oldAttrs.buildInputs ++ [ super.glibc super.glibc.static ];
+        buildInputs = oldAttrs.buildInputs ++ [ glibc glibc.static ];
       });
 }
