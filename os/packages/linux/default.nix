3fabf5ca2457b9e01a709e62c24d7cce399f4822
Jakub Skokan <jakub.skokan@havefun.cz>
2019-09-25 18:21:40 +0200

    os: refactor kernel config into attrset

--- os/packages/linux/default.nix
+++ os/packages/linux/default.nix
@@ -1,6 +1,10 @@
-{ pkgs, fetchpatch, ... }:
+{ pkgs, lib, fetchpatch, ... }:
 let
   kernelPatches = pkgs.kernelPatches;
+
+  mkConfig = attrs: lib.concatStringsSep "\n" (lib.mapAttrsToList (k: v:
+    "${k} ${v}"
+  ) attrs);
 in
   pkgs.callPackage ./linux-5.3.nix {
     kernelPatches =
@@ -12,37 +16,39 @@ in
         {
           name = "vpsadminos-kernel-config";
           patch = null;
-          extraConfig = ''
-            EXPERT y
-            CHECKPOINT_RESTORE y
-            CFS_BANDWIDTH y
-            MEMCG_32BIT_IDS y
-            CGROUP_CGLIMIT y
-            SYSLOG_NS y
-            AUFS_FS y
-            AUFS_BRANCH_MAX_127 y
-            AUFS_SBILIST y
-            AUFS_HNOTIFY y
-            AUFS_HFSNOTIFY y
-            AUFS_EXPORT y
-            AUFS_INO_T_64 y
-            AUFS_XATTR y
-            AUFS_FHSM y
-            AUFS_RDU y
-            AUFS_DIRREN y
-            AUFS_SHWH y
-            AUFS_BR_RAMFS y
-            AUFS_BR_FUSE y
-            AUFS_POLL y
-            AUFS_BR_HFSPLUS y
-            AUFS_BDEV_LOOP y
-          '' +
-          # NFSv4 causes problems to osctl-exportfs. Upon NFS container
-          # descruction, there's an issue with dentries still in use. We only
-          # support NFSv3, so v4 can be disabled.
-          ''
-            NFSD_V4 n
-          '';
+          extraConfig = mkConfig {
+            EXPERT = "y";
+
+            CHECKPOINT_RESTORE = "y";
+            CFS_BANDWIDTH = "y";
+
+            MEMCG_32BIT_IDS = "y";
+            CGROUP_CGLIMIT = "y";
+            SYSLOG_NS = "y";
+
+            AUFS_FS = "y";
+            AUFS_BRANCH_MAX_127 = "y";
+            AUFS_SBILIST = "y";
+            AUFS_HNOTIFY = "y";
+            AUFS_HFSNOTIFY = "y";
+            AUFS_EXPORT = "y";
+            AUFS_INO_T_64 = "y";
+            AUFS_XATTR = "y";
+            AUFS_FHSM = "y";
+            AUFS_RDU = "y";
+            AUFS_DIRREN = "y";
+            AUFS_SHWH = "y";
+            AUFS_BR_RAMFS = "y";
+            AUFS_BR_FUSE = "y";
+            AUFS_POLL = "y";
+            AUFS_BR_HFSPLUS = "y";
+            AUFS_BDEV_LOOP = "y";
+
+            # NFSv4 causes problems to osctl-exportfs. Upon NFS container
+            # descruction, there's an issue with dentries still in use. We only
+            # support NFSv3, so v4 can be disabled.
+            NFSD_V4 = "n";
+          };
         }
       ];
   }
