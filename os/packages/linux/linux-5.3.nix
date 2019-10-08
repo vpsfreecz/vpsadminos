e943ad5edbfdd1787c5d991c6d699a65f8d5396c
Jakub Skokan <jakub.skokan@havefun.cz>
2019-10-08 17:13:37 +0200

    os: upgrade linux to 5.3.5 with overlayfs in userns support

--- os/packages/linux/linux-5.3.nix
+++ os/packages/linux/linux-5.3.nix
@@ -3,7 +3,7 @@
 with stdenv.lib;
 
 buildLinux (args // rec {
-  version = "5.3.1";
+  version = "5.3.5";
 
   # modDirVersion needs to be x.y.z, will automatically add .0 if needed
   modDirVersion = if (modDirVersionArg == null) then concatStrings (intersperse "." (take 3 (splitString "." "${version}.0"))) else modDirVersionArg;
@@ -12,7 +12,7 @@ buildLinux (args // rec {
   extraMeta.branch = concatStrings (intersperse "." (take 2 (splitString "." version)));
 
   src = fetchurl {
-    url = "https://github.com/vpsfreecz/linux/archive/d054bf7583d55c5ce3cb8d91626d17453602bb10.tar.gz";
-    sha256 = "0ffs5iq9bnfijmyrc6wmzc75f1v2gh69f5ahwjmn299p52n4hg77";
+    url = "https://github.com/vpsfreecz/linux/archive/a0d1573a6bebf649898cf8b80dc4394e5ab1af2d.tar.gz";
+    sha256 = "1lvwvbfa2cg9ml2lv219wxg71xi8hwb45rfkqr7lyws7mldaf9vk";
   };
 } // (args.argsOverride or {}))
