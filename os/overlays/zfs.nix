8620de6aa2692dc8453ff40e048b992c7e2affd6
Jakub Skokan <jakub.skokan@havefun.cz>
2019-10-14 22:48:05 +0200

    os: update zfs to 0.8.2-1910140 to fix unlink leak

--- os/overlays/zfs.nix
+++ os/overlays/zfs.nix
@@ -2,12 +2,12 @@ self: super:
 {
   zfs = super.zfsUnstable.overrideAttrs (oldAttrs: rec {
     name = "zfs-${version}";
-    version = "0.8.2.vpsadminos";
+    version = "0.8.2.vpsadminos-1910140";
     src = super.fetchFromGitHub {
       owner = "vpsfreecz";
       repo = "zfs";
-      rev = "91a52106dcb8b280670e36e014e0df66d407b56d";
-      sha256 = "06vqwj9r3m260wy0vbw8aa4l3nk6l4ddzmcr9gf68nxmp30yn71j";
+      rev = "19eaccee409080051f814496c8751bc64d6d1ef2";
+      sha256 = "0vx7abn09shmx7zdwkrds8x3a7fqhrq545b4xil2qz2ynfrl173p";
     };
   });
 }
