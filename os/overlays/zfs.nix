95daa8f2144876702f1411c44ca30fe2527d8929
Pavel Snajdr <snajpa@snajpa.net>
2019-10-16 00:51:25 +0200

    os: update zfs to 0.8.2-1910150

--- os/overlays/zfs.nix
+++ os/overlays/zfs.nix
@@ -2,12 +2,12 @@ self: super:
 {
   zfs = super.zfsUnstable.overrideAttrs (oldAttrs: rec {
     name = "zfs-${version}";
-    version = "0.8.2.vpsadminos-1910140";
+    version = "0.8.2.vpsadminos-1910150";
     src = super.fetchFromGitHub {
       owner = "vpsfreecz";
       repo = "zfs";
-      rev = "19eaccee409080051f814496c8751bc64d6d1ef2";
-      sha256 = "0vx7abn09shmx7zdwkrds8x3a7fqhrq545b4xil2qz2ynfrl173p";
+      rev = "3461a18c3f1830e6085126ec3e6074e489ae5741";
+      sha256 = "1d270l9dl9j8jjffhw09ymyafdg97fc8r6mj4an51sga10bwppcm";
     };
   });
 }
