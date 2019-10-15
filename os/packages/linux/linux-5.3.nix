c2c7b1ff48dfe14247bb2d5e9961ea388d12f2cc
Pavel Snajdr <snajpa@snajpa.net>
2019-10-16 00:44:20 +0200

    os: upgrade linux to 5.3.6

--- os/packages/linux/linux-5.3.nix
+++ os/packages/linux/linux-5.3.nix
@@ -3,7 +3,7 @@
 with stdenv.lib;
 
 buildLinux (args // rec {
-  version = "5.3.5";
+  version = "5.3.6";
 
   # modDirVersion needs to be x.y.z, will automatically add .0 if needed
   modDirVersion = if (modDirVersionArg == null) then concatStrings (intersperse "." (take 3 (splitString "." "${version}.0"))) else modDirVersionArg;
@@ -12,7 +12,7 @@ buildLinux (args // rec {
   extraMeta.branch = concatStrings (intersperse "." (take 2 (splitString "." version)));
 
   src = fetchurl {
-    url = "https://github.com/vpsfreecz/linux/archive/a0d1573a6bebf649898cf8b80dc4394e5ab1af2d.tar.gz";
-    sha256 = "1lvwvbfa2cg9ml2lv219wxg71xi8hwb45rfkqr7lyws7mldaf9vk";
+    url = "https://github.com/vpsfreecz/linux/archive/5a07ba6db968c98cd29d2f76a9570a750c4de201.tar.gz";
+    sha256 = "027gpnv5zyshjlaz11nzx313kppn4149vx0c2ab0fghm85rkgbbd";
   };
 } // (args.argsOverride or {}))
