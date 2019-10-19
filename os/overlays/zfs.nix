003715e92105b79057b9b0c92f4c65d359c014f4
Jakub Skokan <jakub.skokan@havefun.cz>
2019-10-19 11:40:27 +0200

    os: remove root_uid nfs option from zfs

--- os/overlays/zfs.nix
+++ os/overlays/zfs.nix
@@ -2,12 +2,12 @@ self: super:
 {
   zfs = super.zfsUnstable.overrideAttrs (oldAttrs: rec {
     name = "zfs-${version}";
-    version = "0.8.2.vpsadminos-1910150";
+    version = "0.8.2.vpsadminos-1910190";
     src = super.fetchFromGitHub {
       owner = "vpsfreecz";
       repo = "zfs";
-      rev = "3461a18c3f1830e6085126ec3e6074e489ae5741";
-      sha256 = "1d270l9dl9j8jjffhw09ymyafdg97fc8r6mj4an51sga10bwppcm";
+      rev = "ac6a30ee66181892f78d74c59dcad5bffe85d33b";
+      sha256 = "sha256:148kgnbpy4klwgiah2rpiras98g3bnb3d7qp8v228mf7rw1fq2mz";
     };
   });
 }
