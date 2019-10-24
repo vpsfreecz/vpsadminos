edbd3ee0da2685a6ff52cd1624326a36b76a20f7
Jakub Skokan <jakub.skokan@havefun.cz>
2019-10-24 16:16:27 +0200

    os: add missing package files for osctl-exportfs

---
+++ os/packages/osctl-exportfs/gemset.nix
@@ -0,0 +1,64 @@
+{
+  filelock = {
+    groups = ["default"];
+    platforms = [];
+    source = {
+      remotes = ["https://rubygems.vpsfree.cz"];
+      sha256 = "085vrb6wf243iqqnrrccwhjd4chphfdsybkvjbapa2ipfj1ja1sj";
+      type = "gem";
+    };
+    version = "1.1.1";
+  };
+  gli = {
+    groups = ["default"];
+    platforms = [];
+    source = {
+      remotes = ["https://rubygems.vpsfree.cz"];
+      sha256 = "1sgfc8czb7xk0sdnnz7vn61q4ixbkrpz2mkvcgchfkll94rlqhal";
+      type = "gem";
+    };
+    version = "2.17.2";
+  };
+  libosctl = {
+    dependencies = ["rainbow" "require_all"];
+    groups = ["default"];
+    platforms = [];
+    source = {
+      remotes = ["https://rubygems.vpsfree.cz"];
+      sha256 = "0jcihffwkfcpn0i39jpb01faz5r9bgqadak9w9hbrl9pkvrfk2j8";
+      type = "gem";
+    };
+    version = "19.09.0.build20191014132203";
+  };
+  osctl-exportfs = {
+    dependencies = ["filelock" "gli" "libosctl" "require_all"];
+    groups = ["default"];
+    platforms = [];
+    source = {
+      remotes = ["https://rubygems.vpsfree.cz"];
+      sha256 = "0r0000d1f8nbmc85s3rjkzrg9945dz8v2pcfkqqgb6yzm8vdzbvs";
+      type = "gem";
+    };
+    version = "19.09.0.build20191014132203";
+  };
+  rainbow = {
+    groups = ["default"];
+    platforms = [];
+    source = {
+      remotes = ["https://rubygems.vpsfree.cz"];
+      sha256 = "0bb2fpjspydr6x0s8pn1pqkzmxszvkfapv0p4627mywl7ky4zkhk";
+      type = "gem";
+    };
+    version = "3.0.0";
+  };
+  require_all = {
+    groups = ["default"];
+    platforms = [];
+    source = {
+      remotes = ["https://rubygems.vpsfree.cz"];
+      sha256 = "0sjf2vigdg4wq7z0xlw14zyhcz4992s05wgr2s58kjgin12bkmv8";
+      type = "gem";
+    };
+    version = "2.0.0";
+  };
+}
