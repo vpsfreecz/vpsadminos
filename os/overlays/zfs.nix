85678adb6bbf7d8f39a49bd56cff38073605623d
Jakub Skokan <jakub.skokan@havefun.cz>
2019-11-12 16:28:09 +0100

    os: upgrade zfs to vpsadminos-0.8.z-1911120

--- os/overlays/zfs.nix
+++ os/overlays/zfs.nix
@@ -2,12 +2,12 @@ self: super:
 {
   zfs = super.zfsUnstable.overrideAttrs (oldAttrs: rec {
     name = "zfs-${version}";
-    version = "0.8.2.vpsadminos-1910190";
+    version = "0.8.2.vpsadminos-1911120";
     src = super.fetchFromGitHub {
       owner = "vpsfreecz";
       repo = "zfs";
-      rev = "ac6a30ee66181892f78d74c59dcad5bffe85d33b";
-      sha256 = "sha256:148kgnbpy4klwgiah2rpiras98g3bnb3d7qp8v228mf7rw1fq2mz";
+      rev = "aca6706986d2d1e6c19fbe1a908ed43cc85d36b0";
+      sha256 = "sha256:0zccl7qsda97ry4gmhjy6qy8mc39gam2nxihq1kzqchd9jd6za4y";
     };
   });
 }
