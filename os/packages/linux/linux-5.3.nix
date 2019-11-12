68498ccd65217d580737e9c1ca3b99858829eb39
Jakub Skokan <jakub.skokan@havefun.cz>
2019-11-12 16:26:41 +0100

    os: upgrade kernel to 5.3.10

--- os/packages/linux/linux-5.3.nix
+++ os/packages/linux/linux-5.3.nix
@@ -3,7 +3,7 @@
 with stdenv.lib;
 
 buildLinux (args // rec {
-  version = "5.3.7";
+  version = "5.3.10";
 
   # modDirVersion needs to be x.y.z, will automatically add .0 if needed
   modDirVersion = if (modDirVersionArg == null) then concatStrings (intersperse "." (take 3 (splitString "." "${version}.0"))) else modDirVersionArg;
@@ -12,7 +12,7 @@ buildLinux (args // rec {
   extraMeta.branch = concatStrings (intersperse "." (take 2 (splitString "." version)));
 
   src = fetchurl {
-    url = "https://github.com/vpsfreecz/linux/archive/cea3a952c733252f54a5f6d193e6146236e0881d.tar.gz";
-    sha256 = "sha256:1imhmy1x5q468jjqkws9znzkkwdwycwamy9lkk2av8v5nj1psa3l";
+    url = "https://github.com/vpsfreecz/linux/archive/b66d4b75147733563e0d54d6fe2172fd37c2f7f9.tar.gz";
+    sha256 = "sha256:07zm49il6wv1hvr9yrinnvpx9kkkz7mcffnhcc7ysk0n1mx1yrid";
   };
 } // (args.argsOverride or {}))
