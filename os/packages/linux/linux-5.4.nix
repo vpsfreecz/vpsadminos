944898aaa27612f1e668103036d21a18ff1b265d
Pavel Snajdr <snajpa@snajpa.net>
2020-02-03 23:56:10 +0100

    os: Linux 5.4.17

--- os/packages/linux/linux-5.4.nix
+++ os/packages/linux/linux-5.4.nix
@@ -3,7 +3,7 @@
 with stdenv.lib;
 
 buildLinux (args // rec {
-  version = "5.4.10";
+  version = "5.4.17";
 
   # modDirVersion needs to be x.y.z, will automatically add .0 if needed
   modDirVersion = if (modDirVersionArg == null) then concatStrings (intersperse "." (take 3 (splitString "." "${version}.0"))) else modDirVersionArg;
@@ -12,7 +12,7 @@ buildLinux (args // rec {
   extraMeta.branch = concatStrings (intersperse "." (take 2 (splitString "." version)));
 
   src = fetchurl {
-    url = "https://github.com/vpsfreecz/linux/archive/45d78589746267121634d29e5a11ed077ea4076e.tar.gz";
-    sha256 = "sha256:1wj4i35nzlfm55bwxcrx7mbbxlch2jvdh17jsmrhrlnrsvwq1kml";
+    url = "https://github.com/vpsfreecz/linux/archive/7010c3a5fa3a5b1bcf47d49551ce171ade90e7e9.tar.gz";
+    sha256 = "sha256:1y964mcn16rdb70yx20lypgyihrqf8diy9xx7zjqis80qra7ydzh";
   };
 } // (args.argsOverride or {}))
