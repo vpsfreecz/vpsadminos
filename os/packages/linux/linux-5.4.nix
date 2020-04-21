0f31f8b5d84b512bab20a999363e96cf0308ebbd
Jakub Skokan <jakub.skokan@havefun.cz>
2020-04-21 15:55:25 +0200

    os: linux 5.4.34 from vpsadminos-5.4.z-202004210

--- os/packages/linux/linux-5.4.nix
+++ os/packages/linux/linux-5.4.nix
@@ -3,7 +3,7 @@
 with stdenv.lib;
 
 buildLinux (args // rec {
-  version = "5.4.28";
+  version = "5.4.34";
 
   # modDirVersion needs to be x.y.z, will automatically add .0 if needed
   modDirVersion = if (modDirVersionArg == null) then concatStrings (intersperse "." (take 3 (splitString "." "${version}.0"))) else modDirVersionArg;
@@ -12,7 +12,7 @@ buildLinux (args // rec {
   extraMeta.branch = concatStrings (intersperse "." (take 2 (splitString "." version)));
 
   src = fetchurl {
-    url = "https://github.com/vpsfreecz/linux/archive/d809a0d2003e1f244b3f04baff3cc7cc987e1c9f.tar.gz";
-    sha256 = "sha256:0bh031frm7vikxxyfgc8c3d91hz1v83din7cdj8q2i0c9ydpk9p1";
+    url = "https://github.com/vpsfreecz/linux/archive/e0e6d41cbbc49c68ec295f2faecef0432d82be0c.tar.gz";
+    sha256 = "sha256:1iz4adskrpvs9k3dwaj7vfmn0z8z0ixb7zrrqrvdy0lv1jragf0s";
   };
 } // (args.argsOverride or {}))
