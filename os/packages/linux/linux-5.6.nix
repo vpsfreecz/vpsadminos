a68408f76a402878414a37bff0b74ecf32b69856
Jakub Skokan <jakub.skokan@havefun.cz>
2020-06-01 20:54:57 +0200

    os: linux 5.6.15 from vpsadminos-5.6.z-202006010 with nfs client fix

--- os/packages/linux/linux-5.6.nix
+++ os/packages/linux/linux-5.6.nix
@@ -3,7 +3,7 @@
 with stdenv.lib;
 
 buildLinux (args // rec {
-  version = "5.6.14";
+  version = "5.6.15";
 
   # modDirVersion needs to be x.y.z, will automatically add .0 if needed
   modDirVersion = if (modDirVersionArg == null) then concatStrings (intersperse "." (take 3 (splitString "." "${version}.0"))) else modDirVersionArg;
@@ -12,7 +12,7 @@ buildLinux (args // rec {
   extraMeta.branch = concatStrings (intersperse "." (take 2 (splitString "." version)));
 
   src = fetchurl {
-    url = "https://github.com/vpsfreecz/linux/archive/9537115b25b5d697fc66cf375805fc1e95f19cd4.tar.gz";
-    sha256 = "sha256:14123vxsamlw1h79fmkgd8mqhx253ikpw46ccd2rf74r1si38aqr";
+    url = "https://github.com/vpsfreecz/linux/archive/bb0f81df947945eb385c799ac7882be971c9e008.tar.gz";
+    sha256 = "sha256:1yax3b878n8f9jfj2mn5i3i21d5hssl3yzk3xg8z20phpy9lsk65";
   };
 } // (args.argsOverride or {}))
