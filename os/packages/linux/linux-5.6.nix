319a1607780d3402e7d599eccb46242633d80846
Pavel Snajdr <snajpa@snajpa.net>
2020-06-12 13:01:51 +0200

    os: linux 5.6.18
    + sysctl patches
    + oom patches

--- os/packages/linux/linux-5.6.nix
+++ os/packages/linux/linux-5.6.nix
@@ -3,7 +3,7 @@
 with stdenv.lib;
 
 buildLinux (args // rec {
-  version = "5.6.15";
+  version = "5.6.18";
 
   # modDirVersion needs to be x.y.z, will automatically add .0 if needed
   modDirVersion = if (modDirVersionArg == null) then concatStrings (intersperse "." (take 3 (splitString "." "${version}.0"))) else modDirVersionArg;
@@ -12,7 +12,7 @@ buildLinux (args // rec {
   extraMeta.branch = concatStrings (intersperse "." (take 2 (splitString "." version)));
 
   src = fetchurl {
-    url = "https://github.com/vpsfreecz/linux/archive/bb0f81df947945eb385c799ac7882be971c9e008.tar.gz";
-    sha256 = "sha256:1yax3b878n8f9jfj2mn5i3i21d5hssl3yzk3xg8z20phpy9lsk65";
+    url = "https://github.com/vpsfreecz/linux/archive/cd362b321c49615d11ba4653a8a40c0d33003e17.tar.gz";
+    sha256 = "sha256:07am93nr7nnpfrzkzpjndkyzlzg3ysfi8sm2s478c40v7hi4nlak";
   };
 } // (args.argsOverride or {}))
