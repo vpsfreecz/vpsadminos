38333a2450f02a53d2cb553f066cab8fff1af19b
Pavel Snajdr <snajpa@snajpa.net>
2020-06-20 19:50:22 +0200

    os: update Linux and ZFS
    linux 5.6.19 @
    https://github.com/vpsfreecz/linux/commits/44777501fdf848aeb8c2725373c79763d49d6308
    
    openzfs @
    https://github.com/vpsfreecz/zfs/commits/2ea5232e024ac3c97bf2e3a10f913984309cb2ff
    
    Fixes vpsfreecz/vpsadminos#29
    Fixes vpsfreecz/vpsadminos#36

--- os/packages/linux/linux-5.6.nix
+++ os/packages/linux/linux-5.6.nix
@@ -3,7 +3,7 @@
 with stdenv.lib;
 
 buildLinux (args // rec {
-  version = "5.6.18";
+  version = "5.6.19";
 
   # modDirVersion needs to be x.y.z, will automatically add .0 if needed
   modDirVersion = if (modDirVersionArg == null) then concatStrings (intersperse "." (take 3 (splitString "." "${version}.0"))) else modDirVersionArg;
@@ -12,7 +12,7 @@ buildLinux (args // rec {
   extraMeta.branch = concatStrings (intersperse "." (take 2 (splitString "." version)));
 
   src = fetchurl {
-    url = "https://github.com/vpsfreecz/linux/archive/cd362b321c49615d11ba4653a8a40c0d33003e17.tar.gz";
-    sha256 = "sha256:07am93nr7nnpfrzkzpjndkyzlzg3ysfi8sm2s478c40v7hi4nlak";
+    url = "https://github.com/vpsfreecz/linux/archive/44777501fdf848aeb8c2725373c79763d49d6308.tar.gz";
+    sha256 = "sha256:0yln43ia53vjz673sn5zixn6yg0hvmqszly5d6vcf5bi6wfihsbf";
   };
 } // (args.argsOverride or {}))
