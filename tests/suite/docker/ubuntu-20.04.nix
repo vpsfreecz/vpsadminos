9ff4feeecb5b1ef75832e960e31cb6df77cfd061
Jakub Skokan <jakub.skokan@havefun.cz>
2020-07-22 19:47:44 +0200

    tests/suite/docker: fix ubuntu tests

--- tests/suite/docker/ubuntu-20.04.nix
+++ tests/suite/docker/ubuntu-20.04.nix
@@ -7,7 +7,7 @@ import ./base.nix {
       "osctl ct exec docker apt-get -y install apt-transport-https ca-certificates curl software-properties-common",
       "osctl ct exec docker bash -c \"curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add -\"",
       "osctl ct exec docker add-apt-repository \"deb [arch=amd64] https://download.docker.com/linux/ubuntu focal stable\"",
-      "osctl ct exec docker apt-update -y",
+      "osctl ct exec docker apt-get update -y",
       "osctl ct exec docker apt-get -y install docker-ce",
     )
   '';
