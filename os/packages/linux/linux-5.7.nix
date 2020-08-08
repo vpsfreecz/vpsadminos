acd2a5f15a218704ef05fddb638f18abfe11be3a
Pavel Snajdr <snajpa@snajpa.net>
2020-08-08 03:02:46 +0200

    os: Linux 5.7.14 + ksoftlimd v2
    ksoftlimd v2 patch:
    
    https://github.com/vpsfreecz/linux/commit/4e55598d1bf6f6e55eabebad7c7e1bfa5ab8336a

--- os/packages/linux/linux-5.7.nix
+++ os/packages/linux/linux-5.7.nix
@@ -3,7 +3,7 @@
 with stdenv.lib;
 
 callPackage ./generic.nix (args // rec {
-  version = "5.7.12";
+  version = "5.7.14";
 
   # modDirVersion needs to be x.y.z, will automatically add .0 if needed
   modDirVersion = if (modDirVersionArg == null) then concatStrings (intersperse "." (take 3 (splitString "." "${version}.0"))) else modDirVersionArg;
@@ -12,7 +12,7 @@ callPackage ./generic.nix (args // rec {
   extraMeta.branch = concatStrings (intersperse "." (take 2 (splitString "." version)));
 
   src = fetchurl {
-    url = "https://github.com/vpsfreecz/linux/archive/715147f9d8a3dcc3d7f8ed23637f58f6a3928f58.tar.gz";
-    sha256 = "sha256:1bqszb7vprr60b3mh55w4bz2h7r78rm1b7k1hg0dhfl4y6d27190";
+    url = "https://github.com/vpsfreecz/linux/archive/4e55598d1bf6f6e55eabebad7c7e1bfa5ab8336a.tar.gz";
+    sha256 = "sha256:03yibl3xh04wn0i0nfxk9g9avmmb4c128b4iw2vz213kwnw8dm9x";
   };
 } // (args.argsOverride or {}))
