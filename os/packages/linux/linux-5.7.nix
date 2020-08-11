e4dcfa2e0affd4febb960b994226cfa05f09f917
Pavel Snajdr <snajpa@snajpa.net>
2020-08-11 21:06:00 +0200

    os: Linux: ksoftlimd default overllimit sleep adjust to 250ms

--- os/packages/linux/linux-5.7.nix
+++ os/packages/linux/linux-5.7.nix
@@ -12,7 +12,7 @@ callPackage ./generic.nix (args // rec {
   extraMeta.branch = concatStrings (intersperse "." (take 2 (splitString "." version)));
 
   src = fetchurl {
-    url = "https://github.com/vpsfreecz/linux/archive/0ef1cf29f5aa4acdc3760cccf269baf0742e735d.tar.gz";
-    sha256 = "sha256:1z6qw3sgnppnajlbxlhv5m8lb1g158z4pzd9qnpgd8dkdy0hp221";
+    url = "https://github.com/vpsfreecz/linux/archive/e04b3f9cda1d755cac522755b9712b040a538de3.tar.gz";
+    sha256 = "sha256:18sk41ckp7qsfr7q6865zsr5df7nf9g4f74m3rrbnqxs6fkq7d66";
   };
 } // (args.argsOverride or {}))
