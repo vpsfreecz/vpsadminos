5b56cc1572295ad953e407e49278e4a6b7569c6b
Jakub Skokan <jakub.skokan@havefun.cz>
2020-09-21 19:14:28 +0200

    libosctl: introduce method replace_symlink

--- libosctl/lib/libosctl/utils/file.rb
+++ libosctl/lib/libosctl/utils/file.rb
@@ -19,6 +19,15 @@ module OsCtl::Lib
       File.rename(replacement, path)
     end
 
+    # Atomically replace or create symlink
+    # @param path [String] symlink path
+    # @param dst [String] destination
+    def replace_symlink(path, dst)
+      replacement = "#{path}.new-#{SecureRandom.hex(3)}"
+      File.symlink(dst, replacement)
+      File.rename(replacement, path)
+    end
+
     def unlink_if_exists(path)
       File.unlink(path)
       true
