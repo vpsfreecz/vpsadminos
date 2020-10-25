9ddea7de24141369b93bde1d17ec1af072055902
Pavel Snajdr <snajpa@snajpa.net>
2020-10-25 19:41:34 +0100

    os: Linux 5.9.1

--- os/packages/linux/linux-5.9.nix
+++ os/packages/linux/linux-5.9.nix
@@ -3,7 +3,7 @@
 with stdenv.lib;
 
 callPackage ./generic.nix (args // rec {
-  version = "5.9.0";
+  version = "5.9.1";
 
   # modDirVersion needs to be x.y.z, will automatically add .0 if needed
   modDirVersion = if (modDirVersionArg == null) then concatStrings (intersperse "." (take 3 (splitString "." "${version}.0"))) else modDirVersionArg;
@@ -12,7 +12,7 @@ callPackage ./generic.nix (args // rec {
   extraMeta.branch = concatStrings (intersperse "." (take 2 (splitString "." version)));
 
   src = fetchurl {
-    url = "https://github.com/snajpa/linux/archive/740d00580c166bee7309682979be27eaf1a4c596.tar.gz";
-    sha256 = "sha256:0ch5g5srijkw57as3ywg6sdcmxgby7mcm6012p70c24b16qqbd33";
+    url = "https://github.com/snajpa/linux/archive/47bcf717f6ce41ba823e9846463a8b75e8ae3a57.tar.gz";
+    sha256 = "sha256:19mjwfx0xacza0xdnwrm0g2ghh0n97fjxr4p39p51g38rhlvqddh";
   };
 } // (args.argsOverride or {}))
