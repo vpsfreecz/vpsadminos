a9b6e5b343ffeb9a3219f6a7170485b7ef244fb3
Jakub Skokan <jakub.skokan@havefun.cz>
2020-11-09 18:14:13 +0100

    os: linux 5.9.6

--- os/packages/linux/linux-5.9.nix
+++ os/packages/linux/linux-5.9.nix
@@ -3,7 +3,7 @@
 with stdenv.lib;
 
 callPackage ./generic.nix (args // rec {
-  version = "5.9.2";
+  version = "5.9.6";
 
   # modDirVersion needs to be x.y.z, will automatically add .0 if needed
   modDirVersion = if (modDirVersionArg == null) then concatStrings (intersperse "." (take 3 (splitString "." "${version}.0"))) else modDirVersionArg;
@@ -12,7 +12,7 @@ callPackage ./generic.nix (args // rec {
   extraMeta.branch = concatStrings (intersperse "." (take 2 (splitString "." version)));
 
   src = fetchurl {
-    url = "https://github.com/vpsfreecz/linux/archive/10a301c4a73122a92a396e37415922b458263b6f.tar.gz";
-    sha256 = "sha256:0msdi53lf4gi35k50k8246n52l0n9h1xvrdrmskwcbbk1n8mw14v";
+    url = "https://github.com/vpsfreecz/linux/archive/70a14f1fef78ae30782f14bc28d9992f160f1e18.tar.gz";
+    sha256 = "sha256:1f1idz4wbvxwpzqdypgf8wh5nhcc9nyawrlbmc5zsfls002gd3jn";
   };
 } // (args.argsOverride or {}))
