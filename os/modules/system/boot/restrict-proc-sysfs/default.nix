278878bcc38675df0ad165aac41d0978bcac9203
Jakub Skokan <jakub.skokan@havefun.cz>
2020-11-16 14:12:20 +0100

    os: restrict-proc-sysfs: add description

--- os/modules/system/boot/restrict-proc-sysfs/default.nix
+++ os/modules/system/boot/restrict-proc-sysfs/default.nix
@@ -13,7 +13,7 @@ in {
         type = types.bool;
         default = true;
         description = ''
-          Restrict proc and sysfs contents
+          Restrict access to proc, sysfs and any other filesystem contents
         '';
       };
 
@@ -22,6 +22,25 @@ in {
         default = builtins.readFile ./config.txt;
         description = ''
           Config passed to ./restrict-dirs.rb
+
+          Each line represents a rule for a path. The first word is a command,
+          the second word is the path. The command can be one of: restrict, skip
+          and grant. Empty lines and lines beginning with a hash are ignored.
+
+          restrict is used to deny access from containers to the path, skip does
+          not change the access mode and grant will give read-write access to
+          containers and all their users, even unprivileged ones.
+
+          The path can contain patterns, which are expanded. Rules are evaluated
+          from the top. There can be more than one rule for one path, the last
+          rule will be used. This makes it possible to e.g. use wildcards with
+          exceptions:
+
+          restrict /sys/class/*
+          skip /sys/class/net
+
+          The rules above will restrict access to the contents of /sys/class,
+          except for directory /sys/class/net.
         '';
       };
     };
