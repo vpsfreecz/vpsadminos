6071b16608d88301fbd2b1ef5d4d92506361e046
Jakub Skokan <jakub.skokan@havefun.cz>
2021-03-16 19:41:22 +0100

    os: set the initial generation on a live system

--- os/modules/services/misc/nix-daemon.nix
+++ os/modules/services/misc/nix-daemon.nix
@@ -21,6 +21,11 @@ in
       runit.services.nix = {
         run = ''
           nix-store --load-db < /nix/store/nix-path-registration
+
+          if ! isKernelParamSet nolive && [ ! -e /nix/var/nix/profiles/system ] ; then
+            nix-env -p /nix/var/nix/profiles/system --set /run/current-system
+          fi
+
           exec nix-daemon
         '';
         runlevels = [ "rescue" "default" ];
