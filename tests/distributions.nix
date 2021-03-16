5ad8b3ac791ec73a659015e66d408f44480a2031
Jakub Skokan <jakub.skokan@havefun.cz>
2021-03-16 16:13:06 +0100

    tests: test supported versions of centos, debian, ubuntu and void

--- tests/distributions.nix
+++ tests/distributions.nix
@@ -1,12 +1,18 @@
 [
   { distribution = "arch"; version = "latest"; }
   { distribution = "alpine"; version = "latest"; }
-  { distribution = "centos"; version = "latest"; }
-  { distribution = "debian"; version = "latest"; }
+  { distribution = "centos"; version = "7"; }
+  { distribution = "centos"; version = "8"; }
+  { distribution = "debian"; version = "9"; }
+  { distribution = "debian"; version = "10"; }
+  { distribution = "debian"; version = "testing"; }
   { distribution = "fedora"; version = "latest"; }
   { distribution = "gentoo"; version = "latest"; }
   { distribution = "opensuse"; version = "latest"; }
   { distribution = "slackware"; version = "latest"; }
-  { distribution = "ubuntu"; version = "latest"; }
-  { distribution = "void"; version = "latest"; }
+  { distribution = "ubuntu"; version = "16.04"; }
+  { distribution = "ubuntu"; version = "18.04"; }
+  { distribution = "ubuntu"; version = "20.04"; }
+  { distribution = "void"; version = "latest-glibc"; }
+  { distribution = "void"; version = "latest-musl"; }
 ]
