4eb922cff0dd590f0d457b88ee5672b4d56e8cc3
Pavel Snajdr <snajpa@snajpa.net>
2021-04-03 22:54:27 +0200

    os: Linux 5.10.27
    Signed-off-by: Pavel Snajdr <snajpa@snajpa.net>

--- os/packages/linux/linux-5.10.nix
+++ os/packages/linux/linux-5.10.nix
@@ -3,7 +3,7 @@
 with stdenv.lib;
 
 callPackage ./generic.nix (args // rec {
-  version = "5.10.25";
+  version = "5.10.27";
 
   # modDirVersion needs to be x.y.z, will automatically add .0 if needed
   modDirVersion = if (modDirVersionArg == null) then concatStrings (intersperse "." (take 3 (splitString "." "${version}.0"))) else modDirVersionArg;
@@ -12,7 +12,7 @@ callPackage ./generic.nix (args // rec {
   extraMeta.branch = concatStrings (intersperse "." (take 2 (splitString "." version)));
 
   src = fetchurl {
-    url = "https://github.com/vpsfreecz/linux/archive/094477786e2084636630e8a0f1dc941bd5d7a5d8.tar.gz";
-    sha256 = "1g80hgmzbsjac1717p2zy2a19b5adl8hvdwrwfc6iih5w4w235zd";
+    url = "https://github.com/vpsfreecz/linux/archive/e0b77762ebc65f83616fa19aaf2e850e0272a06e.tar.gz";
+    sha256 = "0iayz9kbg1pdlasa9y9h3jyikz8bgw7kj6zql2f1dg10vrv1z5f2";
   };
 } // (args.argsOverride or {}))
