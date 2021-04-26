79e6da8ab7da357f1c2325f7680753718da8f1ba
Jakub Skokan <jakub.skokan@havefun.cz>
2021-04-26 16:49:20 +0200

    test-runner.sh: fix argument passing

--- test-runner.sh
+++ test-runner.sh
@@ -2,4 +2,4 @@
 set -e
 mkdir -p result
 nix-build --out-link result/test-runner os/packages/test-runner/entry.nix > /dev/null
-exec ./result/test-runner/bin/test-runner $@
+exec ./result/test-runner/bin/test-runner "$@"
