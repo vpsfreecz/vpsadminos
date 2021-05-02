613ef953c76e7f1cb11d764da8f8e1a11a1a11fd
Jakub Skokan <jakub.skokan@havefun.cz>
2021-05-02 11:58:30 +0200

    image-scripts: Add Rocky Linux 8.3 RC1

---
+++ image-scripts/images/rocky-8/build.sh
@@ -0,0 +1,35 @@
+. "$IMAGEDIR/config.sh"
+POINTVER=8.3
+RELEASE=https://download.rockylinux.org/pub/rocky/${POINTVER}/BaseOS/x86_64/os/Packages/rocky-release-${POINTVER}-11.el8.noarch.rpm
+BASEURL=https://download.rockylinux.org/pub/rocky/${POINTVER}/BaseOS/x86_64/os/
+
+# CentOS 8 does not seem to have an updates repo, so this variable is used to
+# add AppStream repository just for the installation process.
+UPDATES=https://download.rockylinux.org/pub/rocky/${POINTVER}/AppStream/x86_64/os/
+
+GROUPNAME='core'
+EXTRAPKGS='vim man'
+
+. $INCLUDE/redhat-family.sh
+
+bootstrap
+configure-common
+
+configure-redhat-common
+
+configure-append <<EOF
+/usr/bin/systemctl disable firewalld.service
+/usr/bin/systemctl mask auditd.service
+/usr/bin/systemctl mask kdump.service
+/usr/bin/systemctl mask plymouth-start.service
+/usr/bin/systemctl mask tuned.service
+
+cat <<EOT > /etc/NetworkManager/conf.d/vpsadminos.conf
+[main]
+plugins+=ifcfg-rh
+rc-manager=file
+configure-and-quit=true
+EOT
+EOF
+
+run-configure
