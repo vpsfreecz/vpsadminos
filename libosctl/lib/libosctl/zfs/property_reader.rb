f6c953e0822428f0205984cf932b8cf1f0b94e09
Jakub Skokan <jakub.skokan@havefun.cz>
2021-06-25 20:19:55 +0200

    osctl-exporter: add properties of container datasets

---
+++ libosctl/lib/libosctl/zfs/property_reader.rb
@@ -0,0 +1,37 @@
+module OsCtl::Lib
+  # Read selected dataset properties into {Zfs::DatasetTree}
+  class Zfs::PropertyReader
+    include Utils::Log
+    include Utils::System
+
+    # @param dataset_names [Array<String>]
+    # @param properties [Array<String>]
+    # @param type [String]
+    # @param recursive [Boolean]
+    # @return [Zfs::DatasetTree]
+    def read(dataset_names, properties, type: 'filesystem', recursive: false)
+      tree = Zfs::DatasetTree.new
+
+      zfs_opts = [
+        '-Hp',
+        '-o', 'name,property,value',
+        '-t', type,
+      ]
+
+      zfs_opts << '-r' if recursive
+      zfs_opts << properties.join(',')
+
+      zfs(
+        :get,
+        zfs_opts.join(' '),
+        dataset_names.join(' ')
+      ).output.strip.split("\n").each do |line|
+        dataset, prop, val = line.split("\t")
+
+        tree.add_property(dataset, prop, val)
+      end
+
+      tree
+    end
+  end
+end
