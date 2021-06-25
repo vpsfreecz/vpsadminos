d4d0790023527bcf57b5d4cd4fe86abdb01afed4
Jakub Skokan <jakub.skokan@havefun.cz>
2021-06-25 20:19:55 +0200

    osctl-exporter: add properties of container datasets

---
+++ libosctl/lib/libosctl/zfs/dataset_tree.rb
@@ -0,0 +1,78 @@
+module OsCtl::Lib
+  # Encapsulates a tree of datasets and their properties
+  class Zfs::DatasetTree
+    # @return [String, nil] full name
+    attr_reader :name
+
+    # @return [Hash<String, String>]
+    attr_reader :properties
+
+    # @return [Hash<String, Zfs::DatasetTree>]
+    attr_reader :datasets
+
+    # @param name [String]
+    def initialize(name: nil)
+      @name = name
+      @properties = {}
+      @datasets = {}
+    end
+
+    # Add property to a dataset
+    # @param dataset [String]
+    # @param property [String]
+    # @param value [String]
+    def add_property(dataset, property, value)
+      parts = dataset.split('/')
+      do_add_property([], parts.first, parts[1..-1], property, value)
+    end
+
+    # Lookup dataset in the subtree
+    # @param dataset [String] full name
+    def [](dataset)
+      parts = dataset.split('/')
+      tree = self
+
+      parts.each do |name|
+        tree = tree.datasets[name]
+      end
+
+      tree
+    end
+
+    # Iterate over all datasets in the subtree
+    def each_dataset(&block)
+      block.call(self)
+      datasets.each_value { |ds| ds.each_dataset(&block) }
+    end
+
+    # Print the tree to the console
+    def print(indent: 0)
+      puts "#{' ' * indent}#{name}:"
+
+      properties.each do |k, v|
+        puts "#{' ' * indent}  #{k}=#{v}"
+      end
+
+      datasets.each_value do |tree|
+        tree.print(indent: indent + 2)
+      end
+    end
+
+    protected
+    def do_add_property(path, name, parts, property, value)
+      datasets[name] ||= self.class.new(name: (path + [name]).join('/'))
+
+      if parts.empty?
+        datasets[name].properties[property] = value
+      else
+        datasets[name].do_add_property(
+          path + [name],
+          parts.first,
+          parts[1..-1],
+          property,
+          value
+        )
+      end
+    end
+  end
+end
