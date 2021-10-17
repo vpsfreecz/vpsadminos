852cca52527d49f1675db140e1dceb2e4041d3a2
Jakub Skokan <jakub.skokan@havefun.cz>
2021-10-17 16:39:35 +0200

    image-scripts: Add Devuan 4

---
+++ image-scripts/images/devuan-4/build.sh
@@ -0,0 +1,33 @@
+. "$IMAGEDIR/config.sh"
+RELNAME=chimaera
+BASEURL=http://deb.devuan.org/merged
+
+. $INCLUDE/devuan.sh
+
+bootstrap
+
+cat > $INSTALL/etc/apt/sources.list <<SOURCES
+deb $BASEURL $RELNAME main
+deb-src $BASEURL $RELNAME main
+
+deb $BASEURL $RELNAME-updates main
+deb-src $BASEURL $RELNAME-updates main
+
+deb $BASEURL $RELNAME-security main
+deb-src $BASEURL $RELNAME-security main
+SOURCES
+
+cp "$IMAGEDIR"/cgroups-mount.initscript "$INSTALL"/etc/init.d/cgroups-mount
+chmod +x "$INSTALL"/etc/init.d/cgroups-mount
+
+cp "$IMAGEDIR"/cgconfig.conf "$INSTALL"/etc/cgconfig.conf
+
+configure-common
+
+configure-devuan-append <<EOF
+update-rc.d cgroups-mount defaults
+EOF
+
+configure-devuan
+
+run-configure
