345db090779814c746e071b6f5245076c92d4b73
Jakub Skokan <jakub.skokan@havefun.cz>
2021-11-17 12:58:24 +0100

    image-scripts: gentoo: refactor for readability

--- image-scripts/images/gentoo/build.sh
+++ image-scripts/images/gentoo/build.sh
@@ -3,21 +3,21 @@ BASEURL=https://mirror.vpsfree.cz/gentoo
 
 require_cmd curl
 
-STAGE3BASEURL="${BASEURL}/releases/amd64/autobuilds"
-STAGE3TARBALLURL="${STAGE3BASEURL}/$(curl "${STAGE3BASEURL}/latest-stage3-amd64.txt" | grep -o -m 1 -P '^[\dTZ]+/stage3-amd64-[\dTZ]+.tar.xz')"
-STAGE3TARBALL="$(basename $STAGE3TARBALLURL)"
+STAGE3_BASE_URL="${BASEURL}/releases/amd64/autobuilds"
+STAGE3_TARBALL_URL="${STAGE3_BASE_URL}/$(curl "${STAGE3_BASE_URL}/latest-stage3-amd64.txt" | grep -o -m 1 -P '^[\dTZ]+/stage3-amd64-[\dTZ]+.tar.xz')"
+STAGE3_TARBALL="$(basename $STAGE3_TARBALL_URL)"
 
 
-wget -P $DOWNLOAD ${STAGE3TARBALLURL}{.CONTENTS.gz,.DIGESTS,}
+wget -P $DOWNLOAD ${STAGE3_TARBALL_URL}{.CONTENTS.gz,.DIGESTS,}
 
-if ! (cd $DOWNLOAD; sed -rn '/# SHA512/ {N;p}' ${STAGE3TARBALL}.DIGESTS | sha512sum -c);
+if ! (cd $DOWNLOAD; sed -rn '/# SHA512/ {N;p}' ${STAGE3_TARBALL}.DIGESTS | sha512sum -c);
 then
 	echo "Stage 3 checksum wrong! Quitting."
 	exit 1
 fi
 
 echo "Unpacking Stage3..."
-tar xJpf ${DOWNLOAD}/${STAGE3TARBALL} -C $INSTALL
+tar xJpf ${DOWNLOAD}/${STAGE3_TARBALL} -C $INSTALL
 
 cp /etc/resolv.conf $INSTALL/etc/
 
@@ -31,20 +31,27 @@ configure-append <<EOF
 echo 'LANG="en_US.UTF-8"' >/etc/env.d/02locale
 echo 'GENTOO_MIRRORS="$BASEURL/ http://ftp.fi.muni.cz/pub/linux/gentoo/"' >> /etc/portage/make.conf
 echo "Europe/Prague" > /etc/timezone
+
 emerge-webrsync -v
+
 sed -i '/^USE=/d' /etc/portage/make.conf
 echo 'USE="-udev"' >> /etc/portage/make.conf
 echo "=sys-apps/openrc-0.35* ~amd64" > /etc/portage/package.keywords/image
 echo "sys-apps/busybox mdev" > /etc/portage/package.use/image
+
 emerge --unmerge udev
 emerge --update --deep --newuse --with-bdeps=y --backtrack=120 @system @world
 emerge busybox dhcpcd iproute2 vim
+
 sed -ri 's/^#rc_sys=""/rc_sys="lxc"/' /etc/rc.conf
 sed -ri 's/^([^#].*agetty.*)$/#\1/' /etc/inittab
+
 rc-update add sshd default
 rc-update del udev sysinit
 rc-update add mdev sysinit
+
 eselect news read
+
 sed -i 's/^PasswordAuthentication no/PasswordAuthentication yes/' /etc/ssh/sshd_config
 sed -i 's/^#PermitRootLogin prohibit-password/PermitRootLogin yes/' /etc/ssh/sshd_config
 
