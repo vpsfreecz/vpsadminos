0b21961df4fe869483529119c76fe89a27489cda
Jakub Skokan <jakub.skokan@havefun.cz>
2022-01-11 17:11:17 +0100

    tests: update distribution list

--- tests/distributions.nix
+++ tests/distributions.nix
@@ -1,16 +1,15 @@
 [
-  { distribution = "arch"; version = "latest"; }
+  { distribution = "almalinux"; version = "8"; }
   { distribution = "alpine"; version = "latest"; }
+  { distribution = "arch"; version = "latest"; }
   { distribution = "centos"; version = "7"; }
-  { distribution = "centos"; version = "8"; }
   { distribution = "centos"; version = "latest-stream"; }
-  { distribution = "debian"; version = "9"; }
   { distribution = "debian"; version = "10"; }
+  { distribution = "debian"; version = "11"; }
   { distribution = "debian"; version = "testing"; }
   { distribution = "fedora"; version = "latest"; }
   { distribution = "gentoo"; version = "latest"; }
   { distribution = "opensuse"; version = "latest"; }
-  { distribution = "slackware"; version = "latest"; }
   { distribution = "ubuntu"; version = "16.04"; }
   { distribution = "ubuntu"; version = "18.04"; }
   { distribution = "ubuntu"; version = "20.04"; }
