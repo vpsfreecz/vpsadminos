c2f3302b602aa75e25b6b6af6498627293ce17f0
Pavel Snajdr <snajpa@snajpa.net>
2022-01-31 16:47:01 +0100

    wipololo

--- os/modules/services/livepatches/default.nix
+++ os/modules/services/livepatches/default.nix
@@ -1,12 +1,57 @@
 { config, lib, pkgs, utils, ... }:
 with lib;
 
+let
+  availablePatches = import ../../../livepatches/availablePatches.nix;
+
+  kernel = pkgs.callPackage (import ../../../packages/linux/default.nix) {};
+
+  buildLivePatch = { patchName, kernel, stdenv }:
+    stdenv.mkDerivation rec {
+      pname = "livepatch-${kernel.modDirVersion}-${patchName}";
+      version = "1";
+      src = ../../../livepatches;
+    
+      configurePhase = ''
+        echo configurePhase; pwd; ls; pwd; echo;
+        mkdir ${patchName}
+        cp $src/${patchName}.c ${patchName}/livepatch-${patchName}.c
+        echo 'obj-m += livepatch-${patchName}.o' > ${patchName}/Makefile
+      '';
+      buildPhase = ''
+        echo buildPhase; pwd; ls; pwd;
+        cd ${patchName}
+        make -C ${kernel.dev}/lib/modules/${kernel.modDirVersion}/build \
+          -j$NIX_BUILD_CORES M=$(pwd) modules
+      '';
+    
+      installPhase = ''
+        make -C ${kernel.dev}/lib/modules/${kernel.modDirVersion}/build  \
+          INSTALL_MOD_PATH=$out M=$(pwd) modules_install
+      '';
+    };
+
+  insmodLineGen = (patchName: kernel:
+        "[ -d /sys/kernel/livepatch/" ++ builtins.replaceStrings ["-"] ["_"] patchName ++ " ] || " ++
+        ''insmod ${pkgs.callPackage buildLivePatch {
+          patchName = patchName;
+          kernel = kernel;
+        }}/lib/modules/${kernel.modDirVersion}/extra/livepatch-${patchName}.ko.*''
+  );
+in
 {
+  options = {
+    enabled = mkOption {
+      type = types.bool;
+      default = true;
+      description = ''
+        Enable/disable Live Patching
+      '';
+    };
+  };
   config = {
     runit.services.livepatches = {
-      run = ''
-        modprobe ${pkgs.livepatch-cpu-fakemask}/*.ko
-      '';
+      run = concatMapStringsSep "\n" insmodLineGen kernel availablePatches;
       oneShot = true;
       runlevels = [ "default" ];
     };
