6e1b2a055f46e252cc83699d252aab015ec3a504
Pavel Snajdr <snajpa@snajpa.net>
2022-02-06 13:13:31 +0100

    os: Linux 5.10.98

--- os/packages/linux/linux-5.10.nix
+++ os/packages/linux/linux-5.10.nix
@@ -3,7 +3,7 @@
 with lib;
 
 callPackage ./generic.nix (args // rec {
-  version = "5.10.93";
+  version = "5.10.98";
 
   # modDirVersion needs to be x.y.z, will automatically add .0 if needed
   modDirVersion = if (modDirVersionArg == null) then concatStrings (intersperse "." (take 3 (splitString "." "${version}.0"))) else modDirVersionArg;
@@ -12,7 +12,7 @@ callPackage ./generic.nix (args // rec {
   extraMeta.branch = concatStrings (intersperse "." (take 2 (splitString "." version)));
 
   src = fetchurl {
-    url = "https://github.com/vpsfreecz/linux/archive/5b4d755822c636c4cd51a0fd9d795feb4844c140.tar.gz";
-    sha256 = "0c2zxxf6m3p6nn26daxckp3rjambs29j9va8fhps8ll5pr6fnd1p";
+    url = "https://github.com/vpsfreecz/linux/archive/c0533a6a6b4af2f37863ad1626edaced4dc4edc6.tar.gz";
+    sha256 = "sha256-EqoVOFT/CxIV4zd2eOCM78TBBjUYU7Jrj+86yOwaK0A=";
   };
 } // (args.argsOverride or {}))
