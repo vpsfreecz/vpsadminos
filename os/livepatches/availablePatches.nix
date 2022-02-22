f60210863ba6fa24e0eb22e68cea0413bcc4f452
Pavel Snajdr <snajpa@snajpa.net>
2022-02-23 00:08:59 +0100

    os: Live Patches: availablePatches rework + list command
    Signed-off-by: Pavel Snajdr <snajpa@snajpa.net>

--- os/livepatches/availablePatches.nix
+++ os/livepatches/availablePatches.nix
@@ -1,25 +1,43 @@
-{ lib, ... }:
+{ lib, version ? null, ... }:
 with lib;
 
 let
 
-  patches = {
-    "uname" = availableForAllKernels;
-    "ucounts-overlimit-fix" = availableForRange "5.10.93" "5.10.98";
-    "bp-5.10.101-nfs-fixes" = availableFor "5.10.98";
-    "nfs-unfreezer-workaround" = availableSince "5.10.93";
-  };
+  availablePatches = [
+    { name = "uname";
+      filterFn = availableForAllKernels;
+      version = 2;
+    }
+    { name = "ucounts-overlimit-fix";
+      filterFn = availableForRange "5.10.93" "5.10.98";
+    }
+    { name = "bp-5.10.101-nfs-fixes";
+      filterFn = availableFor "5.10.98";
+    }
+    { name = "nfs-unfreezer-workaround";
+      filterFn = availableSince "5.10.93";
+    }
+  ];
 
   availableForAllKernels = kernelVersion: true;
   availableFor = compatVersion: kernelVersion:
     kernelVersion == compatVersion;
   availableSince = verLow: kernelVersion:
-    (versionAtLeast version verLow);
+    (versionAtLeast kernelVersion verLow);
   availableForRange = verLow: verHigh: kernelVersion:
-    (versionAtLeast version verLow && versionOlder version verHigh);
+    (versionAtLeast kernelVersion verLow && versionUpTo kernelVersion verHigh);
+  versionUpTo = v1: v2: builtins.compareVersions v2 v1 < 1;
 
-  filterPatches = kernelVersion: filterAttrs (n: filterFn: filterFn kernelVersion) patches;
+  getPatchVersion = patch: if (hasAttr "version" patch) then patch.version else 1;
+  filterPatches = kernelVersion: filter (patch: patch.filterFn kernelVersion) availablePatches;
+  filterPatchesVersions = kernelVersion: map getPatchVersion (filterPatches kernelVersion);
+  filterPatchesVersionsSum = kernelVersion: foldl (x: y: x+y) 0 (filterPatchesVersions kernelVersion);
+
+  patchListForVersion = kernelVersion: map (patch: patch.name) (filterPatches kernelVersion);
 in {
-  allPatches = mapAttrsToList (n: v: n) patches;
-  patchesForVersion = kernelVersion: mapAttrsToList (n: v: n) (filterPatches kernelVersion);
+  getPatchVersion = getPatchVersion;
+  patchList  = patchListForVersion version;
+  patchVersion = filterPatchesVersionsSum version;
+  filteredPatches = filterPatches version;
+  allPatches = availablePatches;
 }
--- os/modules/services/livepatches/default.nix
+++ os/modules/services/livepatches/default.nix
@@ -5,89 +5,22 @@ let
   cfg = config.services.live-patches;
 
   patchesDir = ../../../livepatches;
-  availablePatches = (import (patchesDir + /availablePatches.nix) { inherit lib; }).patchesForVersion config.boot.kernelVersion;
-
-  numPatches = length availablePatches;
+  availablePatches = import (patchesDir + /availablePatches.nix) { inherit lib; version = config.boot.kernelVersion; };
+  availablePatchesList = availablePatches.patchList;
+  patchVersion = availablePatches.patchVersion;
 
   kernel = config.boot.kernelPackage;
   kpatch-build = pkgs.callPackage (import ../../../packages/kpatch-build/default.nix) {};
 
-  moduleLoadGen = { moduleName, installModPath }:
-    ''
-      if [ ! -d /sys/kernel/livepatch/${moduleName} ]; then
-        echo live-patches: loading and applying ${moduleName}...
-        if ! insmod ${installModPath}; then
-          echo live-patches: loading and applying ${moduleName} FAILED
-        fi
-      fi
-    '';
+  patchName = "${toString patchVersion}-livepatched";
+  patchModuleName = "livepatch_${toString patchVersion}";
+  installModDir = "lib/modules/${kernel.modDirVersion}/extra";
+  installModPath = "${installModDir}/${patchModuleName}.ko";
 
-  moduleUnloadGen = { moduleName }:
-    ''
-      if [ -d /sys/kernel/livepatch/${moduleName} ] && [ -f /sys/kernel/livepatch/${moduleName}/enabled ]; then
-        echo -en live-patches: disabling ${moduleName}.. 
-        echo 0 > /sys/kernel/livepatch/${moduleName}/enabled 2>/dev/null
-        retries=91
-        while [ -d /sys/kernel/livepatch/${moduleName} ] && [ $retries -gt 0 ]; do
-          if [ "$(( $retries % 5 ))" -eq 0 ]; then
-            echo -en " $retries "
-          fi
-          transition=$(cat /sys/kernel/livepatch/${moduleName}/transition 2>/dev/null)
-          if [ "$transition" -eq 1 ] 2>/dev/null; then
-            echo -en .
-          else
-            echo -en ?
-          fi
-          retries=$(( $retries - 1 ))
-          sleep 1
-        done
-        echo
-      fi
-      retries=13
-      if [ -d /sys/module/${moduleName} ]; then
-        echo -en live-patches: unloading ${moduleName}.. 
-        while [ -d /sys/module/${moduleName} ] && [ $retries -gt 0 ]; do
-          if [ "$(( $retries % 3 ))" -eq 0 ]; then
-            echo -en " $retries "
-          fi
-          echo -en .
-          retries=$(( $retries - 1 ))
-          if ! rmmod ${moduleName} 2>/dev/null; then
-            sleep 0.2
-          fi
-          if [ "$retries" -eq 0 ]; then
-            echo -en "\nlive-patches: unloading ${moduleName}... FAILED"
-          fi
-        done
-        echo
-      fi
-    '';
-
-  moduleStatusGen = { moduleName, extraText ? "" }:
-    ''
-      s="["
-      if [ -d /sys/module/${moduleName} ]; then
-        s="$s loaded";
-        if [ -f /sys/kernel/livepatch/${moduleName}/enabled ] && \
-           [ "$(cat /sys/kernel/livepatch/${moduleName}/enabled 2>/dev/null)" == "1" ]; then
-          s="$s enabled"
-        fi
-        if [ "$(cat /sys/kernel/livepatch/${moduleName}/transition 2>/dev/null)" == "1" ]; then
-          s="$s transition"
-        fi
-      else
-        s="$s unloaded"
-      fi
-      printf "%-27s%s\n" "$s" " ] ${moduleName}${extraText}"
-    '';
-
-  buildLivePatch = { availablePatches, ccacheStdenv }:
-    let
-      patchName = "${toString numPatches}-${builtins.replaceStrings ["."] ["-"] (last availablePatches)}";
-      patchModuleName = "klp_${builtins.replaceStrings ["-"] ["_"] patchName}";
-    in ccacheStdenv.mkDerivation rec {
-      name = "livepatch-${kernel.modDirVersion}-kpatch-build";
-      version = toString numPatches;
+  buildLivePatch = { availablePatchesList, ccacheStdenv }:
+    ccacheStdenv.mkDerivation rec {
+      name = "${patchModuleName}-${kernel.modDirVersion}";
+      version = toString patchVersion;
       src = patchesDir;
 
       buildPhase = ''
@@ -154,58 +87,133 @@ LIVEPATCH_HEADER_END
 
         # command preview:
         echo kpatch-build -n ${patchModuleName} '' +
-      concatMapStringsSep " " (name: "${name}.patch") availablePatches +
+      concatMapStringsSep " " (name: "${name}.patch") availablePatchesList +
       ''; # we dont get a newline between this and the next line; wtf
         # actual command
         $kpb/kpatch-build/kpatch-build -s src -n ${patchModuleName} '' +
-      concatMapStringsSep " " (name: "$src/${name}.patch") availablePatches +
+      concatMapStringsSep " " (name: "$src/${name}.patch") availablePatchesList +
       '' || ((tail -n 150 $CACHEDIR/build.log || echo log not found at $CACHEDIR/build.log) && exit 1)
       '';
 
       nativeBuildInputs = with pkgs; [ perl bc nettools openssl rsync gmp
                           libmpc mpfr gawk zstd elfutils cpio bison flex ];
 
-      installModDir = "lib/modules/${kernel.modDirVersion}/extra";
-      installModPath = "${installModDir}/${patchModuleName}.ko";
       installPhase = ''
         mkdir -p $out/${installModDir};
         cp ${patchModuleName}.ko $out/${installModPath} || (ls -lah && exit 1)
 
-        cat > $out/moduleLoadContent <<moduleLoadContent
-          mkdir -p /lib/modules
-          ln -snf /run/current-system/kernel-modules/lib/modules/${kernel.modDirVersion} /lib/modules/${kernel.modDirVersion}.${patchName}
-          '' + moduleLoadGen { installModPath = "$out/${installModPath}";
-                               moduleName = patchModuleName; } + ''
-moduleLoadContent
 
-        cat > $out/moduleUnloadContent <<"moduleUnloadContent"
-          '' + moduleUnloadGen { moduleName = patchModuleName; } + ''
-moduleUnloadContent
 
-        cat > $out/moduleStatusContent <<"moduleStatusContent"
-          '' + moduleStatusGen {
-                 moduleName = patchModuleName;
-                 extraText = "(" + concatStringsSep " " availablePatches + ")";
-               } + ''
-moduleStatusContent
       '';
     };
 
-  patches = pkgs.callPackage buildLivePatch { inherit availablePatches; };
+  patches = pkgs.callPackage buildLivePatch { inherit availablePatchesList; };
+
+  moduleLoadGen = { moduleName, installModPath }:
+    let
+      modDetectDir = "/sys/kernel/livepatch/${moduleName}";
+    in ''
+      if [ ! -d ${modDetectDir} ]; then
+        echo live-patches: loading and applying ${moduleName}...
+        if ! insmod ${installModPath}; then
+          echo live-patches: loading and applying ${moduleName} FAILED
+        fi
+      fi
+    '';
+
+  moduleUnloadGen = { moduleName }:
+    let
+      modDetectDir = "/sys/kernel/livepatch/${moduleName}";
+    in ''
+      if [ -d ${modDetectDir} ] && [ -f ${modDetectDir}/enabled ]; then
+        echo -en live-patches: disabling ${moduleName}..
+        echo 0 > ${modDetectDir}/enabled 2>/dev/null
+        retries=91
+        while [ -d ${modDetectDir} ] && [ $retries -gt 0 ]; do
+          if [ "$(( $retries % 5 ))" -eq 0 ]; then
+            echo -en " $retries "
+          fi
+          transition=$(cat ${modDetectDir}/transition 2>/dev/null)
+          if [ "$transition" -eq 1 ] 2>/dev/null; then
+            echo -en .
+          else
+            echo -en ?
+          fi
+          retries=$(( $retries - 1 ))
+          sleep 1
+        done
+        echo
+      fi
+      retries=13
+      if [ -d /sys/module/${moduleName} ]; then
+        echo -en live-patches: unloading ${moduleName}..
+        while [ -d /sys/module/${moduleName} ] && [ $retries -gt 0 ]; do
+          if [ "$(( $retries % 3 ))" -eq 0 ]; then
+            echo -en " $retries "
+          fi
+          echo -en .
+          retries=$(( $retries - 1 ))
+          if ! rmmod ${moduleName} 2>/dev/null; then
+            sleep 0.2
+          fi
+          if [ "$retries" -eq 0 ]; then
+            echo -en "\nlive-patches: unloading ${moduleName}... FAILED"
+          fi
+        done
+        echo
+      fi
+    '';
+
+  moduleListGen = { moduleName }:
+  let
+    modDetectDir = "/sys/kernel/livepatch/${moduleName}";
+  in ''
+      s="["
+      if [ -d /sys/module/${moduleName} ]; then
+        s="$s loaded";
+        if [ -f ${modDetectDir}/enabled ] && \
+           [ "$(cat ${modDetectDir}/enabled 2>/dev/null)" == "1" ]; then
+          s="$s enabled"
+        fi
+        if [ "$(cat ${modDetectDir}/transition 2>/dev/null)" == "1" ]; then
+          s="$s transition"
+        fi
+      else
+        s="$s unloaded"
+      fi
+      printf "%-27s%s\n" "$s" " ] ${moduleName}"
+    '';
+
+  moduleStatusGen = { moduleName }:
+    moduleListGen { inherit moduleName; } +
+    foldl (x: y: "${x}\n${y}") "\n" (map (patch:
+          let
+            pVer = availablePatches.getPatchVersion patch;
+          in
+            "printf '%29s %s\n' 'contains:' '${patch.name}" +
+            optionalString (pVer > 1) "(v${toString pVer})" +
+                                          "'") availablePatches.filteredPatches);
 
   moduleLoadContent = ''
-    # Patches built with build-kpatch
-    . $(cat /etc/live-patches)/moduleLoadContent
+    livepatch=$(cat /etc/livepatch-store-path)
+    mkdir -p /lib/modules
+    ln -snf /run/current-system/kernel-modules/lib/modules/${kernel.modDirVersion} /lib/modules/${kernel.modDirVersion}.${patchName}
+    '' + moduleLoadGen { installModPath = "$livepatch/${installModPath}";
+                         moduleName = patchModuleName; } + ''
   '';
 
   moduleUnloadContent = ''
+    livepatch=$(cat /etc/livepatch-store-path)
     # Patches built with build-kpatch
-    . $(cat /etc/live-patches)/moduleUnloadContent
-  '';
+    '' + moduleUnloadGen { moduleName = patchModuleName; } + "\n";
+
+  moduleListContent = ''
+    livepatch=$(cat /etc/livepatch-store-path)
+    '' + moduleListGen { moduleName = patchModuleName; } + "\n";
 
   moduleStatusContent = ''
-    . $(cat /etc/live-patches)/moduleStatusContent
-  '';
+    livepatch=$(cat /etc/livepatch-store-path)
+    '' + moduleStatusGen { moduleName = patchModuleName; } + "\n";
 
   live-patches-util = pkgs.writeScriptBin "live-patches" ''
     case "$1" in
@@ -215,7 +223,10 @@ moduleStatusContent
     unload)
       ${moduleUnloadContent}
       ;;
-    status|list)
+    list)
+      ${moduleListContent}
+      ;;
+    status)
       ${moduleStatusContent}
       ;;
     *)
@@ -236,7 +247,7 @@ in
     };
   };
   config = {
-    environment.etc."live-patches".text = toString patches;
+    environment.etc."livepatch-store-path".text = toString patches;
     environment.systemPackages = [ live-patches-util ];
     runit.services.live-patches = {
       run = optionalString (cfg.enable) "live-patches load && sleep inf";
