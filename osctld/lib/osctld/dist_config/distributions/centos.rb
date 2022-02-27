9a3df191d58f10e54d90776216e60d8832be80a2
Jakub Skokan <jakub.skokan@havefun.cz>
2022-02-27 18:48:46 +0100

    osctld: distconfig: dynamic network configuration on centos

--- osctld/lib/osctld/dist_config/distributions/centos.rb
+++ osctld/lib/osctld/dist_config/distributions/centos.rb
@@ -7,13 +7,10 @@ module OsCtld
     class Configurator < DistConfig::Distributions::RedHat::Configurator
       protected
       def network_class
-        if version.start_with?('stream-') \
-           || version == 'latest-stream' \
-           || version.to_i >= 8
-          DistConfig::Network::RedHatNetworkManager
-        else
-          DistConfig::Network::RedHatInitScripts
-        end
+        [
+          DistConfig::Network::RedHatNetworkManager,
+          DistConfig::Network::RedHatInitScripts,
+        ]
       end
     end
   end
