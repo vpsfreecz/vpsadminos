6b3785d83f0bed01e8e256f026100c51d4bccdf3
Jakub Skokan <jakub.skokan@havefun.cz>
2022-02-27 18:49:07 +0100

    osctld: distconfig: dynamic network configuration on fedora

--- osctld/lib/osctld/dist_config/distributions/fedora.rb
+++ osctld/lib/osctld/dist_config/distributions/fedora.rb
@@ -7,11 +7,10 @@ module OsCtld
     class Configurator < DistConfig::Distributions::RedHat::Configurator
       protected
       def network_class
-        if version.to_i >= 30
-          DistConfig::Network::RedHatNetworkManager
-        else
-          DistConfig::Network::RedHatInitScripts
-        end
+        [
+          DistConfig::Network::RedHatNetworkManager,
+          DistConfig::Network::RedHatInitScripts,
+        ]
       end
     end
   end
