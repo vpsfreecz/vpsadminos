fdcc2251a71eb1ab789fa16abaf8a90fbe1368c9
Pavel Snajdr <snajpa@snajpa.net>
2022-03-29 11:45:48 +0200

    os: KLP: 5.10.98: cumulative upgrade to 5.10.109
    [vpsAdminOS] livepatch: fixup for important fixes from 5.10.109
    llc: only change llc->dev when bind() succeeds
    netfilter: nf_tables: initialize registers in nft_do_chain()
    llc: fix netdevice reference leaks in llc_ui_bind()
    cgroup-v1: Correct privileges check in release_agent writes
    cgroup: Use open-time cgroup namespace for process migration perm checks
    cgroup: Allocate cgroup_file_ctx for kernfs_open_file->priv
    net: ipv6: fix skb_over_panic in __ip6_append_data
    net/packet: fix slab-out-of-bounds access in packet_recvmsg()
    mm: swap: get rid of livelock in swapin readahead

--- os/livepatches/availablePatches.nix
+++ os/livepatches/availablePatches.nix
@@ -24,6 +24,9 @@ let
     { name = "bp-5.10.106-107-cumulative";
       filterFn = availableFor "5.10.98";
     }
+    { name = "bp-5.10.108-109-cumulative";
+      filterFn = availableFor "5.10.98";
+    }
     { name = "ping-group-range";
       filterFn = availableFor "5.10.98";
       version = 7;
