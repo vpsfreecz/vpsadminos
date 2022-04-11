b5ddedc8843f5e13200fcbd95e8c7e7d779bf8b2
Jakub Skokan <jakub.skokan@havefun.cz>
2022-04-11 09:30:51 +0200

    image-scripts: tests/passwd_started: add delay after start on nixos due to start menu

--- image-scripts/tests/passwd_started.sh
+++ image-scripts/tests/passwd_started.sh
@@ -1,2 +1,8 @@
 osctl ct start $CTID || fail "unable to start container"
+
+# On NixOS, we have to wait a bit for the start menu to pass, because
+# we need the initial system activation to take place before the password
+# can be set.
+[ "$DISTNAME" == "nixos" ] && sleep 10
+
 osctl ct passwd $CTID root suCHS3crET || fail "unable to set password"
