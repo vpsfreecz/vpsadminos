17eff091dc89bc1731b247f0c367300aed4f8c14
Jakub Skokan <jakub.skokan@havefun.cz>
2022-04-12 15:44:07 +0200

    libosctl: iteration-only os process list, option passing

--- libosctl/lib/libosctl/process_list.rb
+++ libosctl/lib/libosctl/process_list.rb
@@ -1,16 +1,34 @@
 module OsCtl::Lib
   class ProcessList < Array
-    def initialize(&block)
-      list_processes(&block)
+    # Iterate over all processes
+    # @param opts [Hash] options passed to {OsProcess}
+    # @yieldparam [OsProcess] process
+    def self.each(**opts, &block)
+      new do |p|
+        block.call(p)
+        next(false)
+      end
+
+      nil
+    end
+
+    # Create an array of system processes
+    #
+    # The block can be used to filter out processes for which it returns false.
+    #
+    # @param opts [Hash] options passed to {OsProcess}
+    # @yieldparam [OsProcess] process
+    def initialize(**opts, &block)
+      list_processes(**opts, &block)
     end
 
     protected
-    def list_processes
+    def list_processes(**opts)
       Dir.foreach('/proc') do |entry|
         next if /^\d+$/ !~ entry || !Dir.exist?(File.join('/proc', entry))
 
         begin
-          p = OsProcess.new(entry.to_i)
+          p = OsProcess.new(entry.to_i, **opts)
           next if block_given? && !yield(p)
 
           self << p
