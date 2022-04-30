98b4ac80b9843e3987aa2fdca22eafab2b706e4a
Jakub Skokan <jakub.skokan@havefun.cz>
2022-04-30 09:19:36 +0200

    osctl-exporter: refactor collector setup, support for multiple threads

--- osctl-exporter/lib/osctl/exporter/collector.rb
+++ osctl-exporter/lib/osctl/exporter/collector.rb
@@ -7,6 +7,18 @@ module OsCtl::Exporter
     include OsCtl::Lib::Utils::Log
     include OsCtl::Lib::Utils::File
 
+    CollectorConfig = Struct.new(
+      :collector_class,
+      :require_osctld,
+      :interval,
+      :collector_instance,
+    )
+
+    ThreadConfig = Struct.new(
+      :thread,
+      :queue,
+    )
+
     class << self
       %i(start stop).each do |m|
         define_method(m) do
@@ -16,35 +28,59 @@ module OsCtl::Exporter
     end
 
     def initialize
-      @client = OsCtldClient.new
-      @queue = OsCtl::Lib::Queue.new
+      @threads = []
       @registry = Prometheus::Client.registry
-      @any_collectors = [
-        Collectors::OsCtld,
-        Collectors::Exportfs,
-      ].map { |klass| klass.new(registry) }
-      @connected_collectors = [
-        Collectors::Pool,
-        Collectors::Container,
-        Collectors::HealthCheck,
-      ].map { |klass| klass.new(registry) }
+
+      @collectors = [
+        CollectorConfig.new(Collectors::OsCtld, false, 30),
+        CollectorConfig.new(Collectors::Pool, true, 30),
+        CollectorConfig.new(Collectors::Container, true, 30),
+        CollectorConfig.new(Collectors::Exportfs, false, 60),
+        CollectorConfig.new(Collectors::HealthCheck, true, 120),
+      ]
+
+      @collectors.each do |c|
+        c.collector_instance = c.collector_class.new(registry)
+      end
     end
 
     def start
-      @thread = Thread.new do
-        loop do
-          collect
-          break if queue.pop(timeout: 30) == :stop
+      unique_intervals = collectors.map(&:interval).uniq
+
+      @threads = unique_intervals.map do |interval|
+        queue = OsCtl::Lib::Queue.new
+        thread_collectors = collectors.select { |c| c.interval == interval }
+        client = OsCtldClient.new
+
+        log(
+          :info,
+          "Starting collector thread with #{interval} second interval for: "+
+          "#{thread_collectors.map { |c| c.collector_class.to_s.split('::').last }.join(', ')}"
+        )
+
+        thread = Thread.new do
+          loop do
+            collect(client, thread_collectors)
+            break if queue.pop(timeout: interval) == :stop
+          end
         end
+
+        ThreadConfig.new(thread, queue)
       end
+
+      nil
     end
 
     def stop
-      if thread
-        queue << :stop
-        thread.join
-        @thread = nil
+      return if threads.empty?
+
+      threads.delete_if do |thread_cfg|
+        thread_cfg.queue << :stop
+        thread_cfg.thread.join
+        true
       end
+
+      nil
     end
 
     def log_type
@@ -52,15 +88,18 @@ module OsCtl::Exporter
     end
 
     protected
-    attr_reader :client, :queue, :thread, :registry,
-      :any_collectors, :connected_collectors
+    attr_reader :threads, :registry, :collectors
 
-    def collect
+    def collect(client, thread_collectors)
       client.try_to_connect do
-        any_collectors.each { |c| c.run_collect(client) }
+        thread_collectors.reject(&:require_osctld).each do |c|
+          c.collector_instance.run_collect(client)
+        end
 
         if client.connected?
-          connected_collectors.each { |c| c.run_collect(client) }
+          thread_collectors.select(&:require_osctld).each do |c|
+            c.collector_instance.run_collect(client)
+          end
         end
       end
     end
