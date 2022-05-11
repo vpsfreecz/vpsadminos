a431ea80dd708c5af872411cc6c3fedb2e466c4c
Jakub Skokan <jakub.skokan@havefun.cz>
2022-05-11 07:37:54 +0200

    osctld: distconfig: add support for keyfile-based network manager

---
+++ osctld/lib/osctld/dist_config/network/network_manager.rb
@@ -0,0 +1,106 @@
+require 'osctld/dist_config/network/base'
+
+module OsCtld
+  # Configure network using NetworkManager keyfiles
+  class DistConfig::Network::NetworkManager < DistConfig::Network::Base
+    def usable?
+      return false if Dir.exist?(File.join(rootfs, 'etc/sysconfig/network-scripts'))
+      return false unless Dir.exist?(File.join(rootfs, 'etc/NetworkManager/conf.d'))
+      return false unless Dir.exist?(File.join(rootfs, 'etc/NetworkManager/system-connections'))
+
+      service = 'NetworkManager.service'
+
+      # Check the service is not masked
+      return false if systemd_service_masked?(service)
+
+      # Check the service is enabled
+      return false unless systemd_service_enabled?(service, 'multi-user.target')
+
+      true
+    end
+
+    def configure(netifs)
+      netifs.each do |netif|
+        do_create_connection(netif)
+      end
+
+      setup_nm(netifs)
+    end
+
+    # Cleanup old config files
+    def remove_netif(netifs, netif)
+      do_remove_connection(netif.name)
+      setup_nm(netifs)
+    end
+
+    # Rename config files
+    def rename_netif(netifs, netif, old_name)
+      do_remove_connection(old_name)
+      do_create_connection(netif)
+      setup_nm(netifs)
+    end
+
+    protected
+    def do_create_connection(netif)
+      tpl_base = File.join('dist_config/network/network_manager')
+      ct_base = File.join(rootfs, 'etc', 'NetworkManager/system-connections')
+      keyfile = File.join(ct_base, "#{netif.name}.nmconnection")
+
+      return unless writable?(keyfile)
+
+      OsCtld::ErbTemplate.render_to_if_changed(
+        File.join(tpl_base, netif.type.to_s),
+        {netif: netif},
+        keyfile,
+        perm: 0600,
+      )
+    end
+
+    def do_remove_connection(name)
+      base = File.join(rootfs, 'etc', 'NetworkManager', 'system-connections')
+      files = [
+        "#{name}.nmconnection",
+      ]
+
+      files.each do |f|
+        path = File.join(base, f)
+        next if !File.exist?(path) || !writable?(path)
+
+        File.unlink(path)
+      end
+    end
+
+    def setup_nm(netifs)
+      generate_nm_conf(netifs)
+      generate_nm_udev_rules(netifs)
+    end
+
+    def generate_nm_conf(netifs)
+      conf_d = File.join(rootfs, 'etc', 'NetworkManager', 'conf.d')
+      return unless Dir.exist?(conf_d)
+
+      file = File.join(conf_d, 'osctl.conf')
+      return unless writable?(file)
+
+      OsCtld::ErbTemplate.render_to_if_changed(
+        File.join('dist_config/network/network_manager/nm_conf'),
+        {netifs: netifs},
+        file,
+      )
+    end
+
+    def generate_nm_udev_rules(netifs)
+      rules_d = File.join(rootfs, 'etc', 'udev', 'rules.d')
+      return unless Dir.exist?(rules_d)
+
+      file = File.join(rules_d, '86-osctl.rules')
+      return unless writable?(file)
+
+      OsCtld::ErbTemplate.render_to_if_changed(
+        File.join('dist_config/network/network_manager/udev_rules'),
+        {netifs: netifs},
+        file,
+      )
+    end
+  end
+end
