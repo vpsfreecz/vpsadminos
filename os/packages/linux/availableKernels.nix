92020dfd6c2e1a26aee90fe7a6a7cce34059b41f
Pavel Snajdr <snajpa@snajpa.net>
2022-05-21 16:09:10 +0200

    os: Linux 5.10.117

--- os/packages/linux/availableKernels.nix
+++ os/packages/linux/availableKernels.nix
@@ -2,6 +2,14 @@
 let
   defaultKernelVersion = "5.10.98";
   kernels = {
+    "5.10.117" = {
+      url = linuxGhUrl vpsfGh "6a7f96cfb71f39f91c8320d74987271071e698e2";
+      sha256 = "sha256-MoFyHc7EmmF17A7yaPO0STOkzvusOUBsHhiyb7S3Zls=";
+      zfs = {
+        rev = "cf79c5d7e3185db69f41452e3ebbe0576a8c5dd5";
+        sha256 = "sha256-HLizndgrsOboRkIlmCfEfGFwoXweamkkddKdnQxSWWU=";
+      };
+    };
     "5.10.98" = {
       url = linuxGhUrl vpsfGh "c0533a6a6b4af2f37863ad1626edaced4dc4edc6";
       sha256 = "sha256-EqoVOFT/CxIV4zd2eOCM78TBBjUYU7Jrj+86yOwaK0A=";
