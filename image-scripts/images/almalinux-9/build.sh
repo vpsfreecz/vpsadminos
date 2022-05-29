928b5a785d185ba5c070ca91685d0996457f82c2
Jakub Skokan <jakub.skokan@havefun.cz>
2022-05-29 08:31:39 +0200

    image-scripts: Add AlmaLinux 9

---
+++ image-scripts/images/almalinux-9/build.sh
@@ -0,0 +1,20 @@
+. "$IMAGEDIR/config.sh"
+POINTVER=9.0
+RELEASE=https://repo.almalinux.org/almalinux/${POINTVER}/BaseOS/x86_64/os/Packages/almalinux-release-${POINTVER}-3.el8.x86_64.rpm
+BASEURL=https://repo.almalinux.org/almalinux/${POINTVER}/BaseOS/x86_64/os/
+
+# CentOS 8 does not seem to have an updates repo, so this variable is used to
+# add AppStream repository just for the installation process.
+UPDATES=https://repo.almalinux.org/almalinux/${POINTVER}/AppStream/x86_64/os/
+
+GROUPNAME='core'
+EXTRAPKGS='vim man'
+
+. $INCLUDE/redhat-family.sh
+
+bootstrap
+configure-common
+configure-redhat-common
+configure-rhel-9
+run-configure
+set-initcmd "/sbin/init" "systemd.unified_cgroup_hierarchy=0"
