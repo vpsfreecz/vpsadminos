808d16f953deae818a14e76f4827e3906d1155b9
Jakub Skokan <jakub.skokan@havefun.cz>
2022-05-29 08:41:13 +0200

    image-scripts: almalinux-9: fix release rpm name

--- image-scripts/images/almalinux-9/build.sh
+++ image-scripts/images/almalinux-9/build.sh
@@ -1,6 +1,6 @@
 . "$IMAGEDIR/config.sh"
 POINTVER=9.0
-RELEASE=https://repo.almalinux.org/almalinux/${POINTVER}/BaseOS/x86_64/os/Packages/almalinux-release-${POINTVER}-3.el8.x86_64.rpm
+RELEASE=https://repo.almalinux.org/almalinux/${POINTVER}/BaseOS/x86_64/os/Packages/almalinux-release-${POINTVER}-3.el9.x86_64.rpm
 BASEURL=https://repo.almalinux.org/almalinux/${POINTVER}/BaseOS/x86_64/os/
 
 # CentOS 8 does not seem to have an updates repo, so this variable is used to
