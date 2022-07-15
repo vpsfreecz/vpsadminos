6327ba233d512488b49ba2722d49ebd2bbeaf588
Jakub Skokan <jakub.skokan@havefun.cz>
2022-07-15 14:18:46 +0200

    tests/distributions: add nixos

--- tests/distributions.nix
+++ tests/distributions.nix
@@ -14,6 +14,8 @@ let
     gentoo_openrc    = { distribution = "gentoo";    version = "latest-openrc";   };
     gentoo_systemd   = { distribution = "gentoo";    version = "latest-systemd";  };
     gentoo_musl      = { distribution = "gentoo";    version = "latest-musl";     };
+    nixos_stable     = { distribution = "nixos";     version = "stable";          };
+    nixos_unstable   = { distribution = "nixos";     version = "unstable";        };
     opensuse         = { distribution = "opensuse";  version = "latest";          };
     slackware        = { distribution = "slackware"; version = "latest";          };
     ubuntu_1804      = { distribution = "ubuntu";    version = "18.04";           };
@@ -34,6 +36,8 @@ in {
     debian_testing
     fedora
     gentoo_systemd
+    nixos_stable
+    nixos_unstable
     opensuse
     slackware
     ubuntu_1804
@@ -58,6 +62,8 @@ in {
     debian_testing
     fedora
     gentoo_systemd
+    nixos_stable
+    nixos_unstable
     opensuse
     ubuntu_1804
     ubuntu_2004
