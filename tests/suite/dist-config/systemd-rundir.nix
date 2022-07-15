d342ddbb51275b1b07a729a98b97c77e96fc88f4
Jakub Skokan <jakub.skokan@havefun.cz>
2022-07-15 08:35:49 +0200

    tests: add dist-config/{nonsystemd,systemd}-rundir
    (cherry picked from commit 5772f9457592179f42395bb0b254dbfe967eba44)

---
+++ tests/suite/dist-config/systemd-rundir.nix
@@ -0,0 +1,45 @@
+import ../../make-template.nix ({ distribution, version }: rec {
+  instance = "${distribution}-${version}";
+
+  test = pkgs:
+    let
+      runScript = pkgs.writeScript "dist-config-systemd-rundir-script.sh" ''
+        #!/bin/sh
+        fail() {
+          echo $@
+          exit 1
+        }
+
+        [ -d /run ] || fail "/run not found"
+
+        grep -qx "tmpfs /run tmpfs rw,nosuid,nodev,relatime,mode=755 0 0" /proc/mounts \
+          || fail "/run not found in /proc/mounts or has unexpected options"
+
+        [ -d /run/udev ] || fail "/run/udev" not found
+
+        [ -e /run/udev/control ] || fail "/run/udev/control not found"
+
+        exit 0
+      '';
+    in {
+      name = "dist-config-systemd-rundir@${instance}";
+
+      description = ''
+        Test that containers with ${distribution}-${version} have systemd have /run
+        pre-mounted before systemd is started
+      '';
+
+      machine = import ../../machines/tank.nix pkgs;
+
+      testScript = ''
+        machine.wait_for_osctl_pool("tank")
+        machine.wait_until_online
+
+        machine.succeeds(
+          "osctl ct new --distribution ${distribution} --version ${version} testct",
+        )
+        machine.push_file("${runScript}", "/tmp/test-script.sh")
+        machine.succeeds("osctl ct runscript -r testct /tmp/test-script.sh")
+      '';
+    };
+})
