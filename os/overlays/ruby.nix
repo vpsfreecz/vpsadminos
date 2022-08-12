758febc76b166b9f236e10cb300cac9e7d1a5eef
Jakub Skokan <jakub.skokan@havefun.cz>
2022-08-12 19:54:04 +0200

    os: overlay bundix for ruby 3.0 support
    (cherry picked from commit 990b5715e8159490b26a6daa9a3a2ba2527b0788)

--- os/overlays/ruby.nix
+++ os/overlays/ruby.nix
@@ -36,5 +36,16 @@ in
         };
       }) {};
 
+  bundix = super.bundix.overrideAttrs (oldAttrs: rec {
+    name = "bundix-${version}";
+    version = "master-3d7820ef";
+    src = super.fetchFromGitHub {
+      owner = "nix-community";
+      repo = "bundix";
+      rev = "3d7820efdd77281234182a9b813c2895ef49ae1f";
+      sha256 = "sha256-0CMDJR3xfewNuDthm3fEh6UPeRH9PURYxJ0PI1WPv4U=";
+    };
+  });
+
   osBundlerApp = super.callPackage ../packages/os-bundler-app {};
 }
