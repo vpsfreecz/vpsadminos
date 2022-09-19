5ded52a66a43cc5270762046e23b720f9d6a5080
Jakub Skokan <jakub.skokan@havefun.cz>
2022-09-19 17:47:13 +0200

    os: make it possible to disable system-wide lxcfs instance

--- os/modules/virtualisation/lxcfs.nix
+++ os/modules/virtualisation/lxcfs.nix
@@ -1,17 +1,27 @@
 { config, lib, pkgs, utils, ... }:
 with utils;
 with lib;
-
-{
+let
+  cfg = config.services.lxcfs;
+in {
   ###### interface
 
   options = {
+    services.lxcfs = {
+      enable = mkOption {
+        type = types.bool;
+        default = true;
+        description = ''
+          Enable system-wide LXCFS instance
+        '';
+      };
+    };
   };
 
   ###### implementation
 
   config = {
-    runit.services.lxcfs = {
+    runit.services.lxcfs = mkIf cfg.enable {
       run = ''
         mkdir -p /var/lib/lxcfs
         umount /var/lib/lxcfs 2> /dev/null
