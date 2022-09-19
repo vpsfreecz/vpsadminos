808f2a60b64537893962ae95a323a52b8834b73f
Jakub Skokan <jakub.skokan@havefun.cz>
2022-09-19 18:19:47 +0200

    tests: update lxcfs for per-container lxcfs instances
    (cherry picked from commit 3a78231ef02feb792c9199b7770c90a61411ad80)

--- tests/suite/lxcfs/overlays.nix
+++ tests/suite/lxcfs/overlays.nix
@@ -8,19 +8,16 @@ import ../../make-template.nix ({ distribution, version }: rec {
       Test LXCFS is mounted in containers with ${distribution}-${version}
     '';
 
-    machine = import ../../machines/tank.nix pkgs;
+    machine = import ../../machines/with-tank.nix {
+      inherit pkgs;
+      config =
+        { config, ... }:
+        {
+          services.lxcfs.enable = false;
+        };
+    };
 
     testScript = ''
-      machine.wait_for_osctl_pool("tank")
-      machine.wait_until_online
-
-      machine.all_succeed(
-        "osctl ct new --distribution ${distribution} --version ${version} testct",
-        "osctl ct start testct",
-      )
-
-      _, output = machine.succeeds("osctl ct exec testct cat /proc/mounts")
-
       be_mounted = %w(
         /proc/cpuinfo
         /proc/diskstats
@@ -28,6 +25,7 @@ import ../../make-template.nix ({ distribution, version }: rec {
         /proc/stat
         /proc/uptime
         /sys/devices/system/cpu/online
+        /var/lib/lxcfs
       )
 
       be_unmounted = %w(
@@ -35,16 +33,52 @@ import ../../make-template.nix ({ distribution, version }: rec {
         /proc/swaps
       )
 
-      be_mounted.each do |f|
-        if /^lxcfs #{Regexp.escape(f)} fuse\.lxcfs / !~ output
-          fail "#{f} not mounted"
+      check_mounted = Proc.new do |output|
+        be_mounted.each do |f|
+          if /^lxcfs #{Regexp.escape(f)} fuse\.lxcfs / !~ output
+            fail "#{f} not mounted"
+          end
+        end
+
+        be_unmounted.each do |f|
+          if /^lxcfs #{Regexp.escape(f)} fuse\.lxcfs / =~ output
+            fail "#{f} mounted"
+          end
         end
       end
 
-      be_unmounted.each do |f|
-        if /^lxcfs #{Regexp.escape(f)} fuse\.lxcfs / =~ output
-          fail "#{f} mounted"
-        end
+      machine.wait_for_osctl_pool("tank")
+      machine.wait_until_online
+
+      # LXCFS is enabled by default
+      machine.all_succeed(
+        "osctl ct new --distribution ${distribution} --version ${version} testct",
+        "osctl ct exec -r testct mkdir -p /var/lib/lxcfs",
+      )
+
+      # Test exec and runscript
+      _, output = machine.succeeds("osctl ct exec -r testct cat /proc/mounts")
+      check_mounted.call(output)
+
+      _, output = machine.succeeds("echo cat /proc/mounts | osctl ct runscript -r testct -")
+      check_mounted.call(output)
+
+      # Test a running container
+      machine.succeeds("osctl ct start testct")
+
+      _, output = machine.succeeds("osctl ct exec testct cat /proc/mounts")
+      check_mounted.call(output)
+
+      # Disable LXCFS
+      machine.all_succeed(
+        "osctl ct unset lxcfs testct",
+        "osctl ct restart testct",
+      )
+
+      _, output = machine.succeeds("osctl ct exec testct cat /proc/mounts")
+
+      if /lxcfs/ =~ output
+        fail "found lxcfs mount when it should be disabled: #{output.inspect}"
       end
     '';
   };
