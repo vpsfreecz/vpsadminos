78ae3f4ca11f13306c9daa5356bcaf5e706c6dd7
Jakub Skokan <jakub.skokan@havefun.cz>
2022-09-20 21:39:33 +0200

    osctl ct ls/show: optionally read container's real hostname
    (cherry picked from commit d1d5743e3ad9d3849e15188bf44f71d610f8c863)

--- osctld/lib/osctld/commands/container/show.rb
+++ osctld/lib/osctld/commands/container/show.rb
@@ -12,7 +12,10 @@ module OsCtld
       ct = DB::Containers.find(opts[:id], opts[:pool])
       ct || error!('container not found')
 
-      ok(ct.export)
+      data = ct.export
+      data[:hostname_readout] = ct.read_hostname if opts[:read_hostname]
+
+      ok(data)
     end
   end
 end
