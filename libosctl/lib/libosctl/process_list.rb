6eb7df9aa48cd331869fa9002f5e629aedf0a9d1
Jakub Skokan <jakub.skokan@havefun.cz>
2022-09-22 21:22:54 +0200

    libosctl: read os processes in parallel

--- libosctl/lib/libosctl/process_list.rb
+++ libosctl/lib/libosctl/process_list.rb
@@ -1,40 +1,118 @@
 module OsCtl::Lib
-  class ProcessList < Array
+  class ProcessList
     # Iterate over all processes
     # @param opts [Hash] options passed to {OsProcess}
+    # @option opts [Integer, :auto] :threads number of threads to parse processes in
     # @yieldparam [OsProcess] process
     def self.each(**opts, &block)
-      new(**opts) do |p|
-        block.call(p)
-        next(false)
-      end
-
-      nil
+      new(**opts).each(&block)
     end
 
     # Create an array of system processes
     #
     # The block can be used to filter out processes for which it returns false.
+    # The process list is build in parallel, depending on the `:threads` option,
+    # so the block can be invoked in parallel as well.
     #
     # @param opts [Hash] options passed to {OsProcess}
+    # @option opts [Integer, :auto] :threads number of threads to parse processes in
     # @yieldparam [OsProcess] process
     def initialize(**opts, &block)
-      list_processes(**opts, &block)
+      @results = list_processes(**opts, &block)
     end
 
-    protected
-    def list_processes(**opts)
-      Dir.foreach('/proc') do |entry|
-        next if /^\d+$/ !~ entry || !Dir.exist?(File.join('/proc', entry))
+    def to_enum
+      size = Proc.new do
+        @results.inject(0) { |acc, result| acc + result.size }
+      end
 
-        begin
-          p = OsProcess.new(entry.to_i, **opts)
-          next if block_given? && !yield(p)
+      Enumerator.new(size) do |yielder|
+        result_it = @results.each
 
-          self << p
-        rescue Exceptions::OsProcessNotFound
-          next
+        loop do
+          result = result_it.next
+          process_it = result.each
+
+          loop do
+            yielder << process_it.next
+          end
         end
+      end
+    end
+
+    def each(&block)
+      to_enum.each(&block)
+    end
+
+    include Enumerable
+
+    protected
+    def list_processes(threads: :auto, **opts, &block)
+      num_threads =
+        case threads
+        when :auto
+          8
+        else
+          threads
+        end
+
+      if num_threads == 1
+        list_processes_serial(**opts, &block)
+      else
+        list_processes_parallel(num_threads, **opts, &block)
+      end
+    end
+
+    def list_processes_serial(**opts, &block)
+      result = []
+
+      Dir.foreach('/proc') do |entry|
+        process = parse_process(entry, opts, &block)
+        result << process if process
+      end
+
+      [result]
+    end
+
+    def list_processes_parallel(num_threads, **opts, &block)
+      queue = ::Queue.new
+      results = num_threads.times.map { Array.new }
+
+      work_threads = num_threads.times.map do |i|
+        Thread.new { process_reader(queue, results[i], block, opts) }
+      end
+
+      Dir.foreach('/proc') do |entry|
+        queue << entry
+      end
+
+      num_threads.times { queue << :stop }
+      work_threads.each(&:join)
+
+      results
+    end
+
+    def process_reader(queue, result, block, opts)
+      loop do
+        entry = queue.pop
+        return if entry == :stop
+
+        process = parse_process(entry, opts, &block)
+        next if process.nil?
+
+        result << process
+      end
+    end
+
+    def parse_process(entry, opts)
+      return if /^\d+$/ !~ entry || !Dir.exist?(File.join('/proc', entry))
+
+      begin
+        p = OsProcess.new(entry.to_i, **opts)
+        return if block_given? && !yield(p)
+        return p
+      rescue Exceptions::OsProcessNotFound
+        return
       end
     end
   end
