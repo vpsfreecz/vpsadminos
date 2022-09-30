cfdef63df5504b8d5d512c63872e3679d4492daf
Jakub Skokan <jakub.skokan@havefun.cz>
2022-09-30 20:45:46 +0200

    libosctl: add system uptime reader

---
+++ libosctl/lib/libosctl/uptime.rb
@@ -0,0 +1,26 @@
+module OsCtl::Lib
+  # Read `/proc/uptime`
+  class Uptime
+    # @return [Float] uptime in seconds
+    attr_reader :uptime
+
+    # @return [Float] idle time in seconds
+    attr_reader :idle_time
+
+    # @return [Time] time of boot
+    attr_reader :booted_at
+
+    def initialize(path: '/proc/uptime')
+      parse(path)
+    end
+
+    protected
+    def parse(path)
+      values = File.read(path).strip.split
+
+      @uptime = values[0].to_f
+      @idle_time = values[1].to_f
+      @booted_at = Time.now - @uptime
+    end
+  end
+end
