d8ec57327c966a9fd33094ab5ca7b41e4fcfe6a7
Jakub Skokan <jakub.skokan@havefun.cz>
2022-10-06 16:53:02 +0200

    libosctl: add SysConf to as interface to Etc.sysconf
    (cherry picked from commit d34a720f316e45c8532343ed908d15e45e3e669b)

---
+++ libosctl/lib/libosctl/sys_conf.rb
@@ -0,0 +1,42 @@
+require 'etc'
+require 'singleton'
+
+module OsCtl::Lib
+  class SysConf
+    include Singleton
+
+    VALUES = %i(page_size tics_per_second)
+
+    class << self
+      VALUES.each do |v|
+        define_method(v) { instance.send(v) }
+      end
+    end
+
+    def initialize
+      @values = {}
+    end
+
+    # @param name [Symbol]
+    def get(name)
+      unless VALUES.include?(name)
+        raise ArgumentError, "#{name.inspect} is not known"
+      end
+
+      @values[name] ||= send(:"get_#{name}")
+    end
+
+    VALUES.each do |v|
+      define_method(v) { get(v) }
+    end
+
+    protected
+    def get_page_size
+      Etc.sysconf(Etc::SC_PAGESIZE)
+    end
+
+    def get_tics_per_second
+      Etc.sysconf(Etc::SC_CLK_TCK)
+    end
+  end
+end
