0093ad217d3c452198787529e48a9d61fc0ce502
Jakub Skokan <jakub.skokan@havefun.cz>
2022-10-11 13:36:31 +0200

    osctl-exporter: export values of all txgs reported by zfs

--- osctl-exporter/lib/osctl/exporter/collectors/zpool_txgs.rb
+++ osctl-exporter/lib/osctl/exporter/collectors/zpool_txgs.rb
@@ -18,31 +18,55 @@ module OsCtl::Exporter
 
       BYTES.each do |v|
         add_metric(
+          "zpool_txgs_last_#{v}_bytes",
+          :gauge,
+          :"zpool_txgs_last_#{v}_bytes",
+          docstring: "#{v} in the last txg in bytes",
+          labels: [:pool],
+        )
+
+        add_metric(
           "zpool_txgs_#{v}_bytes",
           :gauge,
           :"zpool_txgs_#{v}_bytes",
           docstring: "#{v} in bytes",
-          labels: [:pool],
+          labels: [:pool, :txg],
         )
       end
 
       OPERATIONS.each do |v|
         add_metric(
+          "zpool_txgs_last_#{v}",
+          :gauge,
+          :"zpool_txgs_last_#{v}",
+          docstring: "Number of operations in the last txg",
+          labels: [:pool],
+        )
+
+        add_metric(
           "zpool_txgs_#{v}",
           :gauge,
           :"zpool_txgs_#{v}",
           docstring: "Number of operations",
-          labels: [:pool],
+          labels: [:pool, :txg],
         )
       end
 
       TIMES.each do |v|
         add_metric(
+          "zpool_txgs_last_#{v}_nanoseconds",
+          :gauge,
+          :"zpool_txgs_last_#{v}_nanoseconds",
+          docstring: "#{v} of the last txg in nanoseconds",
+          labels: [:pool],
+        )
+
+        add_metric(
           "zpool_txgs_#{v}_nanoseconds",
           :gauge,
           :"zpool_txgs_#{v}_nanoseconds",
           docstring: "#{v} in nanoseconds",
-          labels: [:pool],
+          labels: [:pool, :txg],
         )
       end
     end
@@ -55,25 +79,50 @@ module OsCtl::Exporter
 
         @zpool_txgs_count.set(last_txg.txg, labels: {pool: pool})
 
+        # Info about the last txg
         BYTES.each do |v|
-          metrics["zpool_txgs_#{v}_bytes"].set(
+          metrics["zpool_txgs_last_#{v}_bytes"].set(
             last_txg.send(v),
             labels: {pool: pool},
           )
         end
 
         OPERATIONS.each do |v|
-          metrics["zpool_txgs_#{v}"].set(
+          metrics["zpool_txgs_last_#{v}"].set(
             last_txg.send(v),
             labels: {pool: pool},
           )
         end
 
         TIMES.each do |v|
-          metrics["zpool_txgs_#{v}_nanoseconds"].set(
+          metrics["zpool_txgs_last_#{v}_nanoseconds"].set(
             last_txg.send(:"#{v}_ns"),
             labels: {pool: pool},
           )
+        end
+
+        # Info about all txgs
+        txgs.each do |txg|
+          BYTES.each do |v|
+            metrics["zpool_txgs_#{v}_bytes"].set(
+              txg.send(v),
+              labels: {pool: pool, txg: txg.txg},
+            )
+          end
+
+          OPERATIONS.each do |v|
+            metrics["zpool_txgs_#{v}"].set(
+              txg.send(v),
+              labels: {pool: pool, txg: txg.txg},
+            )
+          end
+
+          TIMES.each do |v|
+            metrics["zpool_txgs_#{v}_nanoseconds"].set(
+              txg.send(:"#{v}_ns"),
+              labels: {pool: pool, txg: txg.txg},
+            )
+          end
         end
       end
     end
