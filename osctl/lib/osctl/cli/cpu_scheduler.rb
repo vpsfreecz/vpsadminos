bd6d1147fdebd650f12b1c81c8a889335d140d23
Jakub Skokan <jakub.skokan@havefun.cz>
2022-10-18 17:36:21 +0200

    osctl, osctld: add status and control interfaces for the CPU scheduler
    (cherry picked from commit 4d2621dafc78cc135dd8770dd1dc9e0d4d62afb4)

---
+++ osctl/lib/osctl/cli/cpu_scheduler.rb
@@ -0,0 +1,105 @@
+require 'osctl/cli/command'
+
+module OsCtl::Cli
+  class CpuScheduler < Command
+    PACKAGE_FIELDS = %i(id cpus idle enabled last_check)
+
+    def status
+      require_args!
+      osctld_fmt(:cpu_scheduler_status)
+    end
+
+    def enable
+      require_args!
+      osctld_fmt(:cpu_scheduler_enable)
+    end
+
+    def disable
+      require_args!
+      osctld_fmt(:cpu_scheduler_disable)
+    end
+
+    def package_list
+      require_args!
+
+      if opts[:list]
+        puts (PACKAGE_FIELDS).join("\n")
+        return
+      end
+
+      cols =
+        if opts[:output]
+          opts[:output].split(',').map(&:to_sym)
+        else
+          PACKAGE_FIELDS
+        end
+
+      cpus_i = cols.index(:cpus)
+
+      if cpus_i
+        cols[cpus_i] = {
+          name: :cpus,
+          label: 'CPUS',
+          display: Proc.new { |v| format_cpumask(v) },
+        }
+      end
+
+      fmt_opts = {
+        layout: :columns,
+        cols: cols,
+        sort: opts[:sort] ? opts[:sort].split(',').map(&:to_sym) : %i(idle),
+      }
+
+      fmt_opts[:header] = false if opts['hide-header']
+
+      osctld_fmt(:cpu_scheduler_package_list, fmt_opts: fmt_opts)
+    end
+
+    def package_enable
+      require_args!('package')
+      osctld_fmt(:cpu_scheduler_package_enable, cmd_opts: {package: args[0].to_i})
+    end
+
+    def package_disable
+      require_args!('package')
+      osctld_fmt(:cpu_scheduler_package_disable, cmd_opts: {package: args[0].to_i})
+    end
+
+    protected
+    def format_cpumask(cpu_list)
+      return if cpu_list.empty?
+
+      groups = []
+      acc = []
+      prev = nil
+
+      cpu_list.each do |cpu|
+        if prev.nil? || cpu == prev+1
+          prev = cpu
+          acc << cpu
+        else
+          groups << format_cpumask_range(acc)
+          prev = nil
+          acc = [cpu]
+        end
+      end
+
+      groups << format_cpumask_range(acc)
+      groups.join(',')
+    end
+
+    def format_cpumask_range(acc)
+      len = acc.length
+
+      if len == 1
+        acc.first
+      elsif len == 2
+        acc.join(',')
+      elsif len > 2
+        "#{acc.first}-#{acc.last}"
+      else
+        nil
+      end
+    end
+  end
+end
--- osctld/lib/osctld/cpu_scheduler.rb
+++ osctld/lib/osctld/cpu_scheduler.rb
@@ -10,48 +10,100 @@ module OsCtld
     include OsCtl::Lib::Utils::Log
 
     class << self
-      %i(enabled? needed? use? start stop running? schedule_ct).each do |v|
+      %i(
+        enabled?
+        needed?
+        use?
+        start
+        stop
+        enable
+        disable
+        enable_package
+        disable_package
+        running?
+        schedule_ct
+        export_status
+        export_packages
+      ).each do |v|
         define_method(v) do |*args, **kwargs|
           instance.send(v, *args, **kwargs)
         end
       end
     end
 
+    PackageInfo = Struct.new(
+      :id,
+      :cpuset,
+      :idle,
+      :enabled,
+      :last_check,
+      keyword_init: true,
+    )
+
     def initialize
       init_lock
 
+      @enabled = Daemon.get.config.enable_cpu_scheduler?
       @queue = OsCtl::Lib::Queue.new
       @topology = OsCtl::Lib::CpuTopology.new
-      @package_cpuset = {}
-      @package_idle = {}
+      @package_mutex = Mutex.new
+      @package_info = {}
 
       topology.packages.each_value do |pkg|
-        @package_cpuset[pkg.id] = pkg.cpus.keys.sort.join(',')
+        @package_info[pkg.id] = PackageInfo.new(
+          id: pkg.id,
+          cpuset: pkg.cpus.keys.sort.join(','),
+          idle: 0,
+          enabled: true,
+          last_check: nil,
+        )
       end
 
       log(:info, "#{topology.cpus.length} CPUs in #{topology.packages.length} packages")
     end
 
+    # Start background CPU monitoring
     def start
-      @thread = Thread.new { monitor_cpu_packages }
+      exclusively do
+        @thread = Thread.new { monitor_cpu_packages }
+      end
     end
 
+    # Stop CPU monitoring
     def stop
-      return unless running?
+      exclusively do
+        return unless running?
 
-      queue << :stop
-      thread.join
-      @thread = nil
+        queue << :stop
+        thread.join
+        @thread = nil
+      end
+    end
+
+    # Enable and start the scheduler
+    def enable
+      exclusively do
+        @enabled = true
+        start unless running?
+      end
+    end
+
+    # Disable and stop the scheduler
+    def disable
+      exclusively do
+        @enabled = false
+        stop
+      end
     end
 
     # Return `true` if the scheduler is running
     def running?
-      !@thread.nil?
+      inclusively { !@thread.nil? }
     end
 
     # Return `true` if the scheduler is enabled by configuration
     def enabled?
-      Daemon.get.config.enable_cpu_scheduler?
+      inclusively { @enabled }
     end
 
     # Return `true` if the scheduler is needed by the system
@@ -61,7 +113,37 @@ module OsCtld
 
     # Return `true` if the scheduler is both enabled and needed
     def use?
-      enabled? && needed?
+      inclusively { enabled? && needed? }
+    end
+
+    # @param package_id [Integer]
+    # @return [Boolean]
+    def enable_package(package_id)
+      ret = false
+
+      sync_pkg do
+        next unless package_info.has_key?(package_id)
+
+        package_info[package_id].enabled = true
+        ret = true
+      end
+
+      ret
+    end
+
+    # @param package_id [Integer]
+    # @return [Boolean]
+    def disable_package(package_id)
+      ret = false
+
+      sync_pkg do
+        next unless package_info.has_key?(package_id)
+
+        package_info[package_id].enabled = false
+        ret = true
+      end
+
+      ret
     end
 
     # Assign container to an available CPU package and configure its cpuset
@@ -73,32 +155,64 @@ module OsCtld
       end
     end
 
+    def export_status
+      ret = {}
+
+      exclusively do
+        ret.update(enabled: enabled?, needed: needed?, use: use?, running: running?)
+      end
+
+      ret[:packages] = topology.packages.length
+      ret[:cpus] = topology.cpus.length
+
+      ret
+    end
+
+    def export_packages
+      sync_pkg do
+        topology.packages.each_value.map do |pkg|
+          {
+            id: pkg.id,
+            cpus: pkg.cpus.keys,
+            idle: package_info[pkg.id].idle,
+            enabled: package_info[pkg.id].enabled,
+            last_check: package_info[pkg.id].last_check,
+          }
+        end
+      end
+    end
+
     def log_type
       'cpu-scheduler'
     end
 
     protected
-    attr_reader :topology, :package_cpuset, :package_idle, :thread, :queue
+    attr_reader :topology, :package_info, :thread, :queue
 
     def do_schedule_ct(ctrc)
       fail 'programming error: the scheduler is not running' unless running?
 
-      pkg_id = inclusively do
-        package_idle.sort do |a, b|
-          b[1] <=> a[1]
-        end.first[0]
+      sorted_pkgs = inclusively do
+        package_info.values.sort do |a, b|
+          b.idle <=> a.idle
+        end
       end
 
-      log(:info, "Assigning #{ctrc.ident} to CPU package #{pkg_id}")
+      pkg = sorted_pkgs.detect { |pkg| pkg.enabled }
 
-      cpu_mask = package_cpuset[pkg_id]
+      if pkg.nil?
+        log(:warn, "No enabled package found, unable to schedule #{ctrc.ident}")
+        return
+      end
+
+      log(:info, "Assigning #{ctrc.ident} to CPU package #{pkg.id}")
 
       # cpuset cannot be configured when child groups already exists, so set it
       # as soon as possible.
       CGroup.mkpath('cpuset', ctrc.ct.base_cgroup_path.split('/'))
       package_set = CGroup.set_param(
         File.join(CGroup.abs_cgroup_path('cpuset', ctrc.ct.base_cgroup_path), 'cpuset.cpus'),
-        [cpu_mask]
+        [pkg.cpuset]
       )
 
       # Even when we fail here, the cpuset configuration is propagated to LXC
@@ -112,11 +226,11 @@ module OsCtld
       ctrc.ct.cgparams.set([CGroup::Param.import(
         subsystem: 'cpuset',
         parameter: 'cpuset.cpus',
-        value: [cpu_mask],
+        value: [pkg.cpuset],
         persistent: false,
       )])
 
-      ctrc.cpu_package = pkg_id
+      ctrc.cpu_package = pkg.id
       ctrc.save
     end
 
@@ -133,6 +247,7 @@ module OsCtld
     end
 
     def parse_proc_stat
+      now = Time.now
       new_idle = Hash[topology.packages.map { |k, _| [k, 0] }]
 
       File.open('/proc/stat') do |f|
@@ -150,7 +265,16 @@ module OsCtld
         end
       end
 
-      exclusively { @package_idle = new_idle }
+      sync_pkg do
+        new_idle.each do |pkg_id, idle|
+          package_info[pkg_id].idle = idle
+          package_info[pkg_id].last_check = now
+        end
+      end
+    end
+
+    def sync_pkg(&block)
+      @package_mutex.synchronize(&block)
     end
   end
 end
