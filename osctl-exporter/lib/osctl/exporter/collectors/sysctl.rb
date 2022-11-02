032b4ba0901f9c8fdfd5d751df39c9c1d8087665
Jakub Skokan <jakub.skokan@havefun.cz>
2022-11-02 09:28:48 +0100

    osctl-exporter: track kernel pty sysctls
    (cherry picked from commit e5119a230bfcc198c75fd97c25baf36e7488e47d)

--- osctl-exporter/lib/osctl/exporter/collectors/sysctl.rb
+++ osctl-exporter/lib/osctl/exporter/collectors/sysctl.rb
@@ -11,11 +11,26 @@ module OsCtl::Exporter
         :sysctl_kernel_keys_maxbytes,
         docstring: 'Value of /proc/sys/kernel/keys/maxbytes',
       )
+      @sysctl_kernel_pty_max = registry.gauge(
+        :sysctl_kernel_pty_max,
+        docstring: 'Value of /proc/sys/kernel/pty/max',
+      )
+      @sysctl_kernel_pty_reserve = registry.gauge(
+        :sysctl_kernel_pty_reserve,
+        docstring: 'Value of /proc/sys/kernel/pty/reserve',
+      )
+      @sysctl_kernel_pty_nr = registry.gauge(
+        :sysctl_kernel_pty_nr,
+        docstring: 'Value of /proc/sys/kernel/pty/nr',
+      )
     end
 
     def collect(client)
       @sysctl_kernel_keys_maxkeys.set(File.read('/proc/sys/kernel/keys/maxkeys').strip.to_i)
       @sysctl_kernel_keys_maxbytes.set(File.read('/proc/sys/kernel/keys/maxbytes').strip.to_i)
+      @sysctl_kernel_pty_max.set(File.read('/proc/sys/kernel/pty/max').strip.to_i)
+      @sysctl_kernel_pty_reserve.set(File.read('/proc/sys/kernel/pty/reserve').strip.to_i)
+      @sysctl_kernel_pty_nr.set(File.read('/proc/sys/kernel/pty/nr').strip.to_i)
     end
   end
 end
