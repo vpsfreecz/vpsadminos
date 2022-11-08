1de8fe1a360e329828d1794b3b92c694cc29a79e
Jakub Skokan <jakub.skokan@havefun.cz>
2022-11-08 22:11:03 +0100

    tests: lxcfs/loadavgs: update for lxcfs workers

--- tests/suite/lxcfs/loadavgs.nix
+++ tests/suite/lxcfs/loadavgs.nix
@@ -20,17 +20,14 @@ import ../../make-test.nix (pkgs: {
 
     machine.all_succeed(
       "osctl ct new --distribution alpine testct",
+      "osctl ct exec -r testct mkdir -p /var/lib/lxcfs",
+      "osctl ct start testct",
     )
 
     _, output = machine.succeeds("osctl ct show -H -o lxcfs_mountpoint testct")
     lxcfs_mountpoint = output.strip
 
-    machine.all_succeed(
-      "osctl ct mounts new --fs #{lxcfs_mountpoint}/proc/.loadavgs --mountpoint /loadavgs --opts bind,rw,create=file --type bind testct",
-      "osctl ct start testct",
-    )
-
-    _, output = machine.fails("osctl ct exec testct cat /loadavgs")
+    _, output = machine.fails("osctl ct exec testct cat /var/lib/lxcfs/proc/.loadavgs")
 
     unless output.include?("read error: Operation not permitted")
       fail "EPERM expected, got '#{output}'"
