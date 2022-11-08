e10096ec6c380a53d167959c740347dd7c94dca8
Jakub Skokan <jakub.skokan@havefun.cz>
2022-11-08 15:43:29 +0100

    osctld: increase cpu_scheduler.min_package_container_count_percent to 80
    (cherry picked from commit 737e5914dc23ffc0d856baa198617f42e4cf2cab)

--- osctld/lib/osctld/config.rb
+++ osctld/lib/osctld/config.rb
@@ -13,7 +13,7 @@ module OsCtld
 
       def initialize(cfg)
         @enable = cfg.fetch('enable', false)
-        @min_package_container_count_percent = cfg.fetch('min_package_container_count_percent', 75)
+        @min_package_container_count_percent = cfg.fetch('min_package_container_count_percent', 80)
       end
     end
 
