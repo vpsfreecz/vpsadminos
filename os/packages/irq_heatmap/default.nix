2042a8f4738475bcb91b41829d7a1456303396d0
Pavel Snajdr <snajpa@snajpa.net>
2022-11-12 02:22:20 +0100

    os: irq_heatmap: support more cores and cpui(dle) color scheme
    Signed-off-by: Pavel Snajdr <snajpa@snajpa.net>

--- os/packages/irq_heatmap/default.nix
+++ os/packages/irq_heatmap/default.nix
@@ -3,15 +3,11 @@ pkgs.stdenv.mkDerivation rec {
   name = "irq_heatmap";
   version = "0.git";
   src = fetchFromGitHub {
-    owner = "andyphillips";
+    owner = "snajpa";
     repo = "irq_heatmap";
-    rev = "0bd0857a3d7b3b33f33732ab9f880d25fa758a2a";
-    sha256 = "sha256-uH9iZHB8CGqk74t1QL+YruQsdsRupZBOmLCIVTf1lvY=";
+    rev = "bde047b57da70134028e28b769ef4efc49f3e7a2";
+    sha256 = "sha256-9o8zy7PmXxOuoUfjWkG0MzRkNRY/CCkJj+Fi30ADTqs=";
   };
-  postPatch = ''
-    substituteInPlace ./irq_numa.h --replace "define MAX_SOCKETS 4" "define MAX_SOCKETS 64"
-    substituteInPlace ./irq_numa.h --replace "define MAX_CORES 64" "define MAX_CORES 256"
-  '';
   buildInputs = with pkgs; [
     gnumake numactl
   ];
