fe5a080e4fcaf0de07e71b70abf4c8be3f0cbff2
Pavel Snajdr <snajpa@snajpa.net>
2022-11-13 01:23:34 +0100

    os: irq_heatmap v1.3

--- os/packages/irq_heatmap/default.nix
+++ os/packages/irq_heatmap/default.nix
@@ -1,12 +1,12 @@
 { pkgs, getopt, fetchFromGitHub, ... }:
 pkgs.stdenv.mkDerivation rec {
   name = "irq_heatmap";
-  version = "0.git";
+  version = "1.3";
   src = fetchFromGitHub {
     owner = "snajpa";
     repo = "irq_heatmap";
-    rev = "bde047b57da70134028e28b769ef4efc49f3e7a2";
-    sha256 = "sha256-9o8zy7PmXxOuoUfjWkG0MzRkNRY/CCkJj+Fi30ADTqs=";
+    rev = "357eb7f2d8302c97bf7d889fa1a89045016fed7f";
+    sha256 = "sha256-4ERfs/uipdRjr/ScnvwOXj/q6TtFM6QaAEf9JgcVacI=";
   };
   buildInputs = with pkgs; [
     gnumake numactl
