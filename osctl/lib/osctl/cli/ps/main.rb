c3fc9b208476ab3e75585e87c06aa7512d627e0a
Jakub Skokan <jakub.skokan@havefun.cz>
2023-01-15 21:50:12 +0100

    osctl ct ps: print a list of slow processes to stderr

--- osctl/lib/osctl/cli/ps/main.rb
+++ osctl/lib/osctl/cli/ps/main.rb
@@ -3,6 +3,10 @@ require 'tty-spinner'
 
 module OsCtl::Cli
   class Ps::Main < Command
+    SlowOsProcess = Struct.new(:os_process, :duration)
+
+    READ_TIMEOUT = 5
+
     def run
       if opts[:list]
         puts Ps::Columns::COLS.join("\n")
@@ -33,7 +37,7 @@ module OsCtl::Cli
         end
       end
 
-      pl, pools = get_process_list(ctids, filters)
+      pl, pools, slow_os_procs = get_process_list(ctids, filters)
 
       cols =
         if opts[:output]
@@ -63,6 +67,23 @@ module OsCtl::Cli
       else
         warn "No processes found"
       end
+
+      if slow_os_procs.any?
+        warn "\nSlow processes:"
+        slow_os_procs.sort do |a, b|
+          b.duration <=> a.duration
+        end.each do |slow_proc|
+          pool, id = slow_proc.os_process.ct_id
+          ctid = pool.nil? ? '[host]' : "#{pool}:#{id}"
+          warn(sprintf(
+            '%16s: %10d %-18s %.2fs',
+            ctid,
+            slow_proc.os_process.pid,
+            slow_proc.os_process.name,
+            slow_proc.duration,
+          ))
+        end
+      end
     end
 
     protected
@@ -75,45 +96,64 @@ module OsCtl::Cli
       list_thread = Thread.new { list_processes(list_queue, ctids, filters) }
       update_queue = OsCtl::Lib::Queue.new
       update_thread = Thread.new { update_loop(update_queue) }
-      last_pid = nil
+      last_os_proc = nil
+      last_time = nil
+      slow_os_procs = []
       @update_count = false
       total = 0
       error = false
       ret = nil
 
       loop do
-        v = list_queue.pop(timeout: 5)
+        v = list_queue.pop(timeout: READ_TIMEOUT)
 
         if v.is_a?(Array)
           ret = v
+          ret << slow_os_procs
           break
         end
 
-        pid = v
+        os_proc = v
 
-        if pid.nil? && !error
+        if os_proc.nil? && !error
           msg =
-            if last_pid.nil?
+            if last_os_proc.nil?
               'Taking long to list /proc entries'
             else
-              sprintf('Taking long to process pid %d', last_pid)
+              sprintf('Taking long to process pid %d', last_os_proc.pid)
             end
 
           set_title(spinner, msg)
+
+          if last_os_proc
+            slow_os_procs << SlowOsProcess.new(last_os_proc, 0)
+            last_time = Time.now - READ_TIMEOUT
+          end
+
           error = true
           next
         end
 
-        total += 1
-        last_pid = pid
+        if os_proc && last_time
+          slow_os_procs.last.duration = Time.now - last_time
+          last_time = nil
+        end
 
-        if pid && error
+        total += 1
+        last_os_proc = os_proc
+
+        if os_proc && error
           set_title(spinner, sprintf('Listing processes... %8d', total))
           error = false
-        elsif pid
+        elsif os_proc
           set_title(spinner, sprintf('Listing processes... %8d', total))
           @update_count = false
         end
+      end
+
+      if last_time
+        slow_os_procs.last.duration = Time.now - last_time
+        last_time = nil
       end
 
       update_queue << :stop
@@ -131,7 +171,7 @@ module OsCtl::Cli
       pools = {}
       pl = OsCtl::Lib::ProcessList.new(parse_stat: false, parse_status: false) do |p|
         # Signal which pid we're on
-        queue << p.pid
+        queue << p
 
         # Filter processes
         pool, id = p.ct_id
