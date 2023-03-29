6ed44edd9f4222d6aad3ea123a9889c7890d095b
Jakub Skokan <jakub.skokan@havefun.cz>
2023-03-29 20:05:58 +0200

    os: update lxc to 5.0.2

--- os/overlays/packages.nix
+++ os/overlays/packages.nix
@@ -32,33 +32,14 @@ self: super:
 
   lxc =
     let
-      # Workaround for build failures, try to remove in the future
-      glibc = super.glibc.overrideAttrs (oldAttrs: rec {
-        NIX_CFLAGS_COMPILE = "-Wno-error=stringop-truncation";
-      });
-
       libcap = super.libcap.overrideAttrs (oldAttrs: rec {
         postInstall = builtins.replaceStrings [ ''rm "$lib"/lib/*.a'' ] [ "" ]
                                               oldAttrs.postInstall;
       });
-
-      lxc = super.callPackage <nixpkgs/pkgs/os-specific/linux/lxc/default.nix> {
-        inherit libcap;
-      };
-
-    in
-      lxc.overrideAttrs (oldAttrs: rec {
-        version = "4.0.11";
-
-        src = super.fetchFromGitHub {
-          owner = "vpsfreecz";
-          repo = "lxc";
-          rev = "940320bef1461913d6f8c94084fe52f600e9c998";
-          sha256 = "sha256:1q5x6f9iy8xy4fig0dzyjjc7ky8hymwb9p2rc994vwswmyrilicv";
-        };
-
-        buildInputs = oldAttrs.buildInputs ++ [ glibc glibc.static ];
-      });
+    in super.callPackage ../packages/lxc/default.nix {
+      inherit libcap;
+      systemd = null;
+    };
 
   lxcfs = super.lxcfs.overrideAttrs (oldAttrs: rec {
     version = "4.0.11";
---
+++ os/packages/lxc/default.nix
@@ -0,0 +1,81 @@
+{ lib, stdenv, fetchFromGitHub, meson, ninja, pkg-config, perl, docbook2x
+, docbook_xml_dtd_45, pam, glibc, openssl
+
+# Optional Dependencies
+, libapparmor ? null, gnutls ? null, libselinux ? null, libseccomp ? null
+, libcap ? null, systemd ? null
+}:
+
+with lib;
+stdenv.mkDerivation rec {
+  pname = "lxc";
+  version = "5.0.2";
+
+  src = fetchFromGitHub {
+    owner = "vpsfreecz";
+    repo = "lxc";
+    rev = "c10074538db02b4ce7887564e1d9539d0eb9a589";
+    sha256 = "sha256-2mVbkym7lxPABiAG4RZK/MvvV894AODubEqvPKm8M9A=";
+  };
+
+  nativeBuildInputs = [
+    pkg-config meson ninja docbook2x
+  ];
+
+  buildInputs = [
+    pam libapparmor gnutls libselinux libseccomp libcap openssl
+    glibc glibc.static
+  ];
+
+  patchPhase = ''
+    # Do not create empty directories in localstatedir
+    sed -i '/install_emptydir/d' meson.build
+
+    # Fix install path of bash completions
+    sed -i "s|^bashcompletiondir =.*|bashcompletiondir = join_paths('$out', 'share', 'bash-completion', 'completions')|" meson.build
+
+    # Prevent installation of README into rootfs mount path in /var
+    sed -i 's/install: true/install: false/' doc/rootfs/meson.build
+  '';
+
+  mesonFlags = [
+    "--localstatedir=/var"
+    "-Ddistrosysconfdir=${placeholder "out"}/etc/default"
+    "-Dusernet-config-path=/etc/lxc/lxc-usernet"
+    "-Dpam-cgroup=true"
+    "-Dinit-script=sysvinit"
+    (if isNull systemd then "-Dsd-bus=disabled" else "-Dsd-bus=enabled")
+  ] ++ optional (libapparmor != null) "-Dapparmor=true"
+    ++ optional (libselinux != null) "-Dselinux=true"
+    ++ optional (libseccomp != null) "-Dseccomp=true"
+    ++ optional (libcap != null) "-Dcapabilities=true"
+    ++ [
+    "-Dexamples=false"
+    (if doCheck then "-Dtests=true" else "-Dtests=false")
+    "-Drootfs-mount-path=/var/lib/lxc/rootfs"
+  ];
+
+  postInstall = ''
+    # Remove unused init-script
+    rm -rf $out/etc/init.d
+  '';
+
+  doCheck = false;
+
+  meta = {
+    homepage = "https://linuxcontainers.org/";
+    description = "Userspace tools for Linux Containers, a lightweight virtualization system";
+    license = licenses.lgpl21Plus;
+
+    longDescription = ''
+      LXC is the userspace control package for Linux Containers, a
+      lightweight virtual system mechanism sometimes described as
+      "chroot on steroids". LXC builds up from chroot to implement
+      complete virtual systems, adding resource management and isolation
+      mechanisms to Linux’s existing process management infrastructure.
+    '';
+
+    platforms = platforms.linux;
+    maintainers = with maintainers; [ ];
+  };
+}
