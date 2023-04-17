48da2479e2bfa39ac83fcfbbf7f4c5df866fc303
Jakub Skokan <jakub.skokan@havefun.cz>
2023-04-17 09:40:26 +0200

    os: vdevlog: add man page

--- os/packages/vdevlog/default.nix
+++ os/packages/vdevlog/default.nix
@@ -1,8 +1,20 @@
-{ ruby, substituteAll }:
-substituteAll {
-  name = "vdevlog";
-  src = ./vdevlog.rb;
-  isExecutable = true;
-  dir = "bin";
-  inherit ruby;
-}
+{ ruby, runCommand, substituteAll, ronn }:
+let
+  vdevlog = substituteAll {
+    name = "vdevlog";
+    src = ./vdevlog.rb;
+    isExecutable = true;
+    inherit ruby;
+  };
+in runCommand "vdevlog" {} ''
+  mkdir -p $out/bin
+  ln -s ${vdevlog} $out/bin/vdevlog
+
+  publish_date=2023-04-17
+  mkdir -p $out/share/man/man8
+  ${ronn}/bin/ronn \
+    --roff \
+    --pipe \
+    --date $publish_date \
+    ${./vdevlog.8.ronn} > $out/share/man/man8/vdevlog.8
+''
