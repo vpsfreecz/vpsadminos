4dbf16780d7d20a0c998f6a79d6800203fd51a5f
Jakub Skokan <jakub.skokan@havefun.cz>
2023-04-19 10:06:19 +0200

    image-scripts: Add Fedora 38

---
+++ image-scripts/images/fedora-38/build.sh
@@ -0,0 +1,21 @@
+. "$IMAGEDIR/config.sh"
+BASEURL=http://ftp.fi.muni.cz/pub/linux/fedora/linux/releases/$RELVER/Everything/x86_64/os
+RELEASE="$BASEURL/Packages/f/fedora-release-server-$RELVER-34.noarch.rpm
+$BASEURL/Packages/f/fedora-release-$RELVER-34.noarch.rpm
+$BASEURL/Packages/f/fedora-release-common-$RELVER-34.noarch.rpm
+$BASEURL/Packages/f/fedora-release-identity-basic-$RELVER-34.noarch.rpm"
+GROUPNAME="minimal install"
+EXTRAPKGS="vim man fedora-gpg-keys glibc-langpack-en"
+REMOVEPKGS="plymouth"
+
+. $INCLUDE/redhat-family.sh
+
+bootstrap
+configure-common
+
+configure-redhat-common
+configure-fedora
+configure-fedora-nm-keyfiles
+run-configure
+
+set-initcmd "/sbin/init" "systemd.unified_cgroup_hierarchy=0"
