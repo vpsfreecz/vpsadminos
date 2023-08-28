3854c25e7260d3db1fbbb6e0af96310b72c4c76b
Jakub Skokan <jakub.skokan@havefun.cz>
2023-08-28 18:41:42 +0200

    image-scripts/tests: longer pause on passwd_stopped for nixos and guix
    Wait for ssh host key generation.

--- image-scripts/tests/passwd_stopped.sh
+++ image-scripts/tests/passwd_stopped.sh
@@ -1,7 +1,7 @@
 # NixOS does not have /etc until the first start
 if [ "$DISTNAME" == "nixos" ] || [ "$DISTNAME" == "guix" ] ; then
 	osctl ct start $CTID || fail "unable to start"
-	sleep 10
+	sleep 30
 	osctl ct stop $CTID || fail "unable to stop"
 fi
 osctl ct passwd $CTID root suCHS3crET || fail "unable to set password"
