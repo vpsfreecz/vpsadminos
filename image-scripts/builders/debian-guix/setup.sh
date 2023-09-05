81f9a1742c56caa0020e8c0318b0b17eb4b8e901
Jakub Skokan <jakub.skokan@havefun.cz>
2023-09-05 20:32:09 +0200

    image-scripts: guix: remove workaround for devpts mounting
    This is no longer needed since osctl-image enables nesting profile on
    the container, which allows mounting devpts anywhere.

--- image-scripts/builders/debian-guix/setup.sh
+++ image-scripts/builders/debian-guix/setup.sh
@@ -3,14 +3,4 @@ apt-get update
 apt-get -y install guix
 . /etc/profile.d/guix.sh || true
 
-# This is a workaround for guix pull failing with:
-#
-#   guix pull: error: while setting up the build environment: mounting /dev/pts: Permission denied
-#
-# Mounting of /dev/pts can be avoided if /dev/pts/ptmx does not exist.
-if [ -e /dev/pts/ptmx ] ; then
-	mkdir -p /var/empty
-	mount --bind /var/empty /dev/pts
-fi
-
 guix pull
--- image-scripts/images/guix/build.sh
+++ image-scripts/images/guix/build.sh
@@ -2,16 +2,6 @@
 
 . /etc/profile.d/guix.sh
 
-# This is a workaround for guix pull failing with:
-#
-#   guix pull: error: while setting up the build environment: mounting /dev/pts: Permission denied
-#
-# Mounting of /dev/pts can be avoided if /dev/pts/ptmx does not exist.
-if [ -e /dev/pts/ptmx ] ; then
-	mkdir -p /var/empty
-	mount --bind /var/empty /dev/pts
-fi
-
 guix pull
 hash guix
 
