c67437d447ddae157da9d4ab6b4d691b4a560440
Jakub Skokan <jakub.skokan@havefun.cz>
2023-09-05 13:15:32 +0200

    image-scripts: use ftp.sh.cvut.cz mirror for rocky
    The original mirror is highly unreliable.

--- image-scripts/images/rocky-9/build.sh
+++ image-scripts/images/rocky-9/build.sh
@@ -1,11 +1,11 @@
 . "$IMAGEDIR/config.sh"
 POINTVER=9.2
-RELEASE=https://download.rockylinux.org/pub/rocky/${POINTVER}/BaseOS/x86_64/os/Packages/r/rocky-release-${POINTVER}-1.6.el9.noarch.rpm
-BASEURL=https://download.rockylinux.org/pub/rocky/${POINTVER}/BaseOS/x86_64/os/
+RELEASE=https://ftp.sh.cvut.cz/rocky/${POINTVER}/BaseOS/x86_64/os/Packages/r/rocky-release-${POINTVER}-1.6.el9.noarch.rpm
+BASEURL=https://ftp.sh.cvut.cz/rocky/${POINTVER}/BaseOS/x86_64/os/
 
 # CentOS 8 does not seem to have an updates repo, so this variable is used to
 # add AppStream repository just for the installation process.
-UPDATES=https://download.rockylinux.org/pub/rocky/${POINTVER}/AppStream/x86_64/os/
+UPDATES=https://ftp.sh.cvut.cz/rocky/${POINTVER}/AppStream/x86_64/os/
 
 GROUPNAME='core'
 EXTRAPKGS='rocky-gpg-keys rocky-repos vim man'
