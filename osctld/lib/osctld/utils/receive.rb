0e1937ce1cfb8faf24c97e4e404b2cd554ed0cbd
Jakub Skokan <jakub.skokan@havefun.cz>
2023-09-19 17:54:20 +0200

    osctld: log warnings when send-receive auth keys are not found

--- osctld/lib/osctld/utils/receive.rb
+++ osctld/lib/osctld/utils/receive.rb
@@ -5,7 +5,18 @@ module OsCtld
       error!('key pool not found') unless key_pool
 
       auth_key = key_pool.send_receive_key_chain.get_key(key_name)
+
+      if auth_key.nil?
+        log(:warn, "Authentication key #{key_name.inspect} not found in pool #{key_pool_name}")
+        return false
+      end
+
       used_key = ct.pool.send_receive_key_chain.get_key(ct.send_log.opts.key_name)
+
+      if used_key.nil?
+        log(:warn, "Used key #{ct.send_log.opts.key_name.inspect} not found in pool #{ct.pool.name}")
+        return false
+      end
 
       auth_key.pubkey == used_key.pubkey
     end
