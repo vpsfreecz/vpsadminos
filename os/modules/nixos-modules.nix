d511b2b88c5c710b389b41be639794b63863ec79
Jakub Skokan <jakub.skokan@havefun.cz>
2023-09-28 09:25:39 +0200

    os: nix-daemon module moved to services/system

--- os/modules/nixos-modules.nix
+++ os/modules/nixos-modules.nix
@@ -34,13 +34,13 @@
   <nixpkgs/nixos/modules/misc/ids.nix>
   <nixpkgs/nixos/modules/virtualisation/lxc.nix>
   <nixpkgs/nixos/modules/virtualisation/lxcfs.nix>
-  <nixpkgs/nixos/modules/services/misc/nix-daemon.nix>
   <nixpkgs/nixos/modules/services/networking/dhcpd.nix>
   <nixpkgs/nixos/modules/services/networking/firewall.nix>
   <nixpkgs/nixos/modules/services/networking/firewall-iptables.nix>
   <nixpkgs/nixos/modules/services/networking/firewall-nftables.nix>
   <nixpkgs/nixos/modules/services/networking/nftables.nix>
   <nixpkgs/nixos/modules/services/networking/ssh/sshd.nix>
+  <nixpkgs/nixos/modules/services/system/nix-daemon.nix>
   <nixpkgs/nixos/modules/system/boot/kernel.nix>
   <nixpkgs/nixos/modules/misc/assertions.nix>
   <nixpkgs/nixos/modules/misc/lib.nix>
