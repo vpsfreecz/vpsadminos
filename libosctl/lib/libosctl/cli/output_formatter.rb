e08fdd845106faf8aff93094677a457b4e24060a
Jakub Skokan <jakub.skokan@havefun.cz>
2024-02-14 18:08:54 +0100

    libosctl: disable FormatParameterMismatch cop on OutputFormatter

--- libosctl/lib/libosctl/cli/output_formatter.rb
+++ libosctl/lib/libosctl/cli/output_formatter.rb
@@ -201,7 +201,9 @@ module OsCtl::Lib::Cli
             v = o[i]
           end
 
+          # rubocop:disable Lint/FormatParameterMismatch
           line format("%#{w}s:  %s", c[:label], v)
+          # rubocop:enable Lint/FormatParameterMismatch
         end
 
         line
