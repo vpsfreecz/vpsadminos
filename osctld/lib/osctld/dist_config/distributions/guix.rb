8bc83aa8e9499352e9bdb3318782ca33d33748e1
Jakub Skokan <jakub.skokan@havefun.cz>
2024-03-15 13:21:44 +0100

    osctld: run guix halt in a thread to avoid hangs
    Shepherd is not able to shut down reliably and it can happen that the
    halt command run inside the container will not exit, either. In that
    case, osctld was stuck on it and hard kill was never triggered. The halt
    command is now run in a thread, so that it does not block the timeout
    that leads to hard kill.

--- osctld/lib/osctld/dist_config/distributions/guix.rb
+++ osctld/lib/osctld/dist_config/distributions/guix.rb
@@ -34,55 +34,57 @@ module OsCtld
     end
 
     def stop(opts)
+      return super unless %i[stop shutdown].include?(opts[:mode])
+
+      wait_until = Time.now + opts[:timeout]
+      stopped = false
+      event_queue = Eventd.subscribe
+
       # Shepherd gets stuck when it is sent a signal, so shut it down only using
       # halt.
-      if %i[stop shutdown].include?(opts[:mode])
-        wait_until = Time.now + opts[:timeout]
-        stopped = false
-        event_queue = Eventd.subscribe
-
-        begin
-          ContainerControl::Commands::StopByHalt.run!(
-            ct,
-            message: opts[:message]
-          )
-        rescue ContainerControl::Error => e
-          log(:warn, ct, "Unable to gracefully shutdown shepherd: #{e.message}")
-        end
-
-        loop do
-          unless ct.running?
-            stopped = true
-            break
-          end
-
-          timeout = wait_until - Time.now
-          break if timeout < 0
-
-          event = event_queue.pop(timeout:)
-          break if event.nil?
-
-          # Ignore irrelevant events
-          next if event.type != :state \
-                  || event.opts[:pool] != ct.pool.name \
-                  || event.opts[:id] != ct.id
-
-          if event.opts[:state] == :stopped
-            stopped = true
-            break
-          end
-        end
-
-        Eventd.unsubscribe(event_queue)
-
-        if !stopped && opts[:mode] != :shutdown
-          super(opts.merge(mode: :kill, message: nil))
-        elsif ct.running?
-          raise ContainerControl::Error, 'Timeout while waiting for halt'
-        end
-      else
-        super
+      halt_thread = Thread.new do
+        ContainerControl::Commands::StopByHalt.run!(
+          ct,
+          message: opts[:message]
+        )
+      rescue ContainerControl::Error => e
+        log(:warn, ct, "Unable to gracefully shutdown shepherd: #{e.message}")
       end
+
+      loop do
+        unless ct.running?
+          stopped = true
+          break
+        end
+
+        timeout = wait_until - Time.now
+        break if timeout < 0
+
+        event = event_queue.pop(timeout:)
+        break if event.nil?
+
+        # Ignore irrelevant events
+        next if event.type != :state \
+                || event.opts[:pool] != ct.pool.name \
+                || event.opts[:id] != ct.id
+
+        if event.opts[:state] == :stopped
+          stopped = true
+          break
+        end
+      end
+
+      Eventd.unsubscribe(event_queue)
+
+      if !stopped && opts[:mode] != :shutdown
+        log(:debug, ct, 'Timeout while waiting for graceful shutdown, killing the container')
+        super(opts.merge(mode: :kill, message: nil))
+      elsif ct.running?
+        halt_thread.terminate
+        raise ContainerControl::Error, 'Timeout while waiting for halt'
+      end
+
+      halt_thread.join
     end
 
     def passwd(opts)
