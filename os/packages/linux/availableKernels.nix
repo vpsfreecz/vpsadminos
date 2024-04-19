42c7666df4de2f8bc4079aaad6937e1d66cd46e7
Pavel Snajdr <snajpa@snajpa.net>
2024-04-19 20:44:13 +0200

    os: cleanup availableKernels

--- os/packages/linux/availableKernels.nix
+++ os/packages/linux/availableKernels.nix
@@ -19,14 +19,6 @@ let
         sha256 = "sha256-jebuLXVqFPoASF4OptpcCHPSvynGIHiIZgw+nDHtGeU=";
       };
     };
-    "6.6.1-2" = {
-      url = linuxGhUrl vpsfGh "a4ab6e23a71db25c8400b21e8085d1509318af61";
-      sha256 = "sha256-RIUoL7q+NO5ICjYpVRj0W4PYD2jOgWgo/3vHDrWoEwI=";
-      zfs = {
-        rev = "639b42ba4c5131ba289124b90e4dc194f62fa91f";
-        sha256 = "sha256-hAmZ+YWwbKfb0MC7W8BZdb3A8JtILAOkCU1fetDyADA=";
-      };
-    };
     "6.6.17" = {
       url = linuxGhUrl vpsfGh "d106ce2138763744573619d1c1283ee2cf5b0fc2";
       sha256 = "sha256-uWm2zUBqfJtJYpAkw4LCBw1EjtKZ8147/rHEFAmBIbQ=";
@@ -41,30 +33,6 @@ let
       zfs = {
         rev = "af4510166503cc0667841e61959375071a0e4df7";
         sha256 = "sha256-kEn/vso5oBpCdD58ipRVsfbO0BnL3j9yjD3mNY7PrK0=";
-      };
-    };
-    "6.6.10" = {
-      url = linuxGhUrl vpsfGh "17d9c8ecca34126be30bb3fb52594ad8ee1cf2a2";
-      sha256 = "sha256-hFrj65ISS+MImVQAMAPgUvHMGOd2d9wLVbMuQGczuZ8=";
-      zfs = {
-        rev = "7ae1210c27b18ddf8ccefaf4e45ca46487dfee7d";
-        sha256 = "sha256-0KvWzF/RG+WUaWGJV/Hg1K3VNN8XXxlixM8FGidTaZM=";
-      };
-    };
-    "6.6.9" = {
-      url = linuxGhUrl vpsfGh "a9d2f8ebfa7e15fc2947801911892745ba0f106c";
-      sha256 = "sha256-SwmGr5JIun79V32I2BfxC6FPTgE9xxSH6Keanj0zy9c=";
-      zfs = {
-        rev = "5b20ef272c613bb40a3aa952199f025ec62d31a5";
-        sha256 = "sha256-yPd8M+VqOB2QbEHxpvvMsqCl+DkOjtr0w/YbbLaiXp0=";
-      };
-    };
-    "6.6.1" = {
-      url = linuxGhUrl vpsfGh "4dd38883a0d6c775cf8dd3e6a8cf896817e61d1b";
-      sha256 = "sha256-4I0rDb0t08KRMLag4XQ0OSNYgPgLUxAqUSxTVkcD0uU=";
-      zfs = {
-        rev = "1edd30b631adef06ff6662413e7abc31575c8205";
-        sha256 = "sha256-vmAV7fpllTDVYiBNK9wvVznMr4zL+JdRWOhutD4NA2k=";
       };
     };
     "6.1.53-230601" = {
