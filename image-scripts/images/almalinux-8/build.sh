5145c1b1973cfc3085d31fe9637cac7a1af38067
Jakub Skokan <jakub.skokan@havefun.cz>
2024-06-23 12:33:46 +0200

    images: update build of almalinux-8
    The repository URL no longer includes minor version, i.e. it's 8 instead
    of 8.10.

--- image-scripts/images/almalinux-8/build.sh
+++ image-scripts/images/almalinux-8/build.sh
@@ -1,11 +1,11 @@
 . "$IMAGEDIR/config.sh"
-POINTVER=8.9
-RELEASE=https://repo.almalinux.org/almalinux/${POINTVER}/BaseOS/x86_64/os/Packages/almalinux-release-${POINTVER}-1.el8.x86_64.rpm
-BASEURL=https://repo.almalinux.org/almalinux/${POINTVER}/BaseOS/x86_64/os/
+POINTVER=8.10
+RELEASE=https://repo.almalinux.org/almalinux/${RELVER}/BaseOS/x86_64/os/Packages/almalinux-release-${POINTVER}-1.el8.x86_64.rpm
+BASEURL=https://repo.almalinux.org/almalinux/${RELVER}/BaseOS/x86_64/os/
 
 # CentOS 8 does not seem to have an updates repo, so this variable is used to
 # add AppStream repository just for the installation process.
-UPDATES=https://repo.almalinux.org/almalinux/${POINTVER}/AppStream/x86_64/os/
+UPDATES=https://repo.almalinux.org/almalinux/${RELVER}/AppStream/x86_64/os/
 
 GROUPNAME='core'
 EXTRAPKGS='vim man'
