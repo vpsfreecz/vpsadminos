867cc18b751efeb6b2974a2ec408f4aed4bc077d
Jakub Skokan <jakub.skokan@havefun.cz>
2024-06-23 12:43:24 +0200

    images: rocky-9: 9.4-1.5 -> 9.4-1.7
    ftp.sh.cvut.cz has expired certificate, so switch to ftp.linux.cz.

--- image-scripts/images/rocky-9/build.sh
+++ image-scripts/images/rocky-9/build.sh
@@ -1,11 +1,11 @@
 . "$IMAGEDIR/config.sh"
 POINTVER=9.4
-RELEASE=https://ftp.sh.cvut.cz/rocky/${POINTVER}/BaseOS/x86_64/os/Packages/r/rocky-release-${POINTVER}-1.5.el9.noarch.rpm
-BASEURL=https://ftp.sh.cvut.cz/rocky/${POINTVER}/BaseOS/x86_64/os/
+RELEASE=https://ftp.linux.cz/pub/linux/rocky/${POINTVER}/BaseOS/x86_64/os/Packages/r/rocky-release-${POINTVER}-1.7.el9.noarch.rpm
+BASEURL=https://ftp.linux.cz/pub/linux/rocky/${POINTVER}/BaseOS/x86_64/os/
 
 # CentOS 8 does not seem to have an updates repo, so this variable is used to
 # add AppStream repository just for the installation process.
-UPDATES=https://ftp.sh.cvut.cz/rocky/${POINTVER}/AppStream/x86_64/os/
+UPDATES=https://ftp.linux.cz/pub/linux/rocky/${POINTVER}/AppStream/x86_64/os/
 
 GROUPNAME='core'
 EXTRAPKGS='rocky-gpg-keys rocky-repos vim man'
