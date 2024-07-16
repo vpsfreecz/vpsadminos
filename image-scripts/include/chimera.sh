72862f7ff650d178491506a6be604f434ebb4310
Jakub Skokan <jakub.skokan@havefun.cz>
2024-07-16 12:01:45 +0200

    image-scripts: chimera: verify sha256sums.txt signature

--- image-scripts/include/chimera.sh
+++ image-scripts/include/chimera.sh
@@ -10,10 +10,28 @@ fetch() {
 	rx='chimera-linux-x86_64-ROOTFS-\d+-bootstrap.tar.gz'
 
 	# Fetch checksums to find out latest release name
-	wget -O - "$BASEURL/sha256sums.txt" | grep -P "$rx" > "$DOWNLOAD/sha256sums.txt"
+	wget -O "$DOWNLOAD/sha256sums.txt" "$BASEURL/sha256sums.txt"
+	wget -O "$DOWNLOAD/sha256sums.txt.minisig" "$BASEURL/sha256sums.txt.minisig"
 
 	# Extract the name
 	name=$(grep -o -P "$rx" "$DOWNLOAD/sha256sums.txt")
+
+	# Extract date for pubkey
+	reldate=$(echo "$name" | grep -o -P '\d{8}')
+	keyname="$reldate.pub"
+
+	# Download signing key
+	wget -P "$DOWNLOAD" "https://raw.githubusercontent.com/chimera-linux/cports/master/main/chimera-image-keys/files/$keyname"
+
+	# Verify signing key
+	if ! minisign -Vm sha256sums.txt -p "$DOWNLOAD/$keyname" ; then
+		warn "Failed to verify signature"
+		exit 1
+	fi
+
+	# Now that signature is verified, truncate sha256sums.txt to contain only the used archive
+	grep -P "$rx" "$DOWNLOAD/sha256sums.txt" > "$DOWNLOAD/sha256sums.txt.truncated"
+	mv "$DOWNLOAD/sha256sums.txt.truncated" "$DOWNLOAD/sha256sums.txt"
 
 	# Download rootfs
 	wget -P "$DOWNLOAD" "$BASEURL/$name"
