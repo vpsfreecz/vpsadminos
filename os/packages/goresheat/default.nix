8d511d422fd030a176b0475717f3562d89a7b4a6
Jakub Skokan <jakub.skokan@havefun.cz>
2024-07-24 08:39:56 +0200

    os: update goresheat to 85e8e1978, add option url

--- os/packages/goresheat/default.nix
+++ os/packages/goresheat/default.nix
@@ -7,8 +7,8 @@ buildGoModule {
   src = pkgs.fetchFromGitHub {
     owner = "snajpa";
     repo = "goresheat";
-    rev = "a1bccf5112efd024ddc322cc886bb0679d8d4c1d";
-    sha256 = "sha256-AZXhW3ORsk7MGXDleOVgJngwk1LQuOdBFnCFqs7ZNTs=";
+    rev = "79e1522bb1fbe1cc65525ff6e6884f42ec8e9001";
+    sha256 = "sha256-ybO3xQtwOui5UXtFQcd7k+c15i4MHrZJTYMsKTmTKU8=";
   };
 
   vendorHash = "sha256-iVGS9bvZ01AKuaFt1XLOKp6gW1NnPYTk0LoZzjsNmTg=";
