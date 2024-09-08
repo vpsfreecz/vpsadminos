d490abc31b236fc38c9eac14e4b6436a7693d637
Jakub Skokan <jakub.skokan@havefun.cz>
2024-09-08 14:09:22 +0200

    os: separate nixos and vpsadminos channels in separate store paths
    Individual channels must be in their own store paths and symlinked into
    the resulting channel path, which is what the profile then points to.
    When they're in the same store path, we can't update individual channels
    using nix-channel --update, because it results in file conflicts. The
    way nix-channel works, it copies unchanged channels and updates the
    channels we want to update. Now, if nixos and vpsadminos channels are in
    the same path, nix-channel will copy it and then it tries to add the
    updated channel, which fails, because the original channel was already
    copied.

--- os/modules/installer/cd-dvd/channel.nix
+++ os/modules/installer/cd-dvd/channel.nix
@@ -24,28 +24,27 @@ let
     (lib.cleanSourceFilter path type)
   ) ../../../../image-scripts;
 
-  # We need a copy of the Nix expressions for Nixpkgs and vpsAdminOS on the
-  # CD. These are installed as "nixos/nixpkgs" and "vpsadminos" channels
-  # of the root user, as expected by os-rebuild/os-install.
-  channelSources = pkgs.runCommand "vpsadminos-${config.system.vpsadminos.version}"
-    { }
-    ''
-      mkdir -p $out $out/vpsadminos $out/vpsadminos/artwork
-      cp -prd ${nixpkgs} $out/nixos
-      cp -prd ${ctStartMenu} $out/vpsadminos/ctstartmenu
-      cp -prd ${imageScripts} $out/vpsadminos/image-scripts
-      cp -prd ${os} $out/vpsadminos/os
-      cp -prd ${../../../../artwork/boot.png} $out/vpsadminos/artwork/boot.png
-      chmod -R u+w $out/nixos
-      chmod -R u+w $out/vpsadminos
-      if [ ! -e $out/nixos/nixpkgs ]; then
-        ln -s . $out/nixos/nixpkgs
-      fi
-      echo -n ${config.system.vpsadminos.release} > $out/vpsadminos/.version
-      echo -n ${config.system.vpsadminos.versionSuffix} > $out/vpsadminos/.version-suffix
-      echo -n ${config.system.vpsadminos.revision} > $out/vpsadminos/.git-revision
-      echo ${config.system.vpsadminos.versionSuffix} | sed -e s/pre// > $out/vpsadminos/svn-revision
-    '';
+  nixosChannel = pkgs.runCommand "nixos-${config.system.vpsadminos.version}" {} ''
+    mkdir $out
+    cp -prd ${nixpkgs} $out/nixos
+    chmod -R u+w $out/nixos
+    if [ ! -e $out/nixos/nixpkgs ]; then
+      ln -s . $out/nixos/nixpkgs
+    fi
+  '';
+
+  vpsadminosChannel = pkgs.runCommand "vpsadminos-${config.system.vpsadminos.version}" {} ''
+    mkdir -p $out $out/vpsadminos $out/vpsadminos/artwork
+    cp -prd ${ctStartMenu} $out/vpsadminos/ctstartmenu
+    cp -prd ${imageScripts} $out/vpsadminos/image-scripts
+    cp -prd ${os} $out/vpsadminos/os
+    cp -prd ${../../../../artwork/boot.png} $out/vpsadminos/artwork/boot.png
+    chmod -R u+w $out/vpsadminos
+    echo -n ${config.system.vpsadminos.release} > $out/vpsadminos/.version
+    echo -n ${config.system.vpsadminos.versionSuffix} > $out/vpsadminos/.version-suffix
+    echo -n ${config.system.vpsadminos.revision} > $out/vpsadminos/.git-revision
+    echo ${config.system.vpsadminos.versionSuffix} | sed -e s/pre// > $out/vpsadminos/svn-revision
+  '';
 
 in
 
@@ -67,10 +66,13 @@ in
         if ! [ -e /var/lib/nixos/did-channel-init ]; then
           echo "unpacking the NixOS/Nixpkgs sources..."
           mkdir -p /nix/var/nix/profiles/per-user/root
-          ${config.nix.package.out}/bin/nix-env -p /nix/var/nix/profiles/per-user/root/channels \
-            -i ${channelSources} --quiet --option build-use-substitutes false
+          ${config.nix.package.out}/bin/nix-env \
+            -p /nix/var/nix/profiles/per-user/root/channels \
+            --install ${nixosChannel} ${vpsadminosChannel} \
+            --quiet \
+            --option build-use-substitutes false
           mkdir -m 0700 -p /root/.nix-defexpr
-          ln -s /nix/var/nix/profiles/per-user/root/channels /root/.nix-defexpr/channels
+          ln -sfn /nix/var/nix/profiles/per-user/root/channels /root/.nix-defexpr/channels
           mkdir -m 0755 -p /var/lib/nixos
           touch /var/lib/nixos/did-channel-init
         fi
