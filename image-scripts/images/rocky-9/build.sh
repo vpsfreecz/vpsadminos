5232c5ffe2dcef321086e42988dd22ccf9c8cddb
Jakub Skokan <jakub.skokan@havefun.cz>
2024-11-20 15:45:34 +0100

    image-scripts: rocky 9.4 -> 9.5

--- image-scripts/images/rocky-9/build.sh
+++ image-scripts/images/rocky-9/build.sh
@@ -1,6 +1,6 @@
 . "$IMAGEDIR/config.sh"
-POINTVER=9.4
-RELEASE=https://ftp.linux.cz/pub/linux/rocky/${POINTVER}/BaseOS/x86_64/os/Packages/r/rocky-release-${POINTVER}-1.7.el9.noarch.rpm
+POINTVER=9.5
+RELEASE=https://ftp.linux.cz/pub/linux/rocky/${POINTVER}/BaseOS/x86_64/os/Packages/r/rocky-release-${POINTVER}-1.2.el9.noarch.rpm
 BASEURL=https://ftp.linux.cz/pub/linux/rocky/${POINTVER}/BaseOS/x86_64/os/
 
 # CentOS 8 does not seem to have an updates repo, so this variable is used to
