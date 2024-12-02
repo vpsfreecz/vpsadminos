b350f345255ce7abcc5d3257eff494e2422ebafe
Jakub Skokan <jakub.skokan@havefun.cz>
2024-12-02 16:09:57 +0100

    image-repository: nixos 24.05 -> 24.11

--- os/configs/image-repository.nix
+++ os/configs/image-repository.nix
@@ -58,10 +58,10 @@
       guix.rolling = { name = "guix"; tags = [ "latest" "stable" ]; };
 
       nixos = {
-        "24.05" = { tags = [ "latest" "stable" ]; };
+        "24.11" = { tags = [ "latest" "stable" ]; };
         "unstable" = { tags = [ "unstable" ]; };
 
-        "24.05-impermanence" = { tags = [ "latest" "stable" ]; };
+        "24.11-impermanence" = { tags = [ "latest" "stable" ]; };
         "unstable-impermanence" = { tags = [ "unstable" ]; };
       };
 
