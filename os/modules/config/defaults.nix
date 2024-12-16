36e7961e27319fa9441ccfd5cb10211ce6bcda1d
Jakub Skokan <jakub.skokan@havefun.cz>
2024-12-16 17:23:13 +0100

    os: enable zfs in initrd only for local installations
    ZFS in initrd includes zpool-check.rb ruby script, which in turn depends
    on gcc, thus making initrd unnecessarily large.

--- os/modules/config/defaults.nix
+++ os/modules/config/defaults.nix
@@ -44,8 +44,8 @@
 
   time.timeZone = lib.mkDefault "Europe/Amsterdam";
 
-  boot.supportedFilesystems = { nfs = true; };
-  boot.initrd.supportedFilesystems = { zfs = true; };
+  boot.supportedFilesystems = { nfs = true; zfs = true; };
+  boot.initrd.supportedFilesystems = { zfs = lib.mkDefault (!config.boot.isLiveSystem); };
   boot.zfs.moduleParams.spl = {
     "spl_taskq_thread_dynamic" = lib.mkDefault 0;
     "spl_taskq_thread_priority" = lib.mkDefault 0;
