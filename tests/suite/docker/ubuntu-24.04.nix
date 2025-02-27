29fff1e7651b7f948c19749a0b02847b00ccf517
Jakub Skokan <jakub.skokan@havefun.cz>
2025-02-27 10:21:11 +0100

    tests: add docker test for ubuntu 24.04, remove 18.04

--- tests/suite/docker/ubuntu-24.04.nix
+++ tests/suite/docker/ubuntu-24.04.nix
@@ -1,12 +1,12 @@
 import ./base.nix {
   distribution = "ubuntu";
-  version = "18.04";
+  version = "24.04";
   setupScript = ''
     machine.all_succeed(
       "osctl ct exec docker apt-get update -y",
       "osctl ct exec docker apt-get -y install apt-transport-https ca-certificates curl software-properties-common",
       "osctl ct exec docker bash -c \"curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add -\"",
-      "osctl ct exec docker add-apt-repository \"deb [arch=amd64] https://download.docker.com/linux/ubuntu bionic stable\"",
+      "osctl ct exec docker add-apt-repository \"deb [arch=amd64] https://download.docker.com/linux/ubuntu noble stable\"",
       "osctl ct exec docker apt-get update -y",
       "osctl ct exec docker apt-get -y install docker-ce",
     )
