6d052bf2611b6ffa5b52112655859f74295a0057
Jakub Skokan <jakub.skokan@havefun.cz>
2025-03-08 11:36:46 +0100

    os: extend image-repository build timeout to 72h

--- os/modules/services/misc/build-vpsadminos-container-image-repository/shared.nix
+++ os/modules/services/misc/build-vpsadminos-container-image-repository/shared.nix
@@ -109,7 +109,7 @@ let
       serviceConfig = {
         Type = "oneshot";
         ExecStart = "${buildScript repoName repoCfg}/bin/build-vpsadminos-repository-${repoName}";
-        TimeoutStartSec = "24h";
+        TimeoutStartSec = "72h";
       };
     };
 
