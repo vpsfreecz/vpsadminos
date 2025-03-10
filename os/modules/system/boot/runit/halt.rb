1cee6d64e37eb7ecab659544d21a14fced2e161a
Jakub Skokan <jakub.skokan@havefun.cz>
2025-03-10 12:39:09 +0100

    os: strip halt reason of whitespace
    Empty lines were still considered a reason and the halt process
    continued.

--- os/modules/system/boot/runit/halt.rb
+++ os/modules/system/boot/runit/halt.rb
@@ -109,7 +109,7 @@ class Halt
       end
 
       file.rewind
-      @message = file.each_line.reject { |line| line.start_with?('#') }.join
+      @message = file.each_line.reject { |line| line.start_with?('#') }.join.strip
     ensure
       file.close
       file.unlink
