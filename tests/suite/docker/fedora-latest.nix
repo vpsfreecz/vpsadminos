ad0d0af097707c5a909dbdaf5f2985e411b6f30c
Jakub Skokan <jakub.skokan@havefun.cz>
2025-05-16 08:50:27 +0200

    tests: fix docker installation in docker/fedora-latest

--- tests/suite/docker/fedora-latest.nix
+++ tests/suite/docker/fedora-latest.nix
@@ -5,7 +5,7 @@ import ./base.nix {
     machine.all_succeed(
       "osctl ct exec docker dnf -y update",
       "osctl ct exec docker dnf -y install dnf-plugins-core",
-      "osctl ct exec docker dnf config-manager --add-repo https://download.docker.com/linux/fedora/docker-ce.repo",
+      "osctl ct exec docker dnf-3 config-manager --add-repo https://download.docker.com/linux/fedora/docker-ce.repo",
       "osctl ct exec docker dnf -y install docker-ce docker-ce-cli containerd.io",
       "osctl ct exec docker systemctl start docker",
     )
