a0fce4b57f36d2386db839db4b5df5fde932ef0d
Jakub Skokan <jakub.skokan@havefun.cz>
2025-05-28 08:40:48 +0200

    tests: centos-9-stream -> centos-10-stream

--- tests/distributions.nix
+++ tests/distributions.nix
@@ -1,33 +1,33 @@
 { lib }:
 let
   table = {
-    alma_oldoldstable = { distribution = "almalinux"; version = "oldoldstable";    };
-    alma_oldstable    = { distribution = "almalinux"; version = "oldstable";       };
-    alma_stable       = { distribution = "almalinux"; version = "stable";          };
-    alpine            = { distribution = "alpine";    version = "latest";          };
-    arch              = { distribution = "arch";      version = "latest";          };
-    centos_7          = { distribution = "centos";    version = "7";               };
-    centos_9_stream   = { distribution = "centos";    version = "latest-9-stream"; };
-    chimera           = { distribution = "chimera";   version = "latest";          };
-    debian_oldstable  = { distribution = "debian";    version = "oldstable";       };
-    debian_stable     = { distribution = "debian";    version = "stable";          };
-    debian_testing    = { distribution = "debian";    version = "testing";         };
-    devuan            = { distribution = "devuan";    version = "latest";          };
-    fedora            = { distribution = "fedora";    version = "latest";          };
-    gentoo_openrc     = { distribution = "gentoo";    version = "latest-openrc";   };
-    gentoo_systemd    = { distribution = "gentoo";    version = "latest-systemd";  };
-    gentoo_musl       = { distribution = "gentoo";    version = "latest-musl";     };
-    nixos_stable      = { distribution = "nixos";     version = "stable";          };
-    nixos_unstable    = { distribution = "nixos";     version = "unstable";        };
-    opensuse          = { distribution = "opensuse";  version = "latest";          };
-    rocky_oldstable   = { distribution = "rocky";     version = "oldstable";       };
-    rocky_stable      = { distribution = "rocky";     version = "stable";          };
-    slackware         = { distribution = "slackware"; version = "latest";          };
-    ubuntu_oldoldlts  = { distribution = "ubuntu";    version = "oldoldlts";       };
-    ubuntu_oldlts     = { distribution = "ubuntu";    version = "oldlts";          };
-    ubuntu_lts        = { distribution = "ubuntu";    version = "lts";             };
-    void_glibc        = { distribution = "void";      version = "latest-glibc";    };
-    void_musl         = { distribution = "void";      version = "latest-musl";     };
+    alma_oldoldstable = { distribution = "almalinux"; version = "oldoldstable";     };
+    alma_oldstable    = { distribution = "almalinux"; version = "oldstable";        };
+    alma_stable       = { distribution = "almalinux"; version = "stable";           };
+    alpine            = { distribution = "alpine";    version = "latest";           };
+    arch              = { distribution = "arch";      version = "latest";           };
+    centos_7          = { distribution = "centos";    version = "7";                };
+    centos_10_stream  = { distribution = "centos";    version = "latest-10-stream"; };
+    chimera           = { distribution = "chimera";   version = "latest";           };
+    debian_oldstable  = { distribution = "debian";    version = "oldstable";        };
+    debian_stable     = { distribution = "debian";    version = "stable";           };
+    debian_testing    = { distribution = "debian";    version = "testing";          };
+    devuan            = { distribution = "devuan";    version = "latest";           };
+    fedora            = { distribution = "fedora";    version = "latest";           };
+    gentoo_openrc     = { distribution = "gentoo";    version = "latest-openrc";    };
+    gentoo_systemd    = { distribution = "gentoo";    version = "latest-systemd";   };
+    gentoo_musl       = { distribution = "gentoo";    version = "latest-musl";      };
+    nixos_stable      = { distribution = "nixos";     version = "stable";           };
+    nixos_unstable    = { distribution = "nixos";     version = "unstable";         };
+    opensuse          = { distribution = "opensuse";  version = "latest";           };
+    rocky_oldstable   = { distribution = "rocky";     version = "oldstable";        };
+    rocky_stable      = { distribution = "rocky";     version = "stable";           };
+    slackware         = { distribution = "slackware"; version = "latest";           };
+    ubuntu_oldoldlts  = { distribution = "ubuntu";    version = "oldoldlts";        };
+    ubuntu_oldlts     = { distribution = "ubuntu";    version = "oldlts";           };
+    ubuntu_lts        = { distribution = "ubuntu";    version = "lts";              };
+    void_glibc        = { distribution = "void";      version = "latest-glibc";     };
+    void_musl         = { distribution = "void";      version = "latest-musl";      };
   };
 in {
   cgroupv2 = with table; [
@@ -36,7 +36,7 @@ in {
     alma_stable
     alpine
     arch
-    centos_9_stream
+    centos_10_stream
     chimera
     debian_oldstable
     debian_stable
@@ -66,7 +66,7 @@ in {
     alma_stable
     arch
     centos_7
-    centos_9_stream
+    centos_10_stream
     debian_oldstable
     debian_stable
     debian_testing
