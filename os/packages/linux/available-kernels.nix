fff50dba51cd6671a3ccea6be99aa5972274008f
Pavel Snajdr <snajpa@snajpa.net>
2025-06-11 18:39:13 +0200

    os: Linux 6.12.33: new stable kernel with updated zfs

--- os/packages/linux/available-kernels.nix
+++ os/packages/linux/available-kernels.nix
@@ -1,9 +1,17 @@
 {
-  stableKernelVersion = "6.12.29";
+  stableKernelVersion = "6.12.33";
 
-  unstableKernelVersion = "6.12.29";
+  unstableKernelVersion = "6.12.33";
 
   kernels = {
+    "6.12.33" = {
+      rev = "0655f700a3545ee3a865824694b77c0d2928f17d";
+      sha256 = "sha256-2D8OZsXEW3BJODxDTutV8kjCNRnJG+KKk7t8YtJoLdE=";
+      zfs = {
+        rev = "6546b7270e6c44e7e90fd5fa4dcb2eaba04b0de2";
+        sha256 = "sha256-EFSCMEAN8XYVb+k1khXMJki4HsWkWbaH9VtXlwJINjU=";
+      };
+    };
     "6.12.29" = {
       rev = "e7a0b7edb22842d0188046201ed15217c7239fbf";
       sha256 = "sha256-4IIXcjoT+lipBsIBRncfqPCLDS6/JO068j3qCiZ8zFA=";
