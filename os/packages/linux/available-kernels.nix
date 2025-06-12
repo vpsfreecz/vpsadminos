f52e4a9b42c58356b2b18073a476db607baeea3e
Jakub Skokan <jakub.skokan@havefun.cz>
2025-06-12 10:25:28 +0200

    os: remove old unused kernels

--- os/packages/linux/available-kernels.nix
+++ os/packages/linux/available-kernels.nix
@@ -12,54 +12,6 @@
         sha256 = "sha256-EFSCMEAN8XYVb+k1khXMJki4HsWkWbaH9VtXlwJINjU=";
       };
     };
-    "6.12.29" = {
-      rev = "e7a0b7edb22842d0188046201ed15217c7239fbf";
-      sha256 = "sha256-4IIXcjoT+lipBsIBRncfqPCLDS6/JO068j3qCiZ8zFA=";
-      zfs = {
-        rev = "e4f44b41c662e46d14462166b96592d0d7635818";
-        sha256 = "sha256-D9pEP0NAsZNEXJCA912mE4VMFTSQIHBNx5NU04+W3n0=";
-      };
-    };
-    "6.12.23" = {
-      rev = "eb16add6c39362b4ef8e107c35291a6e9151b05f";
-      sha256 = "sha256-kQOWA/ILTxr3Kaovb79PN2zb0L3ncE/CnMIsKSwCw/o=";
-      zfs = {
-        rev = "c5c6253f7c623f183d556f7df61db09e1697ca9c";
-        sha256 = "sha256-yS6wC7pQsBW1UblfkhX6JdAAospLobC5FDHmM7RPaBE=";
-      };
-    };
-    "6.12.21" = {
-      rev = "50e5042ca2a1c500f2ddbeffa428f756a23ccdc5";
-      sha256 = "sha256-rsZoMNtyh0yJ9Yy4NyLKDx7h0Z2fP9ZUK+Z9lLW2Roc=";
-      zfs = {
-        rev = "4821e1dc9b6479dcd1bfb992bf93aee993ffe74a";
-        sha256 = "sha256-pYlNMeO2dNgAp8gqruV9uW5vEdHCXWGWmnDNLk1yIA8=";
-      };
-    };
-    "6.12.18" = {
-      rev = "2c0d5e8bd235b3fb049a95c687d345a6cebe0f1c";
-      sha256 = "sha256-WlUvQgW7MoixL68ZB3Yc729+A5mBxYf8tcmaph8QXYc=";
-      zfs = {
-        rev = "091ba330df7fc1a442de37bdc5f86d251562e3f7";
-        sha256 = "sha256-/Wh9bzaerjGJWxo7yD4LMoUW+0tazCI2Aw7M49Gwmi4=";
-      };
-    };
-    "6.12.14" = {
-      rev = "bb5e22767d0ec527b33e7aa699d67ab8c51275b8";
-      sha256 = "sha256-dbkgxby2ug71KSfT3DF8DfRLcifiX1Z/Mw/lXS6+mLA=";
-      zfs = {
-        rev = "2b287937fa5695fbb964fd9923817569353d85e8";
-        sha256 = "sha256-bgSZ3O0DBr3KwE027/tO8tdzZpEUs8h5rQKpgVG9YWY=";
-      };
-    };
-    "6.9.12-2" = {
-      rev = "3a74cce5425ef5182df2410e62923b4c0b3ea899";
-      sha256 = "sha256-7AhUVFT9AKkkcJPjyEsFz1r4ydk4lGfFUrf0kj2XEB0=";
-      zfs = {
-        rev = "e81cc7c3efd78a90092e60d239eb93b9174bbf7e";
-        sha256 = "sha256-NJwYoTgi3ahis7kzlrjycM7IlDZx2eZFcWGrB0P+0h8=";
-      };
-    };
     "6.6.21" = {
       rev = "86e0c00fd80469aea354b9fc4ca5913d33ea0d92";
       sha256 = "sha256-zJHOmvyBr0aR97boNZHtfBJviqpmv69RNKqR6eBkJ9A=";
