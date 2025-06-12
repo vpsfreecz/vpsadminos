5268a56d26c3805615555f471850ac77274bf639
Jakub Skokan <jakub.skokan@havefun.cz>
2025-06-12 10:27:54 +0200

    image-scripts: add rocky-10

---
+++ image-scripts/images/rocky-10/build.sh
@@ -0,0 +1,19 @@
+. "$IMAGEDIR/config.sh"
+POINTVER=10.0
+RELEASE=https://ftp.linux.cz/pub/linux/rocky/${POINTVER}/BaseOS/x86_64/os/Packages/r/rocky-release-${POINTVER}-1.6.el10.noarch.rpm
+BASEURL=https://ftp.linux.cz/pub/linux/rocky/${POINTVER}/BaseOS/x86_64/os/
+
+# CentOS 8 does not seem to have an updates repo, so this variable is used to
+# add AppStream repository just for the installation process.
+UPDATES=https://ftp.linux.cz/pub/linux/rocky/${POINTVER}/AppStream/x86_64/os/
+
+GROUPNAME='core'
+EXTRAPKGS='rocky-gpg-keys rocky-repos vim man'
+
+. $INCLUDE/redhat-family.sh
+
+bootstrap
+configure-common
+configure-redhat-common
+configure-rhel-10
+run-configure
