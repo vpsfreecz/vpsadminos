d9c38797f3a73130c64409f543ba40a8baa31564
Jakub Skokan <jakub.skokan@havefun.cz>
2025-06-16 22:35:01 +0200

    osctld: fix pruning of the default repository

--- osctld/lib/osctld/db/repositories.rb
+++ osctld/lib/osctld/db/repositories.rb
@@ -4,19 +4,15 @@ module OsCtld
   class DB::Repositories < DB::PooledList
     def self.setup(pool)
       repo = Repository.new(pool, 'default')
-      add(repo)
     rescue Errno::ENOENT
-      # Commands::Repository::Add.run!(
-      #  pool: pool,
-      #  name: 'default',
-      #  url: 'http://192.168.122.75/repo/'
-      # )
-
       Commands::Repository::Add.run(
         pool:,
         name: 'default',
         url: 'https://images.vpsadminos.org'
       )
+    else
+      add(repo)
+      repo.start
     end
   end
 end
