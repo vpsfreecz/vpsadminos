d6fcac5ebf6fc03f892baa222e05dcf4cd7f097f
Jakub Skokan <jakub.skokan@havefun.cz>
2025-07-16 13:19:30 +0200

    tests: use sysinfo-to-json

--- tests/suite/kernel/loadavg.nix
+++ tests/suite/kernel/loadavg.nix
@@ -56,65 +56,6 @@ let
     while True: time.sleep(3600)
   '';
 
-  sysinfo = pkgs.writeScriptBin "sysinfo.py" ''
-    #!${pkgs.python3}/bin/python3
-    """
-    Dump the Linux sysinfo(2) struct as JSON.
-
-    Fields whose units depend on `mem_unit` (RAM, swap, buffers, etc.)
-    are emitted in **bytes**.
-    Load averages are reported as floats (same scale as /proc/loadavg).
-    """
-
-    import ctypes
-    import ctypes.util
-    import json
-
-
-    class Sysinfo(ctypes.Structure):
-        _fields_ = [
-            ("uptime",      ctypes.c_long),
-            ("loads",       ctypes.c_ulong * 3),
-            ("totalram",    ctypes.c_ulong),
-            ("freeram",     ctypes.c_ulong),
-            ("sharedram",   ctypes.c_ulong),
-            ("bufferram",   ctypes.c_ulong),
-            ("totalswap",   ctypes.c_ulong),
-            ("freeswap",    ctypes.c_ulong),
-            ("procs",       ctypes.c_ushort),
-            ("pad",         ctypes.c_ushort),
-            ("totalhigh",   ctypes.c_ulong),
-            ("freehigh",    ctypes.c_ulong),
-            ("mem_unit",    ctypes.c_uint),
-            ("_f",          ctypes.c_char * 20), # Unused/padding
-        ]
-
-
-    libc = ctypes.CDLL(ctypes.util.find_library("c"))
-    info = Sysinfo()
-    if libc.sysinfo(ctypes.byref(info)) != 0:
-        raise OSError("sysinfo() failed")
-
-    to_bytes = lambda x: x * info.mem_unit
-
-    result = {
-        "uptime":         info.uptime,
-        "loads":          [l / 65536.0 for l in info.loads],
-        "totalram":       to_bytes(info.totalram),
-        "freeram":        to_bytes(info.freeram),
-        "sharedram":      to_bytes(info.sharedram),
-        "bufferram":      to_bytes(info.bufferram),
-        "totalswap":      to_bytes(info.totalswap),
-        "freeswap":       to_bytes(info.freeswap),
-        "procs":          info.procs,
-        "totalhigh":      to_bytes(info.totalhigh),
-        "freehigh":       to_bytes(info.freehigh),
-        "mem_unit":       info.mem_unit,
-    }
-
-    print(json.dumps(result, separators=(",", ":")))
-  '';
-
   mkContainer = {
     user = "testuser";
 
@@ -125,8 +66,8 @@ let
     config =
       { config, ... }:
       {
-        environment.systemPackages = [
-          sysinfo
+        environment.systemPackages = with pkgs; [
+          sysinfo-to-json
         ];
 
         systemd.services.genload = {
@@ -152,8 +93,8 @@ in {
     config =
       { config, ... }:
       {
-        environment.systemPackages = [
-          sysinfo
+        environment.systemPackages = with pkgs; [
+          sysinfo-to-json
         ];
 
         osctl.pools.tank = {
@@ -196,11 +137,11 @@ in {
     end
 
     host_sysinfo = proc do
-      JSON.parse(machine.succeeds("sysinfo.py")[1])
+      JSON.parse(machine.succeeds("sysinfo-to-json")[1])
     end
 
     container_sysinfo = proc do |testct|
-      JSON.parse(machine.succeeds("osctl ct exec #{testct} sysinfo.py")[1])
+      JSON.parse(machine.succeeds("osctl ct exec #{testct} sysinfo-to-json")[1])
     end
 
     configure_examples do |config|
--- tests/suite/kernel/memory-view/base.nix
+++ tests/suite/kernel/memory-view/base.nix
@@ -1,63 +1,9 @@
 { cgroupsVersion }:
 import ../../../make-test.nix ({ pkgs }:
 let
-  sysinfo = pkgs.writeScript "sysinfo.py" ''
-    #!/usr/bin/env python3
-    """
-    Dump the Linux sysinfo(2) struct as JSON.
-
-    Fields whose units depend on `mem_unit` (RAM, swap, buffers, etc.)
-    are emitted in **bytes**.
-    Load averages are reported as floats (same scale as /proc/loadavg).
-    """
-
-    import ctypes
-    import ctypes.util
-    import json
-
-
-    class Sysinfo(ctypes.Structure):
-        _fields_ = [
-            ("uptime",      ctypes.c_long),
-            ("loads",       ctypes.c_ulong * 3),
-            ("totalram",    ctypes.c_ulong),
-            ("freeram",     ctypes.c_ulong),
-            ("sharedram",   ctypes.c_ulong),
-            ("bufferram",   ctypes.c_ulong),
-            ("totalswap",   ctypes.c_ulong),
-            ("freeswap",    ctypes.c_ulong),
-            ("procs",       ctypes.c_ushort),
-            ("pad",         ctypes.c_ushort),
-            ("totalhigh",   ctypes.c_ulong),
-            ("freehigh",    ctypes.c_ulong),
-            ("mem_unit",    ctypes.c_uint),
-            ("_f",          ctypes.c_char * 20), # Unused/padding
-        ]
-
-
-    libc = ctypes.CDLL(ctypes.util.find_library("c"))
-    info = Sysinfo()
-    if libc.sysinfo(ctypes.byref(info)) != 0:
-        raise OSError("sysinfo() failed")
-
-    to_bytes = lambda x: x * info.mem_unit
-
-    result = {
-        "uptime":         info.uptime,
-        "loads":          [l / 65536.0 for l in info.loads],
-        "totalram":       to_bytes(info.totalram),
-        "freeram":        to_bytes(info.freeram),
-        "sharedram":      to_bytes(info.sharedram),
-        "bufferram":      to_bytes(info.bufferram),
-        "totalswap":      to_bytes(info.totalswap),
-        "freeswap":       to_bytes(info.freeswap),
-        "procs":          info.procs,
-        "totalhigh":      to_bytes(info.totalhigh),
-        "freehigh":       to_bytes(info.freehigh),
-        "mem_unit":       info.mem_unit,
-    }
-
-    print(json.dumps(result, separators=(",", ":")))
+  containerSysinfo = pkgs.runCommand "sysinfo-to-json.py" {} ''
+    echo "#!/usr/bin/env python3" > $out
+    cat ${pkgs.sysinfo-to-json}/bin/sysinfo-to-json >> $out
   '';
 in {
   name = "kernel-memory-view-cgroups-v${toString cgroupsVersion}";
@@ -75,7 +21,7 @@ in {
       {
         boot.enableUnifiedCgroupHierarchy = cgroupsVersion == 2;
 
-        environment.systemPackages = with pkgs; [ python3 ];
+        environment.systemPackages = with pkgs; [ sysinfo-to-json ];
       };
   };
 
@@ -135,7 +81,7 @@ in {
 
         context 'in sysinfo()' do
           before(:context) do
-            @host_sys = parse_sysinfo.call(machine.succeeds('/scripts/sysinfo.py')[1])
+            @host_sys = parse_sysinfo.call(machine.succeeds('sysinfo-to-json')[1])
             @ct_sys = parse_sysinfo.call(machine.succeeds("osctl ct runscript #{testct} /scripts/sysinfo.py")[1])
           end
 
@@ -234,7 +180,7 @@ in {
 
             context 'in sysinfo()' do
               before(:context) do
-                @host_sys = parse_sysinfo.call(machine.succeeds('/scripts/sysinfo.py')[1])
+                @host_sys = parse_sysinfo.call(machine.succeeds('sysinfo-to-json')[1])
                 @ct_sys = parse_sysinfo.call(machine.succeeds("osctl ct runscript #{testct} /scripts/sysinfo.py")[1])
               end
 
@@ -307,7 +253,7 @@ in {
       machine.wait_for_osctl_pool('tank')
       machine.wait_until_online
       machine.mkdir('/scripts')
-      machine.push_file('${sysinfo}', '/scripts/sysinfo.py')
+      machine.push_file('${containerSysinfo}', '/scripts/sysinfo.py')
     end
 
     describe 'memory view within container without swap device' do
