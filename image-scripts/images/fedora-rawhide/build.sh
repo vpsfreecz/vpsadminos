1eca9299f0dbb92acd72f02af1df1e0b5283e469
Jakub Skokan <jakub.skokan@havefun.cz>
2025-08-03 18:33:53 +0200

    image-scripts: fedora-rawhide 43-0.16 -> 43-0.19

--- image-scripts/images/fedora-rawhide/build.sh
+++ image-scripts/images/fedora-rawhide/build.sh
@@ -1,5 +1,5 @@
 . "$IMAGEDIR/config.sh"
-RAWHIDE_RELVER=43-0.16
+RAWHIDE_RELVER=43-0.19
 BASEURL=http://ftp.fi.muni.cz/pub/linux/fedora/linux/development/rawhide/Everything/x86_64/os
 RELEASE="$BASEURL/Packages/f/fedora-release-server-$RAWHIDE_RELVER.noarch.rpm
 $BASEURL/Packages/f/fedora-release-$RAWHIDE_RELVER.noarch.rpm
