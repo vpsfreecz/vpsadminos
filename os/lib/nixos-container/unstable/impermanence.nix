5d95023eb297afad4a73a0851813b261faee49f9
Jakub Skokan <jakub.skokan@havefun.cz>
2025-08-03 18:31:20 +0200

    os: use systemd.extraConfig -> systemd.settings in unstable container

--- os/lib/nixos-container/unstable/impermanence.nix
+++ os/lib/nixos-container/unstable/impermanence.nix
@@ -27,9 +27,9 @@ let
       #users.extraUsers.root.openssh.authorizedKeys.keys =
       #  [ "..." ];
 
-      systemd.extraConfig = '''
-        DefaultTimeoutStartSec=900s
-      ''';
+      systemd.settings.Manager = {
+        DefaultTimeoutStartSec = "900s";
+      };
 
       time.timeZone = "Europe/Amsterdam";
 
@@ -65,9 +65,9 @@ in
   services.openssh.enable = lib.mkDefault true;
   services.openssh.settings.PermitRootLogin = lib.mkDefault "yes";
 
-  systemd.extraConfig = ''
-    DefaultTimeoutStartSec=900s
-  '';
+  systemd.settings.Manager = {
+    DefaultTimeoutStartSec = "900s";
+  };
 
   environment.persistence."/persistent" = {
     hideMounts = true;
