f284e3571610422770d7dd5b489ef9dc41d76c98
Jakub Skokan <jakub.skokan@havefun.cz>
2025-08-14 11:46:29 +0200

    tests: fix docker/debian-latest for debian 13
    Update docker installation on debian according to
    https://docs.docker.com/engine/install/debian/

--- tests/suite/docker/debian-latest.nix
+++ tests/suite/docker/debian-latest.nix
@@ -4,11 +4,12 @@ import ./base.nix {
   setupScript = ''
     machine.all_succeed(
       "osctl ct exec docker apt-get update -y",
-      "osctl ct exec docker apt-get -y install apt-transport-https ca-certificates curl gnupg-agent software-properties-common",
-      "osctl ct exec docker bash -c \"curl -fsSL https://download.docker.com/linux/debian/gpg | apt-key add -\"",
-      "osctl ct exec docker add-apt-repository \"deb [arch=amd64] https://download.docker.com/linux/debian buster stable\"",
+      "osctl ct exec docker apt-get -y install ca-certificates curl",
+      "osctl ct exec docker curl -fsSL https://download.docker.com/linux/debian/gpg -o /etc/apt/keyrings/docker.asc",
+      "osctl ct exec docker chmod a+r /etc/apt/keyrings/docker.asc",
+      "osctl ct exec docker bash -c 'echo \"deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/debian $(. /etc/os-release && echo $VERSION_CODENAME) stable\" > /etc/apt/sources.list.d/docker.list'",
       "osctl ct exec docker apt-get update -y",
-      "osctl ct exec docker apt-get -y install docker-ce",
+      "osctl ct exec docker apt-get -y install docker-ce docker-ce-cli containerd.io",
     )
   '';
 }
