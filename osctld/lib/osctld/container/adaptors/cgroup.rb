c2ac19f63a0d31aae576012401aaac5c9ae5969e
Jakub Skokan <jakub.skokan@havefun.cz>
2025-09-27 15:50:04 +0200

    osctld: set systemd.unified_cgroup_hierarchy=1 for cgroups v2-only distros

--- osctld/lib/osctld/container/adaptors/cgroup.rb
+++ osctld/lib/osctld/container/adaptors/cgroup.rb
@@ -16,30 +16,51 @@ module OsCtld
 
       config['init_cmd'] ||= ['/sbin/init']
 
-      if config['init_cmd'].include?(systemd_opt)
-        if cgroup_v2_only_distro?(config['distribution'], config['version'])
-          log(:info, "Removing option #{systemd_opt} from init command")
-          config['init_cmd'].delete(systemd_opt)
-        end
+      cgroup_v2_only = cgroup_v2_only_distro?(config['distribution'], config['version'])
 
-        return
+      if config['init_cmd'].include?(systemd_opt_force_v1)
+        if cgroup_v2_only
+          log(:info, "Removing option #{systemd_opt_force_v1} from init command")
+          config['init_cmd'].delete(systemd_opt_force_v1)
+        end
+      elsif !cgroup_v2_only
+        log(:info, "Adding option #{systemd_opt_force_v1} to init command")
+        config['init_cmd'] << systemd_opt_force_v1
       end
 
-      log(:info, "Adding option #{systemd_opt} to init command")
-      config['init_cmd'] << systemd_opt
+      if config['init_cmd'].include?(systemd_opt_force_v2)
+        unless cgroup_v2_only
+          log(:info, "Removing option #{systemd_opt_force_v2} from init command")
+          config['init_cmd'].delete(systemd_opt_force_v2)
+        end
+      elsif cgroup_v2_only
+        log(:info, "Adding option #{systemd_opt_force_v2} to init command")
+        config['init_cmd'] << systemd_opt_force_v2
+      end
     end
 
     def to_cgroup_v2
       if systemd_distros.include?(config['distribution']) \
          && config['init_cmd'] \
-         && config['init_cmd'].include?(systemd_opt)
-        log(:info, "Removing option #{systemd_opt} from init command")
-        config['init_cmd'].delete(systemd_opt)
+         && config['init_cmd'].include?(systemd_opt_force_v1)
+        log(:info, "Removing option #{systemd_opt_force_v1} from init command")
+        config['init_cmd'].delete(systemd_opt_force_v1)
+      end
+
+      if systemd_distros.include?(config['distribution']) \
+         && config['init_cmd'] \
+         && config['init_cmd'].include?(systemd_opt_force_v2)
+        log(:info, "Removing option #{systemd_opt_force_v2} from init command")
+        config['init_cmd'].delete(systemd_opt_force_v2)
       end
     end
 
-    def systemd_opt
+    def systemd_opt_force_v1
       'systemd.unified_cgroup_hierarchy=0'
+    end
+
+    def systemd_opt_force_v2
+      'systemd.unified_cgroup_hierarchy=1'
     end
 
     def systemd_distros
