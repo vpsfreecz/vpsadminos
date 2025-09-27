5e33a0b799270c7d942c2ce09aa04f8c1a67e5c6
Jakub Skokan <jakub.skokan@havefun.cz>
2025-09-27 15:24:13 +0200

    osctld: update adaptor for cgroups v2-only distributions
    Without systemd.unified_cgroup_hierarchy=0, systemd will mount the
    unified hierarchy even when the host system uses cgroups v1. systemd 258
    drops support for cgroups v1 completely, so there's no point in adding
    this argument anymore.
    
    Furthermore, since 258 does not support cgroups v1, upgrade from 257
    will crash the init system if /sys/fs/cgroup is a hybrid hierarchy.
    This change will make upgrades possible from systemd <= 257 by already
    mounting /sys/fs/cgroup as cgroups v2.
    
    Another workaround for a successful upgrade to systemd 258 is to run:
    
      mount -t cgroup2 cgroup2 /sys/fs/cgroup
    
    before upgrading the system.

--- osctld/lib/osctld/container/adaptors/cgroup.rb
+++ osctld/lib/osctld/container/adaptors/cgroup.rb
@@ -16,7 +16,14 @@ module OsCtld
 
       config['init_cmd'] ||= ['/sbin/init']
 
-      return if config['init_cmd'].include?(systemd_opt)
+      if config['init_cmd'].include?(systemd_opt)
+        if cgroup_v2_only_distro?(config['distribution'], config['version'])
+          log(:info, "Removing option #{systemd_opt} from init command")
+          config['init_cmd'].delete(systemd_opt)
+        end
+
+        return
+      end
 
       log(:info, "Adding option #{systemd_opt} to init command")
       config['init_cmd'] << systemd_opt
@@ -37,6 +44,11 @@ module OsCtld
 
     def systemd_distros
       %w[almalinux arch centos debian fedora gentoo opensuse rocky ubuntu]
+    end
+
+    def cgroup_v2_only_distro?(distribution, version)
+      distribution == 'arch' \
+        || (distribution == 'debian' && (version.start_with?('testing-') || version.start_with?('unstable-')))
     end
   end
 end
