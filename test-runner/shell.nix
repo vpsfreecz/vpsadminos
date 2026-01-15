56154cb514e98dd12a0172572ca48b0e897d1c2d
Jakub Skokan <jakub.skokan@havefun.cz>
2026-01-15 18:23:25 +0100

    test-runner: support for running nix-shell from repository root

--- test-runner/shell.nix
+++ test-runner/shell.nix
@@ -1,6 +1,9 @@
 let
   pkgs = import <nixpkgs> { overlays = (import ../os/overlays); };
   stdenv = pkgs.stdenv;
+  repoRoot = builtins.toString ../.;
+  testRunnerRoot = "${repoRoot}/test-runner";
+  gemHome = "${testRunnerRoot}/.gems";
 
 in
 stdenv.mkDerivation rec {
@@ -14,19 +17,25 @@ stdenv.mkDerivation rec {
   ];
 
   shellHook = ''
-    mkdir -p .gems
-    export GEM_HOME="`pwd`/.gems"
-    export GEM_PATH="$GEM_HOME:$PWD/lib"
+    export REPO_ROOT="${repoRoot}"
+    export TEST_RUNNER_ROOT="${testRunnerRoot}"
+    export GEM_HOME="${gemHome}"
+    export GEM_PATH="$GEM_HOME:$TEST_RUNNER_ROOT/lib"
     export PATH="$GEM_HOME/bin:$PATH"
+
+    mkdir -p "$GEM_HOME"
 
     BUNDLE="$GEM_HOME/bin/bundle"
 
-    [ ! -x "$BUNDLE" ] && ${pkgs.ruby}/bin/gem install bundler
+    if [ ! -x "$BUNDLE" ]; then
+      ${pkgs.ruby}/bin/gem install bundler --no-document
+    fi
 
     export BUNDLE_PATH="$GEM_HOME"
-    export BUNDLE_GEMFILE="$PWD/Gemfile"
+    export BUNDLE_GEMFILE="$TEST_RUNNER_ROOT/Gemfile"
 
-    $BUNDLE install
+    # prism native extension needs relaxed purity for headers in GEM_HOME
+    NIX_ENFORCE_PURITY=0 $BUNDLE install
 
     export RUBYOPT=-rbundler/setup
   '';
