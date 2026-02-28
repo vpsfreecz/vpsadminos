e8e549b82973c29359bbd85ea05e00083ff2f366
Jakub Skokan <jakub.skokan@havefun.cz>
2026-02-28 11:58:43 +0100

    image-scripts: enable nix-command in nixos builder

--- image-scripts/builders/nixos/setup.sh
+++ image-scripts/builders/nixos/setup.sh
@@ -6,6 +6,11 @@ cat <<EOF > /etc/nixos/configuration.nix
     ./vpsadminos.nix
   ];
 
+  nix.settings.experimental-features = [
+    "nix-command"
+    "flakes"
+  ];
+
   environment.systemPackages = with pkgs; [
     git
     gnumake
