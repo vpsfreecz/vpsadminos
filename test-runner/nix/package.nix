67d82a2b7da8a570a83107f2038da624472d99fc
Jakub Skokan <jakub.skokan@havefun.cz>
2026-03-06 15:39:38 +0100

    test-runner: share packaging metadata
    Point the development wrapper at the canonical Gemfile, lockfile and
    gemset under os/packages/test-runner and drop the duplicate copies
    from test-runner/.
    
    This keeps one source of truth for runtime dependencies while still
    loading local test-runner, osvm and libosctl sources during
    development.

--- test-runner/nix/package.nix
+++ test-runner/nix/package.nix
@@ -3,18 +3,21 @@ let
   ruby = pkgs.ruby_vpsadminos;
   deps = pkgs.bundlerEnv {
     name = "test-runner-deps";
-    gemdir = ../.;
-    lockfile = ./Gemfile.lock.packaging;
+    gemfile = ../../os/packages/test-runner/Gemfile;
+    lockfile = ../../os/packages/test-runner/Gemfile.lock;
+    gemset = ../../os/packages/test-runner/gemset.nix;
     groups = [ "default" ];
     inherit ruby;
   };
 
   testRunnerSrc = ../.;
+  osvmSrc = ../../osvm;
+  libosctlSrc = ../../libosctl;
 in
 pkgs.writeShellScriptBin "test-runner" ''
   export GEM_HOME=${deps}/${ruby.gemPath}
   export GEM_PATH=${deps}/${ruby.gemPath}
-  export RUBYLIB=${testRunnerSrc}/lib
+  export RUBYLIB=${testRunnerSrc}/lib:${osvmSrc}/lib:${libosctlSrc}/lib
 
   exec ${ruby}/bin/ruby ${testRunnerSrc}/bin/test-runner "$@"
 ''
