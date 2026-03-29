9772a5df30efb3645130a95e302d4c7bef098328
Jakub Skokan <jakub.skokan@havefun.cz>
2026-03-29 19:14:27 +0200

    osctld/container: keep remote boot image attrs intact
    Remote boot fell back to default vendor and variant values whenever the
    request omitted those fields. Containers that carried custom image
    metadata therefore booted from the wrong remote image.
    
    Fill missing vendor and variant values from the target container so boot
    keeps the container's existing remote image identity.

--- osctld/lib/osctld/commands/container/boot.rb
+++ osctld/lib/osctld/commands/container/boot.rb
@@ -55,8 +55,8 @@ module OsCtld
           tpl[:distribution] ||= ct.distribution
           tpl[:version] ||= ct.version
           tpl[:arch] ||= ct.arch
-          tpl[:vendor] ||= 'default'
-          tpl[:variant] ||= 'default'
+          tpl[:vendor] ||= ct.vendor
+          tpl[:variant] ||= ct.variant
 
           tpl_path = get_image_path!(get_repositories(ct.pool), tpl)
         else
