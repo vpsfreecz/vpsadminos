d74aafcbffa725dc541c964c904e35bb9c32db6f
Jakub Skokan <jakub.skokan@havefun.cz>
2026-04-12 15:07:32 +0200

    image-scripts: update rocky-8 to 8.10-1.11.el8
    Update the rocky-8 image definition to use the current
    rocky-release 8.10-1.11.el8 RPM from the Rocky Linux 8.10 BaseOS repo.
    
    This keeps the image build pointing at the latest available release
    package.

--- image-scripts/images/rocky-8/build.sh
+++ image-scripts/images/rocky-8/build.sh
@@ -1,6 +1,6 @@
 . "$IMAGEDIR/config.sh"
 POINTVER=8.10
-RELEASE=https://ftp.linux.cz/pub/linux/rocky/${POINTVER}/BaseOS/x86_64/os/Packages/r/rocky-release-${POINTVER}-1.10.el8.noarch.rpm
+RELEASE=https://ftp.linux.cz/pub/linux/rocky/${POINTVER}/BaseOS/x86_64/os/Packages/r/rocky-release-${POINTVER}-1.11.el8.noarch.rpm
 BASEURL=https://ftp.linux.cz/pub/linux/rocky/${POINTVER}/BaseOS/x86_64/os/
 
 # CentOS 8 does not seem to have an updates repo, so this variable is used to
