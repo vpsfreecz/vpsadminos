a03cd99018bfb7d74b80fa1c54f592851bd9f069
Pavel Snajdr <snajpa@snajpa.net>
2026-04-14 05:43:48 +0200

    image-scripts: fix debian testing and unstable images
    Update Debian testing to use the current security suite and drop the
    nonexistent unstable-updates suite. Replace sshd_config in the
    Debian testing and unstable image builds so SSH password auth works
    reliably in the image-scripts QEMU tests.

--- image-scripts/images/debian-testing/build.sh
+++ image-scripts/images/debian-testing/build.sh
@@ -17,12 +17,24 @@ deb-src $BASEURL $RELNAME main
 deb $BASEURL $RELNAME-updates main
 deb-src $BASEURL $RELNAME-updates main
 
-deb http://security.debian.org/ $RELNAME/updates main
-deb-src http://security.debian.org/ $RELNAME/updates main
+deb http://security.debian.org/debian-security/ $RELNAME-security main
+deb-src http://security.debian.org/debian-security/ $RELNAME-security main
 SOURCES
 
-configure-append <<EOF
-sed -i 's/^#PermitRootLogin prohibit-password/PermitRootLogin yes/' /etc/ssh/sshd_config
+configure-append <<'EOF'
+# openssh-server 10.x on Debian testing can leave sshd_config as a sparse
+# zero-filled file in the image build, so replace it with a sane text config.
+cat > /etc/ssh/sshd_config <<'SSHD_CONFIG'
+Include /etc/ssh/sshd_config.d/*.conf
+KbdInteractiveAuthentication no
+UsePAM yes
+X11Forwarding yes
+PrintMotd no
+AcceptEnv LANG LC_* COLORTERM NO_COLOR
+Subsystem sftp /usr/lib/openssh/sftp-server
+PasswordAuthentication yes
+PermitRootLogin yes
+SSHD_CONFIG
 EOF
 
 configure-systemd-console-getty
--- image-scripts/images/debian-unstable/build.sh
+++ image-scripts/images/debian-unstable/build.sh
@@ -13,13 +13,22 @@ configure-debian
 cat > "$INSTALL/etc/apt/sources.list" <<SOURCES
 deb $BASEURL $RELNAME main
 deb-src $BASEURL $RELNAME main
-
-deb $BASEURL $RELNAME-updates main
-deb-src $BASEURL $RELNAME-updates main
 SOURCES
 
-configure-append <<EOF
-sed -i 's/^#PermitRootLogin prohibit-password/PermitRootLogin yes/' /etc/ssh/sshd_config
+configure-append <<'EOF'
+# openssh-server 10.x on Debian unstable can leave sshd_config as a sparse
+# zero-filled file in the image build, so replace it with a sane text config.
+cat > /etc/ssh/sshd_config <<'SSHD_CONFIG'
+Include /etc/ssh/sshd_config.d/*.conf
+KbdInteractiveAuthentication no
+UsePAM yes
+X11Forwarding yes
+PrintMotd no
+AcceptEnv LANG LC_* COLORTERM NO_COLOR
+Subsystem sftp /usr/lib/openssh/sftp-server
+PasswordAuthentication yes
+PermitRootLogin yes
+SSHD_CONFIG
 EOF
 
 configure-systemd-console-getty
