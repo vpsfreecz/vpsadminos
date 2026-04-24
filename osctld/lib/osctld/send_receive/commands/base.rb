f76f14a18f57c21275617845dc6e66978703767d
Jakub Skokan <jakub.skokan@havefun.cz>
2026-04-24 22:08:11 +0200

    osctld/send_receive: report receive pipeline failures
    The receive path previously treated a successful zfs receive as success
    when mbuffer had already exited with an error. That could acknowledge a
    broken transfer and persist receive state for an incomplete stream.
    
    Check both child statuses before accepting the receive and report all
    non-zero exit statuses so double failures are visible in the error
    message.

--- osctld/lib/osctld/send_receive/commands/base.rb
+++ osctld/lib/osctld/send_receive/commands/base.rb
@@ -5,5 +5,14 @@ module OsCtld
     def self.handle(name)
       SendReceive::Command.register(name, self)
     end
+
+    protected
+
+    def receive_pipeline_error(mbuffer_status, recv_status)
+      failures = []
+      failures << "mbuffer exited with #{mbuffer_status.exitstatus}" if mbuffer_status.exitstatus != 0
+      failures << "zfs recv exited with #{recv_status.exitstatus}" if recv_status.exitstatus != 0
+      "unable to receive stream, #{failures.join(' and ')}"
+    end
   end
 end
