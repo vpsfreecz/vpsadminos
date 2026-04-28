a246c260bef5e8b27a453c6dd244d840039e6ab8
Jakub Skokan <jakub.skokan@havefun.cz>
2026-04-28 23:52:39 +0200

    image-scripts: add fedora-44
    Add Fedora 44 image build support using the existing Fedora builder.
    Make it the latest stable Fedora image for repository users.

---
+++ image-scripts/images/fedora-44/build.sh
@@ -0,0 +1,26 @@
+. "$IMAGEDIR/config.sh"
+BASEURL=http://ftp.fi.muni.cz/pub/linux/fedora/linux/releases/$RELVER/Everything/x86_64/os
+RELEASE="$BASEURL/Packages/f/fedora-release-server-$RELVER-17.noarch.rpm
+$BASEURL/Packages/f/fedora-release-$RELVER-17.noarch.rpm
+$BASEURL/Packages/f/fedora-release-common-$RELVER-17.noarch.rpm
+$BASEURL/Packages/f/fedora-release-identity-basic-$RELVER-17.noarch.rpm"
+EXTRAPKGS="@core vim man fedora-gpg-keys fedora-repos glibc-langpack-en"
+REMOVEPKGS="plymouth"
+
+. $INCLUDE/redhat-family.sh
+. "$INCLUDE/systemd.sh"
+
+bootstrap
+configure-common
+
+configure-redhat-common
+configure-fedora
+configure-fedora-nm-keyfiles
+
+configure-append <<'EOF'
+systemctl mask systemd-hostnamed.service
+EOF
+
+configure-systemd-console-getty
+
+run-configure
