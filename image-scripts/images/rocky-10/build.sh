129c079a0aafa2ebaa92937548fb577d7a7e8363
Jakub Skokan <jakub.skokan@havefun.cz>
2026-06-07 11:34:16 +0200

    image-scripts: update rocky-10 to 10.2
    Update Rocky Linux 10 image builds to the latest upstream point release
    and release RPM discovered from the Rocky package listing.

--- image-scripts/images/rocky-10/build.sh
+++ image-scripts/images/rocky-10/build.sh
@@ -1,6 +1,6 @@
 . "$IMAGEDIR/config.sh"
-POINTVER=10.1
-RELEASE=https://ftp.linux.cz/pub/linux/rocky/${POINTVER}/BaseOS/x86_64/os/Packages/r/rocky-release-${POINTVER}-1.8.el10.noarch.rpm
+POINTVER=10.2
+RELEASE=https://ftp.linux.cz/pub/linux/rocky/${POINTVER}/BaseOS/x86_64/os/Packages/r/rocky-release-${POINTVER}-1.1.el10.noarch.rpm
 BASEURL=https://ftp.linux.cz/pub/linux/rocky/${POINTVER}/BaseOS/x86_64/os/
 
 # CentOS 8 does not seem to have an updates repo, so this variable is used to
