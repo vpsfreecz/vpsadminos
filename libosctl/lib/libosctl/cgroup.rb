aea3735ef9102bd3fb6ba302160424e6f143f469
Pavel Snajdr <snajpa@snajpa.net>
2026-06-20 18:23:57 +0200

    osctld: use the private cgroupfs view

--- libosctl/lib/libosctl/cgroup.rb
+++ libosctl/lib/libosctl/cgroup.rb
@@ -1,16 +1,15 @@
 module OsCtl::Lib
   module CGroup
-    FS = '/sys/fs/cgroup'.freeze
+    DEFAULT_FS = '/sys/fs/cgroup'.freeze
+    RUNSTATE_FS = '/run/osctl/cgroup'.freeze
+    FS = DEFAULT_FS
+    RUNSTATE_VERSION = '/run/osctl/cgroup.version'.freeze
 
     # @return [1, 2] cgroup hierarchy version
     def self.version
       return @version if @version
 
-      @version = if File.exist?(File.join(FS, 'cgroup.procs'))
-                   2
-                 else
-                   1
-                 end
+      @version = runstate_version || fs_version
 
       @version
     end
@@ -21,6 +20,41 @@ module OsCtl::Lib
 
     def self.v2?
       version == 2
+    end
+
+    def self.fs
+      @fs ||= detect_fs(@version)
+    end
+
+    def self.runstate_version
+      version = Integer(File.read(RUNSTATE_VERSION).strip)
+      version if [1, 2].include?(version)
+    rescue Errno::ENOENT, Errno::EACCES, ArgumentError
+      nil
+    end
+
+    def self.fs_version
+      File.exist?(File.join(fs, 'cgroup.procs')) ? 2 : 1
+    end
+
+    def self.detect_fs(version)
+      [RUNSTATE_FS, DEFAULT_FS].find do |path|
+        usable_fs?(path, version)
+      end || DEFAULT_FS
+    end
+
+    def self.usable_fs?(path, version)
+      return false unless Dir.exist?(path)
+
+      if version == 2
+        File.exist?(File.join(path, 'cgroup.procs'))
+      elsif version == 1
+        Dir.children(path).any?
+      else
+        File.exist?(File.join(path, 'cgroup.procs')) || Dir.children(path).any?
+      end
+    rescue SystemCallError
+      false
     end
   end
 end
